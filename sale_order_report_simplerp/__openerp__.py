9597666a479cece14a814c3e206a85410d3ade6b
sergio <sergiocorato@gmail.com>
2015-09-21 22:55:18 +0200

    sale report initial commit

---
+++ sale_order_report_simplerp/__openerp__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 SimplERP srl (<http://www.simplerp.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Italy - Improved sale order report for SimplERP projects',
+    'version': '4.0.0.0',
+    'category': 'other',
+    'description': """
+    Report esteso per offerte informatiche SimplERP
+    """,
+    'author': 'SimplERP SRL',
+    'website': 'http://www.simplerp.it',
+    'license': 'AGPL-3',
+    "depends": [
+        'report',
+        'sale',
+        'account',
+        'l10n_it_fiscalcode',
+    ],
+    "data": [
+        'views/sale.report_saleorder_simplerp.xml',
+        #'views/report.paperformat_euro_invoice.xml',
+        #'views/account_invoice_report.xml',
+        #'views/report.external_layout_header_inh.xml',
+        #'views/report.external_layout_footer_inh.xml',
+        #'views/account.report_invoice_document_inh.xml',
+    ],
+    "demo": [],
+    "test": [
+    ],
+    "installable": True,
+}

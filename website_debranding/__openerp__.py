780c17efbeb1b3cbb41c678e4ab5084a8f14c215
sergio <sergiocorato@gmail.com>
2015-10-10 17:16:41 +0200

    add useful modules

---
+++ website_debranding/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Website debranding",
+    'version': '1.0.0',
+    'author': 'IT-Projects LLC, Ivan Yelizariev',
+    'category': 'Debranding',
+    'website': 'https://yelizariev.github.io',
+    'price': 60.00,
+    'currency': 'EUR',
+    'depends': ['web',],
+    'data': [
+        'views.xml',
+        'pre_install.yml',
+        ],
+    'installable': True
+}

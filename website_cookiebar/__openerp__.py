34890acd0ddc49dcf233a1713a43a66dbe405734
sergio <sergiocorato@gmail.com>
2016-02-26 00:08:25 +0100

    integrate cookiebar

---
+++ website_cookiebar/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'CookieBar',
+    'category': 'Website',
+    'summary': 'Informs visitors about storing cookies on their browser',
+    'version': '1.0',
+    'description': """
+CookieBar
+======================================
+Informs visitors about storing cookies on their browser
+        """,
+    'author': 'Nedas Žilinskas <nedas.zilinskas@gmail.com>',
+    'website': 'http://nedaszilinskas.com',
+    'depends': ['website'],
+    'data': [
+        'data/x_cookiebar_config.xml',
+        'views/assets.xml',
+        'views/views.xml',
+        'views/templates.xml'
+    ],
+    'installable': True
+}

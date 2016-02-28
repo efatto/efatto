fd9765d93f981a5dc3789d9138ba0843a18d74a9
sergio <sergiocorato@gmail.com>
2016-02-28 17:30:32 +0100

    update script - require argument(s) db(s) separeted by space

--- translation_simplerp/script/translate.sh
+++ translation_simplerp/script/translate.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 shopt -s nullglob
 FILES=/opt/simplerp/buildout/parts/simplerp/translation_simplerp/i18n/*.po
-DATABASE=inserire_il_nome_del_db
+ARGS=$@
 
-for f in $FILES
+for db in $ARGS
 do
-  echo "Elaborazione file $f ..."
-  /opt/simplerp/buildout/bin/start_odoo --language=it_IT --i18n-import=$f --i18n-overwrite  -d $DATABASE
+    echo "Elaborazione db $db ..."
+    for f in $FILES
+    do
+        echo "Elaborazione file $f ..."
+        /opt/simplerp/buildout/bin/start_odoo --language=it_IT --i18n-import=$f --i18n-overwrite  -d $db
+    done
 done

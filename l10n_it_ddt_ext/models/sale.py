81ed45e6759fbc8de8fbcd200ee4058ab208ed38
sergio <sergiocorato@gmail.com>
2016-04-28 09:52:55 +0200

    [IMP] ddt type from sale order

--- l10n_it_ddt_ext/models/sale.py
+++ l10n_it_ddt_ext/models/sale.py
@@ -9,4 +9,28 @@ class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
 
+    def _default_ddt_type(self):
+        return self.env['stock.ddt.type'].search([], limit=1)
+
     create_ddt = fields.Boolean('Automatically create the DDT', default=True)
+    ddt_type_id = fields.Many2one(
+        'stock.ddt.type',
+        'Type of DDT', default=_default_ddt_type)
+
+    def _preparare_ddt_data(self, cr, uid, order, context=None):
+        res = super(SaleOrder, self)._preparare_ddt_data(
+            cr, uid, order, context)
+        ddt_type = order.ddt_type_id
+        if not res.get('carriage_condition_id', False):
+            res['carriage_condition_id'] = ddt_type.carriage_condition_id.id
+        if not res.get('goods_description_id', False):
+            res['goods_description_id'] = ddt_type.goods_description_id.id
+        if not res.get('transportation_reason_id', False):
+            res['transportation_reason_id'] = \
+                ddt_type.transportation_reason_id.id
+        if not res.get('transportation_method_id', False):
+            res['transportation_method_id'] = \
+                ddt_type.transportation_method_id.id
+        if not res.get('ddt_type_id', False):
+            res['ddt_type_id'] = ddt_type.id
+        return res

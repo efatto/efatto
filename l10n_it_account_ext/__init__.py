827f1391657e73c23e2ab9d69aeca39c9ee0a55b
sergio <sergiocorato@gmail.com>
2016-05-18 00:29:20 +0200

    [IMP] journal standard update_posted for all

--- l10n_it_account_ext/__init__.py
+++ l10n_it_account_ext/__init__.py
@@ -13,19 +13,21 @@ def post_init_hook(cr, registry):
     logging.getLogger('openerp.addons.l10n_it_account_ext').info(
         'Setting values for account.journal group_invoice_lines '
         'and update_posted')
+    all_journal_ids = account_journal_model.search(cr, SUPERUSER_ID, [])
+    account_journal_model.write(cr, SUPERUSER_ID, all_journal_ids, {
+        'update_posted': True,
+        })
     invoices_journal_ids = account_journal_model.search(cr, SUPERUSER_ID, [
         ('type', 'in', [
             'sale', 'sale_refund', 'purchase', 'purchase_refund', ]),
         ])
     account_journal_model.write(cr, SUPERUSER_ID, invoices_journal_ids, {
         'group_invoice_lines': True,
-        'update_posted': True,
         })
     cash_bank_journal_ids = account_journal_model.search(cr, SUPERUSER_ID, [
         ('type', 'in', ['cash', 'bank', 'general', ]),
         ])
     account_journal_model.write(cr, SUPERUSER_ID, cash_bank_journal_ids, {
-        'update_posted': True,
         'entry_posted': True,
         })
 

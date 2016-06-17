666216e540ce022f46620b0ea5b2b09b793cad6f
sergio <sergiocorato@gmail.com>
2016-06-17 17:17:36 +0200

    initial commit profile efatto

---
+++ profile_efatto/__openerp__.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Copyright (C) 2016 Sergio Corato - SimplERP srl (<http://www.simplerp.it>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{
+    'name': 'Italy - eFatto profile apps',
+    'version': '8.0.0.0.0',
+    'category': 'other',
+    'description': """
+    Italy - eFatto profile apps
+    """,
+    'author': 'Sergio Corato - SimplERP SRL',
+    'website': 'http://www.simplerp.it',
+    'license': 'AGPL-3',
+    "depends": [
+        'account_bank',
+        'account_balance_line_view',
+        'account_cancel',
+        'account_due_list',
+        'account_invoice_entry_date_ext',
+        'account_invoice_force_number',
+        'account_move_line_no_default_search',
+        'account_move_line_report_xls',
+        'account_auto_fy_sequence',
+        'account_vat_statement_endyear',
+        'account_withholding_tax',
+        'base_setup',
+        'base_iban',
+        'contacts',
+        'country_data_it',
+        'disable_openerp_online',
+        'invoice_report_aeroo_simplerp',
+        'l10n_it_abicab',
+        'l10n_it_account_ext',
+        'l10n_it_base_location_geonames_import',
+        'l10n_it_fiscalcode',
+        'l10n_it_pec',
+        'l10n_it_ricevute_bancarie_data',
+        'l10n_it_vat_registries',
+        'sale_order_report_aeroo',
+        'stock_picking_report_aeroo',
+        'web_debranding',
+    ],
+    "installable": True,
+}

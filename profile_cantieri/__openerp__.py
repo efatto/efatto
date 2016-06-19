3d49152f7e7269ffbe01ed8a2bb75fa6d7742af6
sergio <sergiocorato@gmail.com>
2016-06-20 00:04:18 +0200

    initial commit profile cantieri

---
+++ profile_cantieri/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Copyright (C) 2016 Sergio Corato - SimplERP srl (<http://www.simplerp.it>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{
+    'name': 'Italy - cantieri profile apps',
+    'version': '8.0.0.0.0',
+    'category': 'other',
+    'description': """
+    Italy - cantieri profile apps
+    """,
+    'author': 'Sergio Corato - SimplERP SRL',
+    'website': 'http://www.simplerp.it',
+    'license': 'AGPL-3',
+    "depends": [
+        'disable_openerp_online',
+        'hr_timesheet_sheet',
+        'l10n_it_base_location_geonames_import',
+        'l10n_it_simplerp',
+        'project',
+        'project_task_materials_stock',
+        'reminder_task_deadline',
+        'web_debranding_ext',
+    ],
+    "installable": True,
+}

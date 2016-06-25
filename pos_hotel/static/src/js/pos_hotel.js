83ddf91bec98ac3c1114c7886898c35f496236c7
sergio <sergiocorato@gmail.com>
2016-06-25 09:18:00 +0200

    [IMP] room in receipt

--- pos_hotel/static/src/js/pos_hotel.js
+++ pos_hotel/static/src/js/pos_hotel.js
@@ -44,4 +44,31 @@ Overload: point_of_sale.PosModel
 
         return _initialize_.call(this, session, attributes);
     };
+/* ********************************************************
+Overload: point_of_sale.module.Order
+- Overload module.Order.export_for_printing function to load extra-data
+     - Load 'room' field of model res.partner;
+*********************************************************** */
+    var ModuleOrderSuper = module.Order;
+    module.Order = module.Order.extend({
+        export_for_printing:function(){
+            var res = ModuleOrderSuper.prototype.export_for_printing.call(this);
+            debugger;
+            var client  = this.get('client');
+            var room = '';
+            if (client){
+                if (client.room_id){
+                    room = client.room_id[1];
+                }
+            }
+            res.room = room;
+
+            return res;
+        },
+        export_as_JSON:function(){
+            var res = ModuleOrderSuper.prototype.export_as_JSON.call(this);
+
+            return res;
+        },
+    });
 };

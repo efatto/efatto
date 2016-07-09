d0306a3d43a77a9cffcd7ae3a5996a6475d67845
sergio <sergiocorato@gmail.com>
2016-07-09 17:09:27 +0200

    [IMP] completed initial commit project include

--- project_include/models/project_include.py
+++ project_include/models/project_include.py
@@ -10,82 +10,49 @@ class WizardProjectInclude(models.TransientModel):
 
     parent_project_id = fields.Many2one('project.project')
     parent_task_id = fields.Many2one('project.task') #todo domain only on parent project
+    duplicate = fields.Boolean(default=True)
 
     @api.multi
     def project_include(self):
         self.ensure_one()
-        for task in self.env['project.task'].search(
-                [('project_id', '=', self._context['active_id'])]):
-            task.project_id = self.parent_project_id
-            task.parent_ids += self.parent_task_id
-            task.date_start = self.parent_task_id.date_end and \
-                self.parent_task_id.date_end or \
-                self.parent_task_id.date_start and \
-                self.parent_task_id.date_start or fields.Date.today()
-        # self.env['project.project'].browse(self._context['active_id']).unlink()
-        #
-        # data_obj = self.env['ir.model.data']
-        # if result and len(result):
-        #     res_id = result[0]
-        #     form_view_id = data_obj._get_id(self._cr, self._uid, 'project', 'edit_project')
-        #     form_view = data_obj.read(self._cr, self._uid, form_view_id, ['res_id'])
-        #     tree_view_id = data_obj._get_id(self._cr, self._uid, 'project', 'view_project')
-        #     tree_view = data_obj.read(self._cr, self._uid, tree_view_id, ['res_id'])
-        #     search_view_id = data_obj._get_id(self._cr, self._uid, 'project',
-        #                                       'view_project_project_filter')
-        #     search_view = data_obj.read(self._cr, self._uid, search_view_id, ['res_id'])
-        #     return {
-        #         'name': _('Projects'),
-        #         'view_type': 'form',
-        #         'view_mode': 'form,tree',
-        #         'res_model': 'project.project',
-        #         'view_id': False,
-        #         'res_id': res_id,
-        #         'views': [(form_view['res_id'], 'form'),
-        #                   (tree_view['res_id'], 'tree')],
-        #         'type': 'ir.actions.act_window',
-        #         'search_view_id': search_view['res_id'],
-        #         'nodestroy': True
-        #     }
+        active_project_id = self.env['project.project'].browse(
+            self._context['active_id'])
+        if self.duplicate:
+            for task in self.env['project.task'].search(
+                    [('project_id', '=', self._context['active_id'])]):
+                new_task = task.copy(default={
+                    'name': task.name,
+                    'project_id': self.parent_project_id.id,
+                    'date_start': self.parent_task_id.date_end and
+                                  self.parent_task_id.date_end or
+                                  self.parent_task_id.date_start and
+                                  self.parent_task_id.date_start or fields.Date.today(),
+                })
+                new_task.parent_ids += self.parent_task_id
+            for task in self.env['project.task'].search(
+                    [('project_id', '=', self.parent_project_id.id)]):
+                for parent_task in task.parent_ids:
+                    if parent_task.project_id == active_project_id:
+                        task.parent_ids -= parent_task
+                for child_task in task.child_ids:
+                    if child_task.project_id == active_project_id:
+                        task.child_ids -= child_task
+        elif not self.duplicate:
+            for task in self.env['project.task'].search(
+                    [('project_id', '=', self._context['active_id'])]):
+                task.project_id = self.parent_project_id
+                task.parent_ids += self.parent_task_id
+                task.date_start = self.parent_task_id.date_end and \
+                    self.parent_task_id.date_end or \
+                    self.parent_task_id.date_start and \
+                    self.parent_task_id.date_start or fields.Date.today()
 
-#
-# @api.multi
-# def load_template(self):
-#
-#     moves = {}
-#     for line in self.template_id.template_line_ids:
-#         if line.journal_id.id not in moves:
-#             moves[line.journal_id.id] = self._make_move(
-#                 self.template_id.name,
-#                 period.id,
-#                 line.journal_id.id,
-#                 self.partner_id.id
-#             )
-#
-#         self._make_move_line(
-#             line,
-#             computed_lines,
-#             moves[line.journal_id.id],
-#             period.id,
-#             self.partner_id.id
-#         )
-#         if self.template_id.cross_journals:
-#             trans_account_id = self.template_id.transitory_acc_id.id
-#             self._make_transitory_move_line(
-#                 line,
-#                 computed_lines,
-#                 moves[line.journal_id.id],
-#                 period.id,
-#                 trans_account_id,
-#                 self.partner_id.id
-#             )
-#
-#     return {
-#         'domain': "[('id','in', " + str(moves.values()) + ")]",
-#         'name': 'Entries',
-#         'view_type': 'form',
-#         'view_mode': 'tree,form',
-#         'res_model': 'account.move',
-#         'type': 'ir.actions.act_window',
-#         'target': 'current',
-#     }
+        return {
+            'domain': "[('project_id','=', " + str(self.parent_project_id.id) + ")]",
+            'name': 'Project',
+            'view_type': 'kanban',
+            'view_mode': 'kanban,tree,form',
+            'res_model': 'project.task',
+            'type': 'ir.actions.act_window',
+            'target': 'current',
+        }

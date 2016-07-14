877145524a7b94c3701e652846123c72d345e184
sergio <sergiocorato@gmail.com>
2016-07-14 23:24:23 +0200

    initial commit - task blocked now block all childs

---
+++ project_task_block/models/project.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, fields, api, _
+from openerp.exceptions import Warning, ValidationError
+
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    @api.multi
+    def write(self, vals):
+        def get_recursive_parent(task):
+            for parent in task.parent_ids:
+                if parent.kanban_state == 'blocked':
+                    raise Warning(_('Blocked task parent %s') % parent.name)
+                    return False
+                if parent.parent_ids:
+                    for parent_task in parent.parent_ids:
+                        if parent_task.kanban_state == 'blocked':
+                            raise Warning(_('Blocked task parent %s') %
+                                parent_task.name)
+                            return False
+                        get_recursive_parent(parent_task)
+        get_recursive_parent(self)
+
+        return super(ProjectTask, self).write(vals)

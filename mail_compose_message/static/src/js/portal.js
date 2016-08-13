a01269e73309efad1fe7d971808b720f0cdde607
sergio <sergiocorato@gmail.com>
2016-08-13 22:47:18 +0200

    [IMP] hide send mail element for portal users

--- mail_compose_message/static/src/js/portal.js
+++ mail_compose_message/static/src/js/portal.js
@@ -6,49 +6,27 @@ openerp.mail_compose_message = function (session) {
 
 /* ********************************************************
 Overload: mail.Wall
-- Overload mail.Wall.start function to hide button for some
-     groups
+- Overload mail.Wall.start function to hide button for
+     portal group users
 *********************************************************** */
     var MailWallSuper = mail.Wall;
     mail.Wall = mail.Wall.extend({
         hide_composeElement:function(){
-//            var res = MailWallSuper.prototype.start.call(this);
-            debugger;
             var composeElement = this.$el.find('.mail_compose_message');
             var Users = new openerp.web.Model('res.users');
                 Users.call('has_group', ['base.group_portal']).done(function(is_portal) {
                     if (is_portal) {
-                        composeElement.$el.hide();
+                        composeElement.hide();
                     }else{
-                        composeElement.$el.show();
-                    },
+                        composeElement.show();
+                    }
                 });
-                this._super.apply(this, arguments);
-//            return res;
-            },
+        },
+        start: function () {
+            var res = MailWallSuper.prototype.start.call(this);
+            this.hide_composeElement();
+            return res;
+        },
     });
-*****
-//    var _start_ = mail.Wall.prototype.start;
-//    mail.Wall.prototype.start = function(){
-//        self = this;
-//        debugger;
-//        var composeElement = this.$el.find('.mail_compose_message');
-//        if(openerp.web && openerp.web.UserMenu) {
-//            openerp.web.UserMenu.include({
-//                do_update: function(){
-//                    var self = this;
-//                    var Users = new openerp.web.Model('res.users');
-//                    Users.call('has_group', ['base.group_portal']).done(function(is_portal) {
-//                        if (is_portal) {
-//                            composeElement.$el.hide();
-//                        }else{
-//                            composeElement.$el.show();
-//                        },
-//                    });
-//                    return this._super.apply(this, arguments);
-//                },
-//            });
-//        };
-//        return _start_.call(this);
-//    };
+
 };

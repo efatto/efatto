e063974eca6fccbac864c9a35ea1e9280ecf4b2e
sergio <sergiocorato@gmail.com>
2016-08-16 19:14:14 +0200

    [FIX] reminder for issue

---
+++ reminder_issue_deadline/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Reminders and Agenda for Issues",
+    'version': '1.0.0',
+    'author': 'Matmoz d.o.o.',
+    'license': 'GPL-3',
+    'category': 'Reminders and Agenda',
+    'website': 'https://www.matmoz.si',
+    'price': 21.00,
+    'currency': 'EUR',
+    'depends': ['project','reminder_base', 'service_desk_issue'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views.xml',
+    ],
+    'installable': True,
+}
---
+++ reminder_issue_deadline/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from openerp import models
+
+
+class task(models.Model):
+    _name = 'project.issue'
+    _inherit = ['project.issue', 'reminder']
+
+    _reminder_date_field = 'date_deadline'
+    _reminder_attendees_fields = ['user_id']

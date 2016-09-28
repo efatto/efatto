1ddfb02a87a3cb014d0b3dc059ee898a3c7b08e1
sergio <sergiocorato@gmail.com>
2016-09-28 22:41:59 +0200

    [IMP] mail thread

--- mail_crm_lead/models/crm.py
+++ mail_crm_lead/models/crm.py
@@ -2,15 +2,22 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-import crm
 from openerp.addons.base.res.res_partner import format_address
-from openerp.osv import fields, osv, orm
-import xml.etree.ElementTree as ET
+from openerp.osv import osv
 from openerp.tools.translate import _
+from openerp.addons.mail.mail_message import decode
+
+AVAILABLE_PRIORITIES = [
+    ('0', 'Very Low'),
+    ('1', 'Low'),
+    ('2', 'Normal'),
+    ('3', 'High'),
+    ('4', 'Very High'),
+]
 
 
 class crm_lead(format_address, osv.osv):
-    _inherit = "crm.lead"
+    _inherit = 'crm.lead'
 
     def message_new(self, cr, uid, msg, custom_values=None, context=None):
         """ Overrides mail_thread message_new that is called by the mailgateway
@@ -19,9 +26,6 @@ class crm_lead(format_address, osv.osv):
         """
         if custom_values is None:
             custom_values = {}
-        b = msg.get('body')
-        e = b.replace('<br>', '').replace('\r\n', '')
-        body = ET.fromstring(e)
         email_from = False
         if msg.get('from') != msg.get('to'):
             email_from == msg.get('from')
@@ -35,13 +39,25 @@ class crm_lead(format_address, osv.osv):
             'partner_id': msg.get('author_id', False),
             'user_id': False,
             #'contact_name':
-            #'email_from':
             #'description':
             #'referred':
         }
         if msg.get('author_id'):
             defaults.update(self.on_change_partner_id(cr, uid, None, msg.get('author_id'), context=context)['value'])
-        if msg.get('priority') in dict(crm.AVAILABLE_PRIORITIES):
+        if msg.get('priority') in dict(AVAILABLE_PRIORITIES):
             defaults['priority'] = msg.get('priority')
         defaults.update(custom_values)
         return super(crm_lead, self).message_new(cr, uid, msg, custom_values=defaults, context=context)
+
+
+class MailThread(osv.AbstractModel):
+    _inherit = 'mail.thread'
+
+    def message_parse(self, cr, uid, message, save_original=False, context=None):
+        msg_dict = super(self, MailThread).message_parse(
+            cr, uid, message, save_original=save_original, context=context)
+
+        if message.get('Reply-To'):
+            msg_dict['reply_to'] = decode(message.get('Reply-To'))
+
+        return msg_dict

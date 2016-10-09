f0b2c6192bdfb7869056ab2dc953904e263ee94c
sergio <sergiocorato@gmail.com>
2016-10-09 21:43:34 +0200

    [IMP] only for better readability of ir_mail_server_per_user

--- ir_mail_server_per_user/model/res_config.py
+++ ir_mail_server_per_user/model/res_config.py
@@ -1,8 +1,10 @@
 from openerp import models, fields
 
+
 class base_config_settings(models.TransientModel):
     _inherit = 'base.config.settings'
     
-    group_visible_outgoing_mail_per_user = fields.Boolean( 'Send Userwise Outgoing emails?',
-                        implied_group='base.group_outgoing_mail_per_user',                                     
-                        help = 'You can allow user to set user in Outgoing server.')
+    group_visible_outgoing_mail_per_user = fields.Boolean(
+        'Send Userwise Outgoing emails?',
+        implied_group='base.group_outgoing_mail_per_user',
+        help = 'You can allow user to set user in Outgoing server.')

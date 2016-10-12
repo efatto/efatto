bae4b5ea9f82456b7f03a34cece0d45e25e91a99
sergio <sergiocorato@gmail.com>
2016-10-12 11:52:14 +0200

    [IMP] fix fetchmail - WothaN

---
+++ fetchmail_fix/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Fix fetchmail',
+    'version': '8.0.1.0.0',
+    'category': 'other',
+    'author': 'WohthaN',
+    'description': 'WohthaN fix',
+    'website': '',
+    'license': 'AGPL-3',
+    'depends': [
+        'fetchmail',
+        'mail',
+    ],
+    'data': [
+    ],
+    'installable': True
+}

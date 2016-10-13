102f9f1f40ee7707134117ea3b32b28a33d86941
sergio <sergiocorato@gmail.com>
2016-10-13 09:40:49 +0200

    [IMP] ported reconcile invoice module from 6.1

---
+++ account_invoice_reconcile/models/account_invoice.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, api
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def action_reconcile(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.move.line',
+            'views': [[False, 'tree'], [False, 'form']],
+            'domain': [
+                ['account_id', '=', self.account_id.id],
+                ['reconcile_id', '=', False]
+            ],
+        }

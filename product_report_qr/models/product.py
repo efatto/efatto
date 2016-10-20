34218ed1e10a6eb97b3da65af64ca742ad4d207c
sergio <sergiocorato@gmail.com>
2016-10-20 22:30:45 +0200

    [FIX] report labels

--- product_report_qr/models/product.py
+++ product_report_qr/models/product.py
@@ -2,8 +2,6 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-
-import time
 from openerp.report import report_sxw
 try:
     import qrcode
@@ -21,35 +19,49 @@ class Parser(report_sxw.rml_parse):
         self.cache = {}
 
     def _qr(self, text):
+        qr_code = qrcode.QRCode(version=1, box_size=1, border=1)
+        qr_code.add_data(text)
+        qr_code.make(fit=True)
+        qr_img = qr_code.make_image()
+        draw = qr_img._img.convert("RGB")
+        # Convert the RGB image in printable image
+        #self._convert_image(im)
+
         encoding = 'base64'
-        im = qrcode.make(text)
-        draw = im.convert('RGB')
+        #im = qrcode.make(text)
+        #draw = im.convert('RGB')
         background_stream = StringIO.StringIO()
         draw.save(background_stream, 'PNG')
         return background_stream.getvalue().encode(encoding)
 
     def _get_labels(self):
-        group_label = []
+        group_label = {}
         labels = []
         i = 0
         for product in self.pool['product.product'].browse(
                 self.cr, self.uid, self.ids):
-            group_label.append({
-                'qrcode': product.default_code and self._qr(
-                    product.default_code) or product.name and self._qr(
-                    product.name) or False,
-                'default_code': product.default_code,
+            group_label.update({
+                i: {
+                    'qrcode': product.default_code and self._qr(
+                        product.default_code) or product.name and self._qr(
+                            product.name) or False,
+                    'default_code': product.default_code and
+                    product.default_code or product.name and
+                    product.name or False,
+                }
             })
             i += 1
-            if i == 11:
-                labels.append(group_label)
-                group_label = []
+            if i == 12:
+                labels += [group_label]
+                group_label = {}
                 i = 0
-        if 0 < i < 11:
-            labels.append(group_label)
-            for f in range(11-i, 11, 1):
-                labels.append({
-                    'qrcode': False,
-                    'default_code': False,
+        if 0 < i < 12:
+            for f in range(i, 12, 1):
+                group_label.update({
+                    f: {
+                        'qrcode': False,
+                        'default_code': False,
+                    }
                 })
+            labels += [group_label]
         return labels

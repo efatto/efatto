3ef82534018744047d921e4c4ce2403f55d25592
sergio <sergiocorato@gmail.com>
2016-10-21 17:35:25 +0200

    [FIX] report barcode

--- product_report_qr/models/product.py
+++ product_report_qr/models/product.py
@@ -7,11 +7,11 @@ try:
     import qrcode
 except ImportError:
     qrcode = None
-import StringIO
-try:
-    import elaphe
-except ImportError:
-    barcode = None
+# import StringIO
+# try:
+#     import elaphe
+# except ImportError:
+#     barcode = None
 
 class Parser(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
@@ -21,25 +21,27 @@ class Parser(report_sxw.rml_parse):
         })
         self.cache = {}
 
-    def _qr(self, text):
-        text = text.encode('utf-8')
-        if self.name == 'product.product.labels.data.matrix':
-            img = elaphe.barcode(
-                'datamatrix', text,)
-                # options = dict(version=None, eclevel='M'),
-                # margin = 1, data_mode = '8bits')
-            draw = img.convert("RGB")
-        else:
-            qr_code = qrcode.QRCode(version=None, box_size=1, border=1)
-            qr_code.add_data(text)
-            qr_code.make(fit=True)
-            qr_img = qr_code.make_image()
-            draw = qr_img._img.convert("RGB")
-
-        encoding = 'base64'
-        background_stream = StringIO.StringIO()
-        draw.save(background_stream, 'PNG')
-        return background_stream.getvalue().encode(encoding)
+    # def _qr(self, text):
+    #     text = text.encode('utf-8')
+    #     if self.name == 'product.product.labels.data.matrix':
+    #         img = elaphe.barcode(
+    #             'datamatrix', text,)
+    #             # options = dict(version=None, eclevel='M'),
+    #             # margin = 1, data_mode = '8bits')
+    #         draw = img.convert("RGB")
+    #     else:
+    #         qr_code = qrcode.QRCode(version=None, box_size=1, border=1)
+    #         qr_code.add_data(text)
+    #         qr_code.make(fit=True)
+    #         qr_img = qr_code.make_image()
+    #         draw = qr_img._img.convert("RGB")
+    #
+    #     encoding = 'base64'
+    #     code_stream = StringIO.StringIO()
+    #     draw.save(code_stream, 'PNG')
+    #     value = code_stream.getvalue().encode(encoding)
+    #     code_stream.close()
+    #     return value
 
     def _get_labels(self):
         group_label = {}
@@ -49,9 +51,9 @@ class Parser(report_sxw.rml_parse):
                 self.cr, self.uid, self.ids):
             group_label.update({
                 i: {
-                    'qrcode': product.default_code and self._qr(
-                        product.default_code) or product.name and self._qr(
-                            product.name) or False,
+                    # 'qrcode': product.default_code and self._qr(
+                    #     product.default_code) or product.name and self._qr(
+                    #         product.name) or False,
                     'default_code': product.default_code and
                     product.default_code or product.name and
                     product.name or False,
@@ -66,7 +68,7 @@ class Parser(report_sxw.rml_parse):
             for f in range(i, 12, 1):
                 group_label.update({
                     f: {
-                        'qrcode': False,
+                        # 'qrcode': False,
                         'default_code': False,
                     }
                 })

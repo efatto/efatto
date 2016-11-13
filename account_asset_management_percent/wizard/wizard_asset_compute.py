b45944ac9e5116c6685c5cf5b3c3a6a048d5e063
sergio <sergiocorato@gmail.com>
2016-11-13 23:19:20 +0100

    [FIX] asset depreciation lines

--- account_asset_management_percent/wizard/wizard_asset_compute.py
+++ account_asset_management_percent/wizard/wizard_asset_compute.py
@@ -14,30 +14,48 @@ class AssetDepreciationConfirmationWizard(models.TransientModel):
             fy = self.env['account.fiscalyear'].browse(fiscalyear_id)
         return fy and fy or False
 
+    def _get_last_period(self):
+        fiscalyear_id = self.env['account.fiscalyear'].find()
+        if fiscalyear_id:
+            fy = self.env['account.fiscalyear'].browse(fiscalyear_id)
+            period = self.env['account.period'].with_context(
+                {'account_period_prefer_normal': True}
+            ).search([
+                ('fiscalyear_id', '=', fy.id),
+                ('date_stop', '=', fy.date_stop),
+            ], limit=1)
+        return period and period or False
+
     set_init = fields.Boolean(
-        'Set init',
-        help='Set depreciation as init for current fiscal year.')
+        'Set manual',
+        help='Set depreciation as manual for selected Fiscal Year.')
     fy_id = fields.Many2one(
         'account.fiscalyear', 'Fiscal Year',
         domain="[('state', '=', 'draft')]",
         default=_get_default_fiscalyear,
-        required=True, help='Calculate depreciation table for asset acquired'
-        ' in this Fiscal Year'
+        required=True, help='Calculate depreciation table for asset started '
+        'in selected Fiscal Year (in open or draft state).'
     )
     period_id = fields.Many2one(
         'account.period', 'Period',
         domain="[('special', '=', False), ('state', '=', 'draft')]",
-        required=False,
+        required=True, default=_get_last_period,
+        help='Confirm depreciation rows in selected period for asset '
+             'in open state.'
         )
 
     @api.multi
     def asset_set_init(self):
         self.ensure_one()
         ass_obj = self.env['account.asset.asset']
-        asset_ids = ass_obj.search([('state', 'in', ['open', 'draft']),
-            ('type', '=', 'normal')])
-        asset_board_obj = self.env['account.asset.depreciation.line']
         fy = self[0].fy_id
+        asset_ids = ass_obj.search([
+            ('state', 'in', ['open', 'draft']),
+            ('type', '=', 'normal'),
+            ('date_start', '>=', fy.date_start),
+            ('date_start', '<=', fy.date_stop)
+        ])
+        asset_board_obj = self.env['account.asset.depreciation.line']
         set_init = self[0].set_init
         init_move_ids = []
         for asset in asset_ids:
@@ -56,7 +74,7 @@ class AssetDepreciationConfirmationWizard(models.TransientModel):
                         asset_board.init_entry = True
                     init_move_ids.append(asset_board.id)
         return {
-            'name': _('Asset Moves Confirmed as Init entry'),
+            'name': _('Asset Moves Created'),
             'view_type': 'form',
             'view_mode': 'tree,form',
             'res_model': 'account.asset.depreciation.line',

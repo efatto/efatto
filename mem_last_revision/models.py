0af555b6fb19df9c9b09137f12e2204d45d9445b
sergio <sergiocorato@gmail.com>
2016-11-13 18:20:09 +0100

    [INI] removed mem repos moved in simplerp-wip

--- mem_last_revision/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-
-class mem_vehicle(models.Model):
-    _inherit = 'mem.vehicle'
-
-    oil_change_odometer = fields.Float("Last oil change (odometer)")

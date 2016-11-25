e1149b44d8d793797ccc0cc535ed9c34101a8a9e
sergio <sergiocorato@gmail.com>
2016-11-25 17:08:09 +0100

    [FIX] price computation for attribute

--- product_code_builder_parent_price/models/product.py
+++ product_code_builder_parent_price/models/product.py
@@ -11,27 +11,36 @@ class ProductProduct(models.Model):
 
     @api.multi
     def _get_price_extra(self):
-        # result = dict.fromkeys(ids, False)
         for product in self:
-            price_extra = price_attribute = 0.0
+            price_extra = price_attribute = cost_extra = cost_attribute = 0.0
             for variant_id in product.attribute_value_ids:
                 for price_id in variant_id.price_ids:
                     if price_id.product_tmpl_id.id == product.product_tmpl_id.id:
                         price_extra += price_id.price_extra
-                        # ADD extra price for attribute
-                    attribute_line = product.product_tmpl_id.\
-                        attribute_line_ids.filtered(lambda r:
-                                                    r.attribute_id ==
-                                                    price_id.attribute_id)
-                    if attribute_line:
-                        price_attribute = attribute_line.price_extra
+                        cost_extra += price_id.cost_extra
+                # ADD extra price for attribute
+                attribute_line = product.product_tmpl_id.\
+                    attribute_line_ids.filtered(lambda r:
+                                                r.attribute_id ==
+                                                variant_id.attribute_id)
+                if attribute_line:
+                    price_attribute = attribute_line.price_extra
+                    cost_attribute = attribute_line.cost_extra
             price_extra += price_attribute
             product.price_extra = price_extra
+            cost_extra += cost_attribute
+            product.cost_extra = cost_extra
 
     price_extra = fields.Float(
         compute=_get_price_extra,
         string='Variant Extra Price',
         help="This is the sum of the extra price of all attributes",
+        digits_compute=dp.get_precision('Product Price')
+    )
+    cost_extra = fields.Float(
+        compute=_get_price_extra,
+        string='Variant Extra Cost',
+        help="This is the sum of the extra cost of all attributes",
         digits_compute=dp.get_precision('Product Price')
     )
 
@@ -40,5 +49,16 @@ class ProductAttributeLine(models.Model):
     _inherit = "product.attribute.line"
 
     price_extra = fields.Float(
-            'Price Extra', digits_compute=dp.get_precision('Product Price')
+        'Price Extra', digits_compute=dp.get_precision('Product Price')
+    )
+    cost_extra = fields.Float(
+        'Cost Extra', digits_compute=dp.get_precision('Product Price')
+    )
+
+
+class ProductAttributePrice(models.Model):
+    _inherit = "product.attribute.price"
+
+    cost_extra = fields.Float(
+        'Cost Extra', digits_compute=dp.get_precision('Product Price')
     )

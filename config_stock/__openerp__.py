6fa9f63c8ca4b97c635d3d411684970cad750b21
sergio <sergiocorato@gmail.com>
2016-11-27 16:25:41 +0100

    [IMP] module to configure stock without manual work

---
+++ config_stock/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Copyright (C) 2016 Sergio Corato - SimplERP srl (<http://www.simplerp.it>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{
+    'name': 'Module to configure stock',
+    'version': '8.0.1.0.0',
+    'category': 'other',
+    'description': """
+Module to configure stock.
+""",
+    'author': 'Sergio Corato - SimplERP SRL',
+    'website': 'http://www.simplerp.it',
+    'license': 'AGPL-3',
+    "depends": [
+        'stock_account',
+    ],
+    "data": [
+        'data/group.xml',
+    ],
+    "installable": True
+}

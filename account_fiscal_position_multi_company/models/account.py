b74d5e22fd4aeba90a64bd07d037208c2904d85a
sergio <sergiocorato@gmail.com>
2016-11-29 09:29:28 +0100

    [FIX] fiscal position for multicompany

--- account_fiscal_position_multi_company/models/account.py
+++ account_fiscal_position_multi_company/models/account.py
@@ -8,8 +8,33 @@ from openerp import models, api
 class AccountFiscalPosition(models.Model):
     _inherit = 'account.fiscal.position'
 
-    @api.multi
-    @api.depends('fiscal_position')
+    @api.v7
+    def map_tax(self, cr, uid, fposition_id, taxes, context=None):
+        if not taxes:
+            return []
+        user = self.pool.get('res.users').browse(cr, uid, uid,
+                                                 context=context)
+        if user.company_id:
+            company_id = user.company_id
+        if not fposition_id:
+            return map(lambda x: x.id, [tax for tax in taxes if
+                                        tax.company_id == company_id])
+
+        result = set()
+        for t in taxes:
+            ok = False
+            for tax in fposition_id.tax_ids:
+                if tax.tax_src_id.id == t.id and \
+                        tax.tax_dest_id.company_id == company_id:
+                    if tax.tax_dest_id:
+                        result.add(tax.tax_dest_id.id)
+                    ok = True
+            if not ok:
+                if t.company_id == company_id:
+                    result.add(t.id)
+        return list(result)
+
+    @api.v8
     def map_tax(self, taxes):
         result = self.env['account.tax'].browse()
         for tax in taxes:
@@ -18,8 +43,9 @@ class AccountFiscalPosition(models.Model):
                 if t.tax_src_id == tax:
                     tax_count += 1
                     if t.tax_dest_id and \
-                            t.tax_dest_id.company_id == self.user.company_id:
+                            t.tax_dest_id.company_id == \
+                            self.env.user.company_id:
                         result |= t.tax_dest_id
-            if not tax_count and tax.company_id == self and self.user.company_id:
+            if not tax_count and tax.company_id == self.env.user.company_id:
                 result |= tax
         return result

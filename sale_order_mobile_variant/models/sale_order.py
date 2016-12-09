3a4349c3b1a6b39bb72d10512d3d457f5f5a6aed
sergio <sergiocorato@gmail.com>
2016-12-09 23:32:12 +0100

    [FIX] onchange fields

--- sale_order_mobile_variant/models/sale_order.py
+++ sale_order_mobile_variant/models/sale_order.py
@@ -16,10 +16,21 @@ class SaleOrder(models.Model):
         domain="[('product_tmpl_id','=',product_template_id)]"
         )
     product_attribute_value_ids = fields.Many2many(
-        string='Values', related='product_attribute_line_id.value_ids', readonly=True)
+        string='Values',
+        related='product_attribute_line_id.value_ids',
+        readonly=True)
     product_attribute_value_id = fields.Many2one(
         'product.attribute.value',
         domain="[('id', 'in', product_attribute_value_ids[0][2])]"
     )
     product_attribute_image = fields.Binary(
         related='product_attribute_value_id.image')
+
+    @api.onchange('product_template_id')
+    def onchange_product(self):
+        self.product_attribute_line_id = \
+            self.product_attribute_value_id = False
+
+    @api.onchange('product_attribute_line_id')
+    def onchange_product_attribute(self):
+        self.product_attribute_value_id = False

33cf2eda37339a6fcc883427e5272cf6824e098d
sergio <sergiocorato@gmail.com>
2016-12-26 01:42:53 +0100

    [IMP] add fields and button to fast add product

--- l10n_it_ddt_mobile/models/stock_picking_package_preparation.py
+++ l10n_it_ddt_mobile/models/stock_picking_package_preparation.py
@@ -2,11 +2,37 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-from openerp import models, api, exceptions, _
+from openerp import models, api, fields, exceptions, _
 
 
 class StockPickingPackagePreparation(models.Model):
     _inherit = 'stock.picking.package.preparation'
+
+    product_id = fields.Many2one(
+        comodel_name='product.product',
+        string='Product',
+    )
+    product_qty = fields.Float(
+        string='Q.ty',
+    )
+
+    @api.multi
+    def add_product_in_order(self):
+        for pack in self:
+            if pack.product_id in pack.line_ids.mapped('product_id'):
+                line = pack.line_ids.filtered(lambda r:
+                                              r.product_id ==
+                                              pack.product_id)
+                line.product_uom_qty += pack.product_qty
+            else:
+                pack.line_ids.create({
+                    'product_id': pack.product_id.id,
+                    'product_uom_qty': pack.product_qty,
+                    'product_uom': pack.product_id.uom_id.id,
+                    'name': pack.product_id.name,
+                    'package_preparation_id': pack.id,
+                    'lot_id': False,
+                })
 
     @api.multi
     def action_put_in_pack(self):

0e57cadf6d7adc3d309bf75d2e9a44cb8bc4fe6c
sergio <sergiocorato@gmail.com>
2017-01-02 15:32:31 +0100

    [FIX] translation and help

--- sale_order_weight/models/sale_order.py
+++ sale_order_weight/models/sale_order.py
@@ -11,27 +11,24 @@ class SaleOrder(models.Model):
 
     weight = fields.Float(
         compute='_compute_weight',
-        help="The weight is computed when the order "
-        "is done.",
+        help="The weight is computed when the order is done.",
         digits_compute=dp.get_precision('Stock Weight'))
     net_weight = fields.Float(
         compute='_compute_weight',
-        help="The weight is computed when the "
-        "order is done.",
+        help="The weight is computed when the order is done.",
         digits_compute=dp.get_precision('Stock Weight'))
     volume = fields.Float(
         compute='_compute_weight',
-        help="The volume is computed when the "
-        "order is done.",
+        help="The volume is computed when the order is done.",
         digits_compute=dp.get_precision('Stock Weight'))
     weight_custom = fields.Float(
-        help="Custom weight.",
+        help="Put here weight when computed amount is not exact.",
         digits_compute=dp.get_precision('Stock Weight'))
     net_weight_custom = fields.Float(
-        help="Custom net weight.",
+        help="Put here net weight when computed amount is not exact.",
         digits_compute=dp.get_precision('Stock Weight'))
     volume_custom = fields.Float(
-        help="Custom volume.",
+        help="Put here net volume when computed amount is not exact.",
         digits_compute=dp.get_precision('Stock Weight'))
     compute_weight = fields.Boolean(default=True)
 

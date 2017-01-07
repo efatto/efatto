9b583418ad129f51d910a936ffffb7f23ee794a6
sergio <sergiocorato@gmail.com>
2017-01-07 22:54:08 +0100

    [FIX] project issue create with analytic

--- project_issue_analytic/models/project_issue_sheet_analytic.py
+++ project_issue_analytic/models/project_issue_sheet_analytic.py
@@ -9,10 +9,9 @@ class ProjectIssue(models.Model):
     _inherit = 'project.issue'
 
     @api.multi
-    def write(self, vals):
-        for issue in self:
-            if issue.project_id and not issue.analytic_account_id:
-                issue.analytic_account_id = \
-                    issue.project_id.analytic_account_id
-
-        return super(ProjectIssue, self).write(vals)
+    @api.onchange('project_id')
+    def on_change_project(self):
+        if self.project_id:
+            account = self.project_id.analytic_account_id
+            if account:
+                self.analytic_account_id = account

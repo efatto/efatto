339ebc23ca05b64bf7c0f7808d4f7a606b064e83
sergio <sergiocorato@gmail.com>
2017-01-23 00:20:31 +0100

    [IMP] module to modify view of calendar of tasks

---
+++ project_task_calendar/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2016 Sergio Corato - SimplERP SRL
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Better view of task on calendar',
+    'version': '8.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'View task on calendar based on date_end and date_start',
+    'description':
+        'View task on calendar based on date_end and date_start',
+    'author': 'Sergio Corato - Simplerp srl',
+    'website': 'http://www.simplerp.it',
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'views/task_view.xml'
+    ],
+    'installable': True,
+}

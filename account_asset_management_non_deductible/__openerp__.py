25994f07906273defb839bddbb5f16bfc656d98d
sergio <sergiocorato@gmail.com>
2017-02-17 14:04:15 +0100

    [IMP] add non deductible asset depreciation

--- account_asset_management_non_deductible/__openerp__.py
+++ account_asset_management_non_deductible/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #
-#    Copyright (C) 2015-2016 SimplERP srl (<http://www.simplerp.it>).
+#    Copyright (C) 2016 Sergio Corato (<http://www.librerp.it>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
@@ -21,8 +21,8 @@
     'name': 'Italian customization for asset non deductible',
     'version': '8.0.1.0.0',
     'category': 'other',
-    'author': 'Sergio Corato - SimplERP SRL',
-    'website': 'http://www.simplerp.it',
+    'author': 'Sergio Corato',
+    'website': 'http://www.librerp.it',
     'license': 'AGPL-3',
     'description': 'Italian customization for asset',
     'depends': [

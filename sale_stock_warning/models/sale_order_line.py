eba2f4cc0b42b416b1d0e2c62f50287013e3f947
sergio <sergiocorato@gmail.com>
2017-03-03 09:48:01 +0100

    [FIX] incorrect super qty

--- sale_stock_warning/models/sale_order_line.py
+++ sale_stock_warning/models/sale_order_line.py
@@ -15,7 +15,7 @@ class SaleOrderLine(models.Model):
         lang=False, update_tax=True, date_order=False, packaging=False,
         fiscal_position=False, flag=False, warehouse_id=False, context=None):
         res = super(SaleOrderLine, self).product_id_change_with_wh(
-            cr, uid, ids, pricelist, product, qty=0,
+            cr, uid, ids, pricelist, product, qty=qty,
             uom=uom, qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
             lang=lang, update_tax=update_tax, date_order=date_order, packaging=packaging,
             fiscal_position=fiscal_position, flag=flag, warehouse_id=warehouse_id, context=context

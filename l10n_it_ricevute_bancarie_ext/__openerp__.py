a1f8f4ac70af0beae7229ed47039631ddd4e41d7
sergio <sergiocorato@gmail.com>
2017-04-03 10:35:42 +0200

    [FIX] module name missing

--- l10n_it_ricevute_bancarie_ext/__openerp__.py
+++ l10n_it_ricevute_bancarie_ext/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #
-#    Copyright (C) 2015 SimplERP srl (<http://www.simplerp.it>).
+#    Copyright (C) 2017 Sergio Corato.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
@@ -18,10 +18,10 @@
 #
 #
 {
-    'name': 'l10n_it_ricevute_bancarie_ext',
+    'name': 'Ricevute bancarie accruement',
     'version': '8.0.1.0.0',
     'category': 'other',
-    'author': 'Sergio Corato - SimplERP SRL',
+    'author': 'Sergio Corato',
     'website': 'http://www.simplerp.it',
     'license': 'AGPL-3',
     'depends': [

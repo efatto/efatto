123e343684cd1e80da29bd81c80042c2f265aa9a
sergio <sergiocorato@gmail.com>
2017-04-21 15:03:35 +0200

    [FIX] translation

---
+++ stock_move_details/models/stock.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import fields, models, api, _
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    @api.multi
+    def _set_sign_product_qty(self):
+        for move in self:
+            move.qty = move.product_uom_qty * (
+                -1 if move.location_dest_id.usage == 'customer' else 1)
+
+    qty_available = fields.Float(
+        related='product_id.qty_available',
+    )
+    virtual_available = fields.Float(
+        related='product_id.virtual_available',
+    )
+    qty = fields.Float(
+        compute='_set_sign_product_qty',
+    )
+

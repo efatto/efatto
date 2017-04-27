de3e937f6ba84707c4b6a874e03cb2259d9cd780
sergio <sergiocorato@gmail.com>
2017-04-27 09:29:27 +0200

    [IMP] add product bag module

---
+++ product_bag/models/product.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, api, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    product_bag_id = fields.Many2one(
+        'product.template', 'Bag',
+        domain=[('is_bag','=',True)]
+    )
+    is_bag = fields.Boolean()
+
+    @api.onchange('product_bag_id','product_pack_id')
+    def onchange_bag(self):
+        for product in self:
+            product.volume = product.product_bag_id.volume + \
+                             product.product_pack_id.volume
+            product.weight = product.weight_net + \
+                             product.product_bag_id.weight_net + \
+                             product.product_pack_id.weight_net
+
+    @api.onchange('weight_net')
+    @api.depends('product_bag_id','product_pack_id')
+    def onchange_weight_net(self):
+        for product in self:
+            product.weight = product.weight_net + \
+                             product.product_bag_id.weight_net + \
+                             product.product_pack_id.weight_net

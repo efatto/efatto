d90a025e1f154a21ec6c22c4cf68da6f018e6523
sergio <sergiocorato@gmail.com>
2017-05-11 00:01:10 +0200

    [IMP] add product template report qr

--- product_report_qr/models/product.py
+++ product_report_qr/models/product.py
@@ -13,6 +13,7 @@ try:
 except ImportError:
     elaphe = None
 
+
 class Parser(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
         super(Parser, self).__init__(cr, uid, name, context)

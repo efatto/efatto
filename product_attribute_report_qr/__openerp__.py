6549f192a207342b8180679d55dc5f8a2a8dc401
sergio <sergiocorato@gmail.com>
2017-05-11 00:02:09 +0200

    [IMP] add product attribute report qr

---
+++ product_attribute_report_qr/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright (C) 2016 Sergio Corato - SimplERP srl (<http://www.simplerp.it>)
+#    Copyright (c) 2015 Oihane Crucelaegui - AvanzOSC
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{
+    'name': 'Product label qr code',
+    'version': '8.0.1.0.0',
+    'category': 'Product Management',
+    'description': "Make Product label QRCode and Data Matrix",
+    'author': 'Sergio Corato - SimplERP Srl',
+    'website': 'http://simplerp.it',
+    'depends': [
+        'report_aeroo',
+        'report_branding',
+    ],
+    'data': [
+        'report/reports.xml',
+    ],
+    'installable': True,
+}

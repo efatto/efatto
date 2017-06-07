4c0b18b9edcc2a8d526251f84cdaec8afe51c259
sergio <sergiocorato@gmail.com>
2017-06-07 12:10:13 +0200

    [IMP] add report and button for picking

--- stock_picking_report_aeroo/__openerp__.py
+++ stock_picking_report_aeroo/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #    Copyright (C) 2013-2014 Didotech Srl (<http://www.didotech.com>)
-#    Copyright (C) 2016 Sergio Corato - SimplERP srl (<http://www.simplerp.it>)
+#    Copyright (C) 2017 Sergio Corato
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
@@ -21,8 +21,8 @@
     'name': 'Aeroo ddt report',
     'version': '8.1.0.0.0',
     'category': 'other',
-    'author': 'Sergio Corato - SimplERP SRL, Didotech Srl',
-    'website': 'http://www.simplerp.it',
+    'author': 'Sergio Corato, Didotech Srl',
+    'website': 'http://www.efatto.it',
     'license': 'AGPL-3',
     'depends': [
         'account',
@@ -34,6 +34,7 @@
     ],
     'data': [
         'data/reports.xml',
+        'views/picking_view.xml',
     ],
     'installable': True
 }
--- stock_picking_report_aeroo/models/stock_picking.py
+++ stock_picking_report_aeroo/models/stock_picking.py
@@ -2,4 +2,13 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-from . import models
+from openerp import models, api, fields
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.multi
+    def print_picking(self):
+        return self.env['report'].get_action(
+            self, 'stock.report_picking')

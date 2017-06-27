86eafd3b2e7b9ba1377fd62cec292830ac34ed20
sergio <sergiocorato@gmail.com>
2017-06-27 22:26:46 +0200

    [MOVE] move fields to stock_picking_usability

--- stock_picking_report_aeroo/models/stock_picking.py
+++ stock_picking_report_aeroo/models/stock_picking.py
@@ -8,11 +8,6 @@ from openerp import models, api, fields
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    sale_partner_invoice_id = fields.Many2one(
-        related='sale_id.partner_invoice_id')
-    sale_partner_id = fields.Many2one(
-        related='sale_id.partner_id')
-
     @api.multi
     def print_picking(self):
         return self.env['report'].get_action(

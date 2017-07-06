c639c94594f3c15f2a4cff304207246de487b02d
sergio <sergiocorato@gmail.com>
2017-07-06 22:28:01 +0200

    [FIX] update description

--- account_invoice_reconcile/__openerp__.py
+++ account_invoice_reconcile/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #
-#    Copyright (C) 2016 Sergio Corato - SimplERP srl (<http://www.simplerp.it>)
+#    Copyright (C) 2016-2017 Sergio Corato
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
@@ -21,11 +21,9 @@
     'name': 'account_invoice_reconcile',
     'version': '8.0.1.0.0',
     'category': 'other',
-    'description': """
-Add reconcile action to invoice
-""",
-    'author': 'Sergio Corato - SimplERP SRL',
-    'website': 'http://www.simplerp.it',
+    'description': "Add reconcile action to invoice",
+    'author': 'Sergio Corato',
+    'website': 'http://www.efatto.it',
     'license': 'AGPL-3',
     "depends": [
         'account',

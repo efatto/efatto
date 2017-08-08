a8ae8d38a9721fc6b9fe19cc104ba780c70d0e0b
sergio <sergiocorato@gmail.com>
2017-08-08 18:26:46 +0200

    [MOV] to wip

--- account_smart_reporting/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from . import account
-from . import account_config
-from . import account_report
-from . import mail

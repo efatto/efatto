c24e7f13889c47d897cb8c5c3f4534a816d9bd0a
sergio <sergiocorato@gmail.com>
2017-08-16 11:49:24 +0200

    [MOV] to variant

--- product_code_builder_parent_price/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from . import product
-from . import pricelist
-from . import sale
-from . import purchase

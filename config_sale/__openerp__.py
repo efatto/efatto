e6918f14c69e9504ff46f0db6ff410f837381d3d
sergio <sergiocorato@gmail.com>
2017-10-15 22:44:18 +0200

    [IMP] translation and description

--- config_sale/__openerp__.py
+++ config_sale/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #
-#    Copyright (C) 2016 Sergio Corato - SimplERP srl (<http://www.simplerp.it>)
+#    Copyright (C) 2016-2017 Sergio Corato
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
@@ -23,9 +23,16 @@
     'category': 'other',
     'description': """
 Module to configure sale.
+Add the next groups to base user:
+- invoice so lines;
+- invoice deli orders;
+- delivery invoice address;
+- proforma invoice;
+- sale pricelist;
+It also set sale order policy default to manual.
 """,
-    'author': 'Sergio Corato - SimplERP SRL',
-    'website': 'http://www.simplerp.it',
+    'author': 'Sergio Corato',
+    'website': 'http://www.efatto.it',
     'license': 'AGPL-3',
     "depends": [
         'account',

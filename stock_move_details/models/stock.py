3d23b12d6153228ab715602dc6f86b73abb90480
sergio <sergiocorato@gmail.com>
2017-10-29 19:07:17 +0100

    [IMP] usability in stock move tree view

--- stock_move_details/models/stock.py
+++ stock_move_details/models/stock.py
@@ -26,5 +26,9 @@ class StockMove(models.Model):
         compute='_set_sign_product_qty',
     )
     picking_partner_id = fields.Many2one(
-        related='picking_id.partner_id',
+        'res.partner', string='Picking Partner', store=True,
+        related='picking_id.partner_id', help='Partner of the picking')
+    product_code = fields.Char(
+        string='Product Code',
+        related='product_id.default_code'
     )

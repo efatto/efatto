3186ff5ca90cc23b3efa1133827318a2705aedfb
sergio <sergiocorato@gmail.com>
2017-12-15 18:03:07 +0100

    [IMP] add new ddt types

--- stock_ddt_data/__openerp__.py
+++ stock_ddt_data/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #
-#    Copyright (C) 2016 Sergio Corato - SimplERP srl (<http://www.simplerp.it>)
+#    Copyright (C) 2016-2017 Sergio Corato
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
@@ -24,8 +24,8 @@
     'description': """
     Dati standard per DDT con sequenze pluriennali con anno fiscale.
     """,
-    'author': 'Sergio Corato - SimplERP SRL',
-    'website': 'http://www.simplerp.it',
+    'author': 'Sergio Corato',
+    'website': 'http://www.efatto.it',
     'license': 'AGPL-3',
     "depends": [
         'account_auto_fy_sequence',

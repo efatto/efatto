2717a825189d1007058dc8d3883883f91db53aa1
sergio <sergiocorato@gmail.com>
2018-03-12 19:35:43 +0100

    [FIX] total undeductible vat in vat registry report

--- l10n_it_vat_registries_ext/models/vat_registry.py
+++ l10n_it_vat_registries_ext/models/vat_registry.py
@@ -24,6 +24,43 @@ class VatRegistryParser(Parser):
         fy_code = fy_obj.browse(self.cr, self.uid, [fy]).code
         return fy_code
 
+    def is_totally_undeductable(self, tax):
+        children_tax_codes = []
+        # FIX tax totally non deductible
+        if not tax.child_ids and tax.nondeductible:
+            return True
+        # END FIX
+        for tax in tax.child_ids:
+            children_tax_codes.append(tax.tax_code_id.id)
+        if len(set(children_tax_codes)) == 1:
+            return True
+        else:
+            return False
+
+    def get_undeductible_balances(self, tax):
+        total_undeduct = 0
+        total_deduct = 0
+        if self.is_totally_undeductable(tax):
+            # FIX tax totally non deductible
+            if not tax.child_ids:
+                total_undeduct = self.compute_tax_code_total(
+                    tax.tax_code_id)
+            # END FIX
+            else:
+                total_undeduct = self.compute_tax_code_total(
+                    tax.child_ids[0].tax_code_id)
+        else:
+            for child in tax.child_ids:
+                # deductibile
+                if child.tax_code_id and child.account_collected_id:
+                    total_deduct = self.compute_tax_code_total(
+                        child.tax_code_id)
+                # undeductibile
+                elif child.tax_code_id:
+                    total_undeduct = self.compute_tax_code_total(
+                        child.tax_code_id)
+        return (total_undeduct, total_deduct)
+
     def __init__(self, cr, uid, name, context):
         super(VatRegistryParser, self).__init__(cr, uid, name, context=context)
 

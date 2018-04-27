3db6e8794810b754715a697b59d32469f7df6d16
sergio <sergiocorato@gmail.com>
2018-04-27 16:06:19 +0200

    [IMP] test color

---
+++ asn_odoo_theme_test/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Odoo 9 Backend-Theme for test',
+    'version': '1.0',
+    'author': 'Ajeng Shilvie N',
+    'description': '''
+        A custom module to change menu bar like Odoo Enterprise for test
+    ''',
+    'category': 'Themes/ASN',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/custom_view.xml',
+    ],
+    'css': ['static/src/css/styles.css'],
+    'auto_install': False,
+    'installable': True,
+}

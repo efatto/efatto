4cb1663f4cee28e4ef277e0f07016c370a7f9886
sergio <sergiocorato@gmail.com>
2018-06-26 11:18:58 +0200

    [IMP] add module to filter analytic account

--- account_analytic_follower/models/analytic.py
+++ account_analytic_follower/models/analytic.py
@@ -11,28 +11,15 @@ class AnalyticAccount(models.Model):
     @api.multi
     def name_get(self):
         res = []
-        if not self.ids:
-            return res
         for elmt in self.sudo():
             res.append((elmt.id, self._get_one_full_name(elmt)))
         return res
 
-    @api.multi
+    @api.one
     @api.depends('name', 'parent_id')
-    def _get_full_name(self, name=None, args=None):
-        res = {}
-        for elmt in self.sudo():
-            parent_path = ''
-            for level in range(6, 0):
-                if level <= 0:
-                    parent_path = '...'
-                    break
-                if elmt.parent_id and not elmt.type == 'template':
-                    parent_path += " / %s" % elmt.name
-                else:
-                    parent_path += elmt.name
-            res[elmt.id] = parent_path
-        return res
+    def _get_full_name(self):
+        self.complete_name = self._get_one_full_name(self)
 
     complete_name = fields.Char(
-        compute=_get_full_name, compute_sudo=True, string='Full Name')
+        compute=_get_full_name, store=True, compute_sudo=True,
+        string='Full Name')

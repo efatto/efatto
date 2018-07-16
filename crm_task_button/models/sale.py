63d60cb7ea8893ad08c5946a5f48726764753624
sergio <sergiocorato@gmail.com>
2018-07-16 16:51:22 +0200

    [IMP] task button from crm

--- crm_task_button/models/sale.py
+++ crm_task_button/models/sale.py
@@ -2,7 +2,7 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-from openerp import fields, api, models
+from openerp import fields, api, models, _
 
 
 class CrmLead(models.Model):
@@ -10,37 +10,27 @@ class CrmLead(models.Model):
 
     task_count = fields.Integer(related='partner_id.task_count')
 
-    # def _crm_task_count(self, cr, uid, ids, field_name, arg, context=None):
-    #     res = super(res_partner, self)._sale_order_count(cr, uid, ids, field_name=field_name, arg=arg, context=context)
-    #     # The current user may not have access rights for sale orders
-    #     try:
-    #         for partner in self.browse(cr, uid, ids, context):
-    #             res[partner.id] = len(filter(lambda x: x.state not in ['draft', 'sent', 'cancel'], partner.sale_order_ids)) \
-    #                               + len(partner.mapped(
-    #                               'child_ids.sale_order_ids'))
-    #     except:
-    #         pass
-    #     return res
-    #
-    # def _draft_sale_order_count(self, cr, uid, ids, field_name, arg, context=None):
-    #     res = dict(map(lambda x: (x,0), ids))
-    #     # The current user may not have access rights for sale orders
-    #     try:
-    #         for partner in self.browse(cr, uid, ids, context):
-    #             res[partner.id] = len(filter(lambda x: x.state in ['draft', 'sent', 'cancel'], partner.sale_order_ids)) \
-    #                               + len(partner.mapped(
-    #                               'child_ids.sale_order_ids'))
-    #     except:
-    #         pass
-    #     return res
-    #
-    # _columns = {
-    #     'sale_order_count': fields.function(_sale_order_count,
-    #                                         string='# of Sales Order',
-    #                                         type='integer'),
-    #     'draft_sale_order_count': fields.function(_draft_sale_order_count,
-    #                                         string='# of Draft Sales Order',
-    #                                         type='integer'),
-    #     'sale_order_ids': fields.one2many('sale.order','partner_id',
-    #                                       'Sales Order')
-    # }
+    @api.multi
+    def action_open_task_calendar_week(self):
+        self.ensure_one()
+        context = self._context.copy()
+        context.update({
+            'search_default_partner_id': self.partner_id.id,
+            'default_partner_id': self.partner_id.id,
+        })
+        view_calendar_id = self.env.ref('project.view_task_calendar')
+        view_tree_id = self.env.ref('project.view_task_tree2')
+        view_form_id = self.env.ref('project.view_task_form2')
+        return {
+            'name': _('Tasks'),
+            'type': 'ir.actions.act_window',
+            'res_model': 'project.task',
+            'view_mode': 'calendar',
+            'view_type': 'calendar',
+            'views': [(view_calendar_id.id, 'calendar'),
+                      (view_tree_id.id, 'tree'),
+                      (view_form_id.id, 'form'),
+                      ],
+            'view_id': False,
+            'context': context,
+        }

fbd5d43d6e6f5971d79ec45a8a78cc9d37ce2607
sergio <sergiocorato@gmail.com>
2018-08-11 15:19:34 +0200

    index on 10.0: fff4a91 [IMP] port to 10.0 project stage state date

---
+++ project_task_partner_shipping/models/task.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import fields, api, models, _
+
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    partner_shipping_id = fields.Many2one(
+        'res.partner', 'Delivery Address',
+        domain="[('parent_id', '=', partner_id)]",
+        help="Delivery address for current task.")

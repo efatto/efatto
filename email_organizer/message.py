40f33b5eebcfbb86e8299925994f350c4d0efe74
sergio <sergiocorato@gmail.com>
2018-08-17 08:54:49 +0200

    [REM]

--- email_organizer/message.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp import models, fields
-
-
-class mail_message(models.Model):
-    _inherit = 'mail.message'
-
-    name = fields.Char('Name')
--- email_organizer/model.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp import models, fields
-
-
-class ir_model(models.Model):
-    _inherit = 'ir.model'
-
-    email_organizer = fields.Boolean('Use in Email Organizer')

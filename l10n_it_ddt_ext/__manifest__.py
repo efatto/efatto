e13407971f0edf6441866ec9142f4a6c4f5a1e51
sergio <sergiocorato@gmail.com>
2018-08-27 18:36:24 +0200

    [9.0] ddt ext

--- l10n_it_ddt_ext/__manifest__.py
+++ l10n_it_ddt_ext/__manifest__.py
@@ -19,7 +19,7 @@
 #
 {
     'name': 'DDT fixes',
-    'version': '10.0.1.0.0',
+    'version': '9.0.1.0.0',
     'category': 'other',
     'author': 'Sergio Corato',
     'website': 'https://efatto.it',

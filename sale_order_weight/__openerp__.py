b7c63d3a91f7adacbebc0cca4ef63f60c11b3b41
sergio <sergiocorato@gmail.com>
2018-08-31 00:02:40 +0200

    [9.0] port sale order weight

--- sale_order_weight/__openerp__.py
+++ sale_order_weight/__openerp__.py
@@ -19,7 +19,7 @@
 #
 {
     'name': 'Sale order weight',
-    'version': '10.0.1.0.0',
+    'version': '9.0.1.0.0',
     'category': 'other',
     'author': 'Sergio Corato',
     'description': '''
--- sale_order_weight/models/sale_order.py
+++ sale_order_weight/models/sale_order.py
@@ -2,8 +2,8 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-from odoo import models, api, fields
-import odoo.addons.decimal_precision as dp
+from openerp import models, api, fields
+import openerp.addons.decimal_precision as dp
 
 
 class SaleOrder(models.Model):

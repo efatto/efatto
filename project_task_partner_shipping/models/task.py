507fc48e7dab19725da64603adedc5ce6f9370b2
sergio <sergiocorato@gmail.com>
2018-08-30 23:30:05 +0200

    [9.0] port project task partner shipping

--- project_task_partner_shipping/models/task.py
+++ project_task_partner_shipping/models/task.py
@@ -2,7 +2,7 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-from odoo import fields, models, _
+from openerp import fields, models, _
 
 
 class ProjectTask(models.Model):

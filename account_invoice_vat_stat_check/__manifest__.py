fa1551678856638591d11d8d8f7b170f1b398576
sergiocorato <sergiocorato@gmail.com>
2019-03-21 00:07:53 +0100

    [10.0] port

---
+++ account_invoice_vat_stat_check/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2019 Sergio Corato
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account invoice check VAT statement",
+    "version": "10.0.1.0.0",
+    "category": "Accounting",
+    "description": """
+Check if exists a closed VAT statement for period used to register the invoice:
+if true, raise an error.
+""",
+    "author": "Sergio Corato",
+    "website": "https://efatto.it",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        "account_vat_period_end_statement",
+    ],
+    "installable": True
+}
--- account_invoice_vat_stat_check/models/account_invoice.py
+++ account_invoice_vat_stat_check/models/account_invoice.py
@@ -1,46 +1,46 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from openerp import models, api, _
-from openerp.exceptions import Warning as UserError
+
+from odoo import models, api, _
+from odoo.exceptions import UserError
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    # @api.multi
-    # def action_cancel(self):
-    #     # TODO in many cases it is done to correct minor problems,
-    #     # give the user the choice
-    #     for obj_inv in self:
-    #         period = obj_inv.period_id
-    #         vat_statement = self.env[
-    #             'account.vat.period.end.statement'].search(
-    #                 [('period_ids', 'in', period.id)])
-    #         if vat_statement:
-    #             if vat_statement[0].state != 'draft':
-    #                 raise UserError(
-    #                     _('Period %s have already a closed vat statement. '
-    #                       'Do not remove the invoice.')
-    #                     % period.name
-    #                 )
-    #     return super(AccountInvoice, self).action_cancel()
-
-    @api.multi
-    def action_number(self):
-        # TODO: this method is not invoked when the invoice is approved
-        # without changes
-        for obj_inv in self:
-            period = obj_inv.period_id
+    def _check_invoice_reference(self):
+        for invoice in self:
+            date_range = self.env['date.range'].search([
+                ('date_start', '<=', invoice.date_invoice),
+                ('date_stop', '>=', invoice.date_invoice),
+            ])
             vat_statement = self.env[
-                    'account.vat.period.end.statement'].search(
-                    [('period_ids', 'in', period.id)])
+                'account.vat.period.end.statement'].search(
+                    [('date_range_ids', 'in', date_range.id)])
             if vat_statement:
                 if vat_statement[0].state != 'draft':
                     raise UserError(
                         _('Period %s have already a closed vat statement. '
                           'Set registration date to a greater period.')
-                        % period.name
+                           % date_range.name
                     )
-        return super(AccountInvoice, self).action_number()
+        return super(AccountInvoice, self)._check_invoice_reference()
+
+    @api.multi
+    def action_cancel(self):
+        for invoice in self:
+            date_range = self.env['date.range'].search([
+                ('date_start', '<=', invoice.date_invoice),
+                ('date_stop', '>=', invoice.date_invoice),
+            ])
+            vat_statement = self.env[
+                'account.vat.period.end.statement'].search(
+                [('date_range_ids', 'in', date_range.id)])
+            if vat_statement:
+                if vat_statement[0].state != 'draft':
+                    raise UserError(
+                        _('Period %s have already a closed vat statement. '
+                          'Reset to draft vat statement to cancel this '
+                          'invoice.')
+                          % date_range.name
+                    )
+        return super(AccountInvoice, self).action_cancel()

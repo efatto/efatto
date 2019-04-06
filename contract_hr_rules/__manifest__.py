607a45056a014488baa3d5a725561b6bc065f234
sergiocorato <sergiocorato@gmail.com>
2019-04-06 11:29:49 +0200

    [IMP] add rules custom

---
+++ contract_hr_rules/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Sergio Corato
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Contract and hr rules custom',
+    'version': '10.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'description': 'Contract and hr rules custom',
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'contract',
+    ],
+    'data': [
+        'security/contract_security.xml',
+    ],
+    'installable': False
+}

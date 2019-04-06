3a71da1af3e2e3c5aa4d145895d3bd8d0215823d
sergiocorato <sergiocorato@gmail.com>
2019-04-06 17:03:44 +0200

    index on 9.0: 5fc7d61a [9.0] use v. 10

---
+++ account_journal_sequence/models/account_journal.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, fields, api, _
+
+
+class AccountJournal(models.Model):
+    _inherit = 'account.journal'
+    _order = 'sequence'
+
+    sequence = fields.Integer(
+        string='Sequence',
+        default=10,
+        help='Sequence used to order account journal')

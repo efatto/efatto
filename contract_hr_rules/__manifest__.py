38641e6285e89d5a851b2caf8fdbec15e7de0132
sergiocorato <sergiocorato@gmail.com>
2019-04-10 17:29:48 +0200

    [FIX] model missing

--- contract_hr_rules/__manifest__.py
+++ contract_hr_rules/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Contract and hr rules custom',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'other',
     'author': 'Sergio Corato',
     'description': 'Contract and hr rules custom',

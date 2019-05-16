8f3186874a3ee4bf48fb660fc2f66ec23021f980
sergiocorato <sergiocorato@gmail.com>
2019-05-16 19:16:02 +0200

    [IMP] type

--- l10n_it_fatturapa_share/controllers/main.py
+++ l10n_it_fatturapa_share/controllers/main.py
@@ -5,14 +5,14 @@ from openerp.http import Controller, route, request, Response
 
 class FatturaPAAttachmentDownload(Controller):
 
-    @route('/download/<model>', type='https', auth='user', methods=['GET'])
+    @route('/download/<model>', type='http', auth='user', methods=['GET'])
     def index(self, model=None, **kwargs):
         if model == 'out':
             fa_model = 'fatturapa.attachment.out'
         elif model == 'in':
             fa_model = 'fatturapa.attachment.in'
         else:
-            return '<h1>Only out or in accepted</h1>'
+            return '<h1>Only type \'out\' or \'in\' are accepted</h1>'
         FatturaPAAttachment = request.env[fa_model]
         atts = FatturaPAAttachment.search([])
         return request.render('l10n_it_fatturapa_share.index', {'atts': atts})

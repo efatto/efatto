ebe8d03b7e19fd237f44aa640351b662e461b411
sergiocorato <sergiocorato@gmail.com>
2019-05-21 11:36:13 +0200

    [IMP] compute hours version

--- account_analytic_sal/__manifest__.py
+++ account_analytic_sal/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Account analytic SAL',
-    'version': '10.0.1.0.6',
+    'version': '10.0.1.0.7',
     'category': 'Extra Tools',
     'description':
         'Account analytic SAL',

2c08cb3003be133831f693421e691eff59ca74ea
sergiocorato <sergiocorato@gmail.com>
2019-05-21 17:34:29 +0200

    [IMP] hide contacts menu for users

--- account_analytic_sal/__manifest__.py
+++ account_analytic_sal/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Account analytic SAL',
-    'version': '10.0.1.0.8',
+    'version': '10.0.1.0.9',
     'category': 'Extra Tools',
     'description':
         'Account analytic SAL',

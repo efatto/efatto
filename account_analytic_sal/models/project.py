2ae798f7d69dd4e85153d493674144c715152abd
sergiocorato <sergiocorato@gmail.com>
2019-05-21 17:00:46 +0200

    [FIX] set hours planned visible only to manager

--- account_analytic_sal/models/project.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-
-class Task(models.Model):
-    _inherit = 'project.task'
-
-    planned_hours = fields.Float(groups='project.group_project_manager')

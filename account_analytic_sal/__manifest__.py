ba36aa3d5c19fb41f941eaef86b925a30627679b
sergiocorato <sergiocorato@gmail.com>
2019-05-24 15:03:43 +0200

    [IMP] move last date invoice and translation

--- account_analytic_sal/__manifest__.py
+++ account_analytic_sal/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Account analytic SAL',
-    'version': '10.0.1.0.11',
+    'version': '10.0.1.0.12',
     'category': 'Extra Tools',
     'description':
         'Account analytic SAL',

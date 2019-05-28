9daea03f8d9e45228e3125ef2cf75a106a03805f
sergiocorato <sergiocorato@gmail.com>
2019-05-28 17:20:51 +0200

    [IMP] add single tag for account analytic line and improve time view in contract

--- account_analytic_sal/models/account.py
+++ account_analytic_sal/models/account.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, exceptions
+from odoo import models, fields, api, exceptions, _
 import odoo.addons.decimal_precision as dp
 
 
@@ -135,6 +135,43 @@ class AccountAnalyticAccount(models.Model):
     manager_id = fields.Many2one(
         'res.users', 'Account Manager', track_visibility='onchange')
     date_end = fields.Date('End Date')
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    tag_id = fields.Many2one('account.analytic.tag', string="Tag", copy=True)
+
+    @api.model
+    def create(self, values):
+        # todo add tag from creator model?
+        #  create tag, if missing, for creator model
+        #  then assign it to line
+        if self._context.get('invoice') and self._context.get('type'):
+            if self._context['type'] in ['out_invoice', 'out_refund']:
+                tag_id = self._get_tag(_('Sales'))
+            else:
+                tag_id = self._get_tag(_('Purchases'))
+        else:
+            tag_id = self._get_tag(_('Timesheets'))
+        # tag_id = self._get_tags('Materials')
+        # tag_id = self._get_tags('Generic')
+        values.update({
+            'tag_id': tag_id.id
+        })
+        res = super(AccountAnalyticLine, self).create(values)
+        return res
+
+    @api.multi
+    def _get_tag(self, name):
+        tag_id = self.env['account.analytic.tag'].search([
+            ('name', '=', name)
+        ], limit=1)
+        if not tag_id:
+            tag_id = self.env['account.analytic.tag'].create({
+                'name': name
+            })
+        return tag_id
 
 
 class AccountInvoiceLine(models.Model):

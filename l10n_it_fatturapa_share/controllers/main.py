1a56ed59131a6c5af098557b6739a5db15421fee
sergiocorato <sergiocorato@gmail.com>
2019-05-30 15:55:28 +0200

    [FIX] controller

--- l10n_it_fatturapa_share/controllers/main.py
+++ l10n_it_fatturapa_share/controllers/main.py
@@ -1,18 +1,58 @@
 # -*- coding: utf-8 -*-
+import json
+import base64
+import werkzeug
 
-from openerp.http import Controller, route, request, Response
+from openerp.http import Controller, route, request, Response,\
+    serialize_exception as _serialize_exception
+from openerp.tools import html_escape
 
 
 class FatturaPAAttachmentDownload(Controller):
 
     @route('/download/<model>', type='http', auth='user', methods=['GET'])
     def index(self, model=None, **kwargs):
-        if model == 'out':
-            fa_model = 'fatturapa.attachment.out'
-        elif model == 'in':
-            fa_model = 'fatturapa.attachment.in'
-        else:
+        res = {}
+        if model not in ['fatturapa.attachment.out',
+                         'fatturapa.attachment.in']:
             return '<h1>Only type \'out\' or \'in\' are accepted</h1>'
-        FatturaPAAttachment = request.env[fa_model]
-        atts = FatturaPAAttachment.search([])
-        return request.render('l10n_it_fatturapa_share.index', {'atts': atts})
+        atts = request.env[model].search([])
+        config_obj = request.env['ir.config_parameter'].get_param(
+            'web.base.url')
+        attachment_url = config_obj + "/web/attachments/token/"
+        for att_obj in atts:
+            if att_obj.access_token:
+                att_link = attachment_url + att_obj.access_token
+                res.update({
+                    att_obj.name: att_link})
+        return request.render('l10n_it_fatturapa_share.index', {'atts': res})
+
+    @route('/web/attachments/token/<string:token>', type='http', auth="none")
+    def get_attachments(self, token, **kwargs):
+        try:
+            attachment_ids = request.env['ir.attachment'].sudo().search(
+                [('access_token', '=', token)])
+            if attachment_ids:
+                for attachment_obj in attachment_ids:
+                    file_content = base64.b64decode(attachment_obj.datas)
+                    disposition = 'attachment; filename=%s' % werkzeug.urls.\
+                        url_quote(attachment_obj.datas_fname)
+                    return request.make_response(
+                        file_content,
+                        [('Content-Type', attachment_obj.mimetype),
+                         ('Content-Length', len(file_content)),
+                         ('Content-Disposition', disposition)])
+            else:
+                error = {
+                    'code': 200,
+                    'message': "Unable to find the attachments",
+                }
+                return request.make_response(html_escape(json.dumps(error)))
+
+        except Exception as e:
+            se = _serialize_exception(e)
+            error = {
+                'code': 200,
+                'message': "Odoo Server Error %s " % se,
+            }
+            return request.make_response(html_escape(json.dumps(error)))
--- l10n_it_fatturapa_share/models/ir_attachment.py
+++ l10n_it_fatturapa_share/models/ir_attachment.py
@@ -2,36 +2,35 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2019 Sergio Corato
+#    Copyright (C) 2018-BroadTech IT Solutions
+#    (<http://www.broadtech-innovations.com/>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    License, or (at your option) any later version
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU Affero General Public License for more details
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
 ##############################################################################
-{
-    'name': 'Download e-invoices',
-    'version': '8.0.1.0.0',
-    'category': 'Extra Tools',
-    'description':
-        'Download e-invoice via https',
-    'author': 'Sergio Corato',
-    'website': 'https://efatto.it',
-    'depends': [
-        'web',
-    ],
-    'data': [
-        'views/ir_attachment_view.xml',
-        'views/template.xml',
-    ],
-    'installable': True,
-}
+
+import uuid
+
+from openerp import api, fields, models
+
+
+class Attachment(models.Model):
+    _inherit = "ir.attachment"
+
+    access_token = fields.Char('Token', readonly=True)
+
+    @api.model
+    def create(self, vals):
+        res = super(Attachment, self).create(vals)
+        res.access_token = uuid.uuid4()
+        return res

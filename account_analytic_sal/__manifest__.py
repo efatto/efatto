3b7c96c61480eca470e80f85e6baae64a4bf987a
sergiocorato <sergiocorato@gmail.com>
2019-06-03 19:27:46 +0200

    [IMP] rename fields and add sale

--- account_analytic_sal/__manifest__.py
+++ account_analytic_sal/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Account analytic SAL',
-    'version': '10.0.1.0.18',
+    'version': '10.0.1.0.19',
     'category': 'Extra Tools',
     'description':
         'Account analytic SAL',

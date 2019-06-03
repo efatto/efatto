d537534ff2969449cb9633e7b4045eee84ddb788
sergiocorato <sergiocorato@gmail.com>
2019-06-03 12:04:34 +0200

    [IMP] change view contracts

--- account_analytic_sal/__manifest__.py
+++ account_analytic_sal/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Account analytic SAL',
-    'version': '10.0.1.0.15',
+    'version': '10.0.1.0.16',
     'category': 'Extra Tools',
     'description':
         'Account analytic SAL',

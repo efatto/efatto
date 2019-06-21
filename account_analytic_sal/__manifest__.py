b49562044f4f7f6717083a8045b49de8793d0672
sergiocorato <sergiocorato@gmail.com>
2019-06-21 15:31:23 +0200

    [FIX] revert contacts view remove with replace

--- account_analytic_sal/__manifest__.py
+++ account_analytic_sal/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Account analytic SAL',
-    'version': '10.0.1.0.21',
+    'version': '10.0.1.0.22',
     'category': 'Extra Tools',
     'description':
         'Account analytic SAL',

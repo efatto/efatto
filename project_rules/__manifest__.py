844bf27034c569bdf1b02cc94d15a6982570f01d
sergiocorato <sergiocorato@gmail.com>
2019-07-08 12:43:08 +0200

    [IMP] add rules for project manager to modify-create-delete project task

--- project_rules/__manifest__.py
+++ project_rules/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Project rules limit to followers and members',
-    'version': '10.0.1.2.0',
+    'version': '10.0.1.3.0',
     'category': 'other',
     'author': 'Sergio Corato',
     'description': 'Project rules limit to followers and members',

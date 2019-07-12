b53b5c8536e42b09377ac836792bae6b1a466fd8
sergiocorato <sergiocorato@gmail.com>
2019-07-12 15:15:03 +0200

    [FIX] add helps

--- account_analytic_sal/__manifest__.py
+++ account_analytic_sal/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Account analytic SAL',
-    'version': '10.0.1.0.26',
+    'version': '10.0.1.0.27',
     'category': 'Extra Tools',
     'description':
         'Account analytic SAL',

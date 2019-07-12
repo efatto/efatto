8d63ed315158cb427f6de938a4150cc30e73ffc1
sergiocorato <sergiocorato@gmail.com>
2019-07-12 12:51:06 +0200

    [FIX] set readonly to fields

--- sale_order_product_policy/__manifest__.py
+++ sale_order_product_policy/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Sale order product policy',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'Extra Tools',
     'description':
         'Show product invoice policy in sale order line',
--- sale_order_product_policy/models/sale.py
+++ sale_order_product_policy/models/sale.py
@@ -7,6 +7,6 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     product_invoice_policy = fields.Selection(
-        related='product_id.invoice_policy')
+        related='product_id.invoice_policy', readonly=True)
     product_track_service = fields.Selection(
-        related='product_id.track_service')
+        related='product_id.track_service', readonly=True)

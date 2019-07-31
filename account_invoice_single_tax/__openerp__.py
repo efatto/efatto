bfc02a562541c62182d65480d801d4503979262f
sergiocorato <sergiocorato@gmail.com>
2019-07-31 19:16:01 +0200

    [IMP] add module to block invoice with multiple taxes

---
+++ account_invoice_single_tax/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Sergio Corato
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    'name': "Single tax restrict in invoice",
+    'version': "8.0.1.0.0",
+    "author": "Sergio Corato",
+    'website': "https://efatto.it",
+    'category': "Localisation / Accounting",
+    'license': "LGPL-3",
+    'depends': ["account"],
+    'data': [
+    ],
+    'installable': True,
+}

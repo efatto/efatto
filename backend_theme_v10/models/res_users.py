20d219ac6523e2b57e648d215cc4c5a7341df244
sergiocorato <sergiocorato@gmail.com>
2019-08-13 10:15:19 +0200

    [FIX] pep8

--- backend_theme_v10/models/res_users.py
+++ backend_theme_v10/models/res_users.py
@@ -4,8 +4,8 @@
 
 from odoo import models, fields
 
-class ResUsers(models.Model):
 
+class ResUsers(models.Model):
     _inherit = 'res.users'
 
     sidebar_visible = fields.Boolean("Show App Sidebar", default=True)

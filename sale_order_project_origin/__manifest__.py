704bc6645d34ccca553c98f8713d8dc9c1d62eb5
sergiocorato <sergiocorato@gmail.com>
2019-08-13 10:29:27 +0200

    [FIX] import

--- sale_order_project_origin/__manifest__.py
+++ sale_order_project_origin/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Project search by origin in sale order',
-    'version': '10.0.1.0.1',
+    'version': '10.0.1.0.2',
     'category': 'Extra Tools',
     'description':
         'Project search by origin in sale order. Origin is automatically '

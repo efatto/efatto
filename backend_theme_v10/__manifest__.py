747f24044d3991e08841dd0f0568d9fde45ad983
sergiocorato <sergiocorato@gmail.com>
2019-08-13 10:14:38 +0200

    [FIX] manifest

--- backend_theme_v10/__manifest__.py
+++ backend_theme_v10/__manifest__.py
@@ -8,17 +8,15 @@
     "version": "10.0.1.0.23",
     "category": "Themes/Backend",
     "website": "http://www.openworx.nl",
-	"description": """
-		Backend theme for Odoo 10.0 community edition.
-    """,
-	'images':[
+    "description": "Backend theme for Odoo 10.0 community edition.",
+    "images": [
         'images/screen.png'
-	],
+    ],
     "author": "Openworx",
     "license": "LGPL-3",
     "installable": True,
     "depends": [
-	'web_responsive',
+        'web_responsive',
     ],
     "data": [
         'views/assets.xml',

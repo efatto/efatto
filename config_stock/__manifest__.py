7c54ca653c1c2d0ad537531e3dbbd2680fe58ff5
sergiocorato <sergiocorato@gmail.com>
2019-09-26 10:53:54 +0200

    [PORT] config stock

--- config_stock/__manifest__.py
+++ config_stock/__manifest__.py
@@ -1,43 +1,28 @@
-# -*- coding: utf-8 -*-
-#
-#
-#    Copyright (C) 2016-2018 Sergio Corato
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# Copyright 2019 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Module to configure stock',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'other',
     'description': """
 Module to configure stock.
 Add the next groups to base user:
-- inventory valuation;
-- stock packaging;
-- locations. NO MORE EXISTS
+* stock.group_lot_on_delivery_slip
+* stock.group_stock_multi_locations
+* stock.group_stock_production_lot
+* stock.group_stock_adv_location
+* uom.group_uom
+* product.group_stock_packaging
 It also set invoice policy default to delivery.
 """,
     'author': 'Sergio Corato',
     'website': 'https://efatto.it',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     "depends": [
         'sale',
-        'stock_account',
     ],
     "data": [
         'data/group.xml',
     ],
-    'installable': False
+    'installable': True
 }

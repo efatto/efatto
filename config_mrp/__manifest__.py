75a6e698e70c8f532a26b0e4c467218224c6d8de
sergiocorato <sergiocorato@gmail.com>
2019-09-26 11:02:53 +0200

    [IMP] config mrp

---
+++ config_mrp/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'Module to configure mrp',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'description': """
+Module to configure stock.
+Add the next groups to base user:
+- mrp.group_mrp_routings
+""",
+    'author': 'Sergio Corato',
+    'website': 'https://efatto.it',
+    'license': 'LGPL-3',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+        'data/group.xml',
+    ],
+    'installable': False
+}

4569d8ac37f060204629ebee416703a82cf2ad99
sergiocorato <sergiocorato@gmail.com>
2019-09-27 16:55:42 +0200

    [FIX] remove lot

--- config_stock/__manifest__.py
+++ config_stock/__manifest__.py
@@ -2,14 +2,12 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Module to configure stock',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'other',
     'description': """
 Module to configure stock.
 Add the next groups to base user:
-* stock.group_lot_on_delivery_slip
 * stock.group_stock_multi_locations
-* stock.group_stock_production_lot
 * stock.group_stock_adv_location
 * uom.group_uom
 * product.group_stock_packaging

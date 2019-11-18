d535cf0eb70f831260edc70d557ac01a2dab6749
sergiocorato <sergiocorato@gmail.com>
2019-11-18 17:49:19 +0100

    [IMP] add module for reverse charge data

---
+++ l10n_it_reverse_charge_data/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# Copyright 2019 Sergio Corato <https://github.com/sergiocorato>
+
+{
+    'name': 'ITA - Reverse charge data',
+    'version': '10.0.1.0.0',
+    'category': 'Account',
+    'description': 'Add reverse charge data',
+    'author': 'Sergio Corato <info@efatto.it>',
+    'website': 'https://efatto.it',
+    'license': 'LGPL-3',
+    'depends': [
+        'l10n_it_reverse_charge',
+    ],
+    'data': [
+        'data/account_data.xml',
+    ],
+    'installable': True,
+}

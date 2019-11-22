2e675189905b38a029ade9c2ac6d956e31eb5711
sergiocorato <sergiocorato@gmail.com>
2019-11-22 09:52:10 +0100

    [FIX] check invoice single tax line

--- account_invoice_single_tax/__openerp__.py
+++ account_invoice_single_tax/__openerp__.py
@@ -3,8 +3,9 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     'name': "Single tax restrict in invoice",
-    'version': "8.0.1.0.0",
+    'version': "8.0.1.0.1",
     "author": "Sergio Corato",
+    'description': "Forbid multiple taxes in invoice line",
     'website': "https://efatto.it",
     'category': "Localisation / Accounting",
     'license': "LGPL-3",

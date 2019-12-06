8679510f847f736f893f9450033275f8bfd6f580
sergiocorato <sergiocorato@gmail.com>
2019-12-06 10:44:39 +0100

    [IMP] add reminder project

---
+++ reminder_project_deadline/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Reminders and Agenda for Project",
+    'version': '10.0.1.0.0',
+    'author': 'Sergio Corato',
+    'license': 'LGPL-3',
+    'category': 'Reminders and Agenda',
+    'website': 'https://efatto.it',
+    'depends': ['reminder_base', 'project'],
+    'data': [
+        'views/project.xml',
+    ],
+    'installable': True,
+}

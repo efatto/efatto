ae9f06282721a8e552c62fb7095eadaba6291b0f
sergiocorato <sergiocorato@gmail.com>
2020-01-03 08:53:00 +0100

    [PORT] payment data

--- l10n_it_payment_data/__manifest__.py
+++ l10n_it_payment_data/__manifest__.py
@@ -1,25 +1,8 @@
-# -*- coding: utf-8 -*-
-#
-#
-#    Copyright (C) 2015-2019 Sergio Corato
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# Copyright 2015-2020 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Italy common payment data',
-    'version': '10.0.1.0.1',
+    'version': '12.0.1.0.1',
     'category': 'other',
     'description': """
     Italy common payment data
@@ -28,7 +11,7 @@
     'website': 'https://efatto.it',
     'license': 'AGPL-3',
     "depends": [
-        'account_payment_term_month',
+        'account_payment_term_extension',
         'l10n_it_fiscal_payment_term',
     ],
     "data": [

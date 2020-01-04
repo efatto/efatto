f5064d559ab0604c873b4751576251e17fe3c271
sergiocorato <sergiocorato@gmail.com>
2020-01-04 15:21:48 +0100

    [PORT] task color
    [IFX]

--- project_task_state_color/__manifest__.py
+++ project_task_state_color/__manifest__.py
@@ -1,26 +1,8 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2018 Sergio Corato
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2018-2020 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Project task colors',
-    'version': '10.0.1.0.1',
+    'version': '12.0.1.0.0',
     'category': 'Extra Tools',
     'description':
         'Change task color on state.',
@@ -29,7 +11,6 @@
     'depends': [
         'calendar',
         'project',
-        'project_task_partner_shipping',
         'web_widget_color',
     ],
     'data': [

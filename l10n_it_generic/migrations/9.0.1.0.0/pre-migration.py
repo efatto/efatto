625a34060d849cecbd5862d09ed62c5e3b3dff61
sergiocorato <sergiocorato@gmail.com>
2020-01-06 18:20:26 +0100

    [FIX] migrate

--- l10n_it_generic/migrations/9.0.1.0.0/pre-migration.py
+++ l10n_it_generic/migrations/9.0.1.0.0/pre-migration.py
@@ -129,12 +129,9 @@ xmlid_renames = [
 ]
 
 
-def migrate(cr, version):
-    cr.execute("delete from ir_ui_view where arch like '%invoice_type_id%';")
-    cr.execute('delete from ir_ui_view where id = 1260;')
-
-
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
     cr = env.cr
+    cr.execute("delete from ir_ui_view where arch like '%invoice_type_id%';")
+    cr.execute('delete from ir_ui_view where id = 1260;')
     openupgrade.rename_xmlids(cr, xmlid_renames)

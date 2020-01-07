620420ac0a0df1149df9cd4362b34c027c89072e
sergiocorato <sergiocorato@gmail.com>
2020-01-08 00:39:37 +0100

    [FIX][wip] payment data update

---
+++ l10n_it_payment_data/migrations/9.0.1.0.0/post-migration.py
@@ -0,0 +1,9 @@
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    cr = env.cr
+    openupgrade.load_data(
+        cr, 'account', 'migrations/9.0.1.0.0/noupdate_changes.xml',
+    )

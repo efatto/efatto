b5b860156480d612acb84ea3b3a782fee4f0b4f1
sergiocorato <sergiocorato@gmail.com>
2020-01-10 17:34:05 +0100

    [IMP]

--- l10n_it_payment_data/migrations/9.0.1.0.0/post-migration.py
+++
@@ -1,9 +0,0 @@
-from openupgradelib import openupgrade
-
-
-@openupgrade.migrate(use_env=True)
-def migrate(env, version):
-    cr = env.cr
-    openupgrade.load_data(
-        cr, 'l10n_it_payment_data', 'migrations/9.0.1.0.0/noupdate_changes.xml',
-    )

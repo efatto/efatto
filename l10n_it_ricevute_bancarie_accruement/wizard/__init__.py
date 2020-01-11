5bfb28b5775a397f5c4ae5f65bae24f22907fb0a
sergiocorato <sergiocorato@gmail.com>
2020-01-11 18:05:09 +0100

    [CLEAN]

--- l10n_it_ricevute_bancarie_accruement/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from . import wizard_accreditation
-from . import wizard_unsolved
--- l10n_it_ricevute_bancarie_ext/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from . import wizard_riba_file_export
-from . import wizard_riba_issue

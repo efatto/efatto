815b82b6eec95d66e4d9b1ff0e1ecfb5395fead7
sergiocorato <sergiocorato@gmail.com>
2020-01-11 16:21:42 +0100

    [IMP] add history to account name

---
+++ account_history/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Account name history',
+    'version': '8.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'website': 'http://www.efatto.it',
+    'license': 'AGPL-3',
+    'description': 'Account name history data',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account.xml',
+    ],
+    'installable': True,
+}

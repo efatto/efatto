52a9a9e26fe7fe12fce505e3d9455add39923d46
sergiocorato <sergiocorato@gmail.com>
2020-01-13 09:27:54 +0100

    [PORT] hr menu only employee

--- hr_menu_employee_visibility/__manifest__.py
+++ hr_menu_employee_visibility/__manifest__.py
@@ -1,25 +1,8 @@
-# -*- coding: utf-8 -*-
-#
-#
-#    Copyright (C) 2018 Sergio Corato
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# Copyright 2018-2020 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Hide hr menu for not employed user',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'other',
     'author': 'Sergio Corato',
     'description':

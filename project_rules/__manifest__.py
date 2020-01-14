2a73d183accba5434959b6ca3f5f0878edcc67d8
sergiocorato <sergiocorato@gmail.com>
2020-01-14 14:52:37 +0100

    [IMP] desc

--- project_rules/__manifest__.py
+++ project_rules/__manifest__.py
@@ -1,11 +1,12 @@
 # Copyright 2016-2020 Sergio Corato
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Project rules limit to followers and members',
+    'name': 'Project rules limit to followers and members or managed',
     'version': '12.0.1.0.0',
     'category': 'other',
     'author': 'Sergio Corato',
-    'description': 'Project rules limit to followers and members',
+    'description': 'Change project rules: add favourite and set manager as an user, '
+                   'viewing only project task managed or following or favourite.',
     'website': 'https://efatto.it',
     'license': 'LGPL-3',
     'depends': [

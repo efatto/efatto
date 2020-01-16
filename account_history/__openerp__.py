fa6195e60cca44cf630cfea2d5e45758c5aca38e
sergiocorato <sergiocorato@gmail.com>
2020-01-16 15:52:32 +0100

    [FIX] add security

--- account_history/__openerp__.py
+++ account_history/__openerp__.py
@@ -4,16 +4,17 @@
 
 {
     'name': 'Account name history',
-    'version': '8.0.1.0.0',
+    'version': '8.0.1.0.1',
     'category': 'other',
     'author': 'Sergio Corato',
-    'website': 'http://www.efatto.it',
+    'website': 'https://efatto.it',
     'license': 'AGPL-3',
     'description': 'Account name history data',
     'depends': [
         'account',
     ],
     'data': [
+        'security/ir.model.access.csv',
         'views/account.xml',
     ],
     'installable': True,

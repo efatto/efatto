5e3b8630086e889b5fb181953b03f74108128bf0
sergiocorato <sergiocorato@gmail.com>
2020-01-29 23:16:03 +0100

    [IMP] module to hide contacts

---
+++ contacts_menu_visibility/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'Hide contacts menu for basic user',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'description':
+        'Hide contacts menu for basic user.',
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'contacts',
+    ],
+    'data': [
+        'views/contacts.xml',
+    ],
+    'installable': True,
+}

8c12905f827afcc8523a21d878e1d522c9c79700
sergiocorato <sergiocorato@gmail.com>
2020-02-19 11:04:08 +0100

    index on 12.0: 976266e5 [FIX] create/update info

---
+++ l10n_it_ddt_ext/__manifest__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Copyright (C) 2016-2019 Sergio Corato
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{
+    'name': 'DDT various fixes',
+    'version': '10.0.1.0.1',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'description': '''
+This module add:
+* ability to assign ddt type from sale order,
+* ability to assign default create_ddt via company,
+* fix use of fiscal sequence for ddt,
+* limit domain to picking addable to ddt to not already in other ddts,
+* remove package_id from tree view of ddts,
+* add ddt start date,
+* change ddt date type from datetime to date,
+* check ddt number progression on date last ddt emitted on the same sequence
+and fix date when not right,
+* fill ddt data from ddt type when not filled in sale order of partner,
+* block cancel of sale order is ddt and picking linked are in states done or
+in pack.''',
+    'depends': [
+        'stock_picking_package_preparation',
+        'l10n_it_ddt',
+    ],
+    'data': [
+        'views/sale.xml',
+        'views/stock.xml',
+        'views/config.xml',
+    ],
+    'installable': True,
+}
---
+++ l10n_it_ddt_ext/models/sale.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models, api, _, exceptions
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.model
+    def default_get(self, fields):
+        res = super(SaleOrder, self).default_get(fields)
+        if res:
+            company = self.env.user.company_id
+            res['create_ddt'] = company.create_ddt \
+                if company.create_ddt else False
+        return res
+
+    def _default_ddt_type(self):
+        return self.env['stock.ddt.type'].search([], limit=1)
+
+    ddt_type_id = fields.Many2one(
+        'stock.ddt.type',
+        'Type of DDT', default=_default_ddt_type)
+
+    def _preparare_ddt_data(self):
+        res = super(SaleOrder, self)._preparare_ddt_data()
+        ddt_type = self.ddt_type_id
+        if not res.get('carriage_condition_id', False):
+            res['carriage_condition_id'] = \
+                ddt_type.default_carriage_condition_id.id
+        if not res.get('goods_description_id', False):
+            res['goods_description_id'] = \
+                ddt_type.default_goods_description_id.id
+        if not res.get('transportation_reason_id', False):
+            res['transportation_reason_id'] = \
+                ddt_type.default_transportation_reason_id.id
+        if not res.get('transportation_method_id', False):
+            res['transportation_method_id'] = \
+                ddt_type.default_transportation_method_id.id
+        if not res.get('ddt_type_id', False):
+            res['ddt_type_id'] = self.ddt_type_id.id
+        return res
+
+    @api.multi
+    def action_cancel(self):
+        for order in self:
+            for ddt in order.ddt_ids:
+                if ddt.state in ['done', 'in_pack']:
+                    raise exceptions.Warning(_(
+                        'DDT is already done or in pack.'))
+                for picking in ddt.picking_ids:
+                    if picking.state in ['done', 'in_pack']:
+                        raise exceptions.Warning(
+                            _('Picking is already done or in pack.'))
+                # all pickings are cancelled, so remove ddt
+                ddt.unlink()
+                # todo if ddt had a number because it was put in pack and then
+                # todo cancelled, the sequence will have a hole
+        return super(SaleOrder, self).action_cancel()

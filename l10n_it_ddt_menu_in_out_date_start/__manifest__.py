3f5029c3a28854dc96cf5468b11072b8f03ecfda
sergiocorato <sergiocorato@gmail.com>
2020-02-21 17:54:31 +0100

    [IMP] add glue module

---
+++ l10n_it_ddt_menu_in_out_date_start/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2016-2020 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'DDT supplier with date start glue module',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'description': '''This auto-install module add date start in supplier DDT''',
+    'depends': [
+        'l10n_it_ddt_menu_in_out',
+        'l10n_it_ddt_date_start',
+    ],
+    'data': [
+        'views/stock.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}

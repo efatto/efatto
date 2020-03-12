85e971076a20b7dff2dcd373f8a79324fd7ed034
sergiocorato <sergiocorato@gmail.com>
2020-03-12 16:06:02 +0100

    index on 12.0: 714cbc35 [FIX] dependency

---
+++ project_stage_unique/models/project.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from odoo import models
+
+
+class ProjectTaskType(models.Model):
+    _inherit = 'project.task.type'
+
+    _sql_constraints = [(
+        'name_project_task_type_uniq',
+        'unique(name)',
+        'A task type with the same name already exists for this company !'
+        )]

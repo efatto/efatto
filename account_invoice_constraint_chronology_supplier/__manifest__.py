abf68688073f8ce1c0babc8130c1588a7ff5c786
sergiocorato <sergiocorato@gmail.com>
2020-03-13 15:11:52 +0100

    [IMP] port function to check invoice date chronology

---
+++ account_invoice_constraint_chronology_supplier/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2015-2019 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Invoice Constraint Chronology",
+    "version": "12.0.1.0.0",
+    "category": "Accounting",
+    "author": "Odoo Community Association (OCA), ACSONE SA/NV, Sergio Corato",
+    "maintainer": "Sergio Corato",
+    "website": "https://efatto.it",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": ["view/account_view.xml"]
+}
---
+++ account_invoice_constraint_chronology_supplier/model/account_invoice.py
@@ -0,0 +1,66 @@
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+import datetime
+
+from odoo import models, api, fields, _
+from odoo.exceptions import UserError
+from odoo.tools.misc import format_date
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    @api.model
+    def _prepare_later_invoices_domain(self, invoice):
+        domain = [
+            ('journal_id', '=', invoice.journal_id.id),
+            ('date', '>', invoice.date),
+            ('number', '<', invoice.number),
+        ]
+        # check until last date of registration year date_range only
+        registration_fy = self.env['account.fiscal.year'].search([
+            ('date_from', '<=', invoice.date),
+            ('date_to', '>=', invoice.date)
+        ])
+        if registration_fy:
+            domain.append(('date', '<=', registration_fy.date_to))
+        # check on same type if sequence for refund is separate
+        if invoice.journal_id.refund_sequence:
+            domain.append(('type', '=', invoice.type))
+        return domain
+
+    @api.multi
+    def action_move_create(self):
+        previously_validated = self.filtered(lambda inv: inv.move_name)
+        res = super(AccountInvoice, self).action_move_create()
+        for inv in self:
+            if not inv.journal_id.check_chronology:
+                continue
+
+            if inv.type in ('in_invoice', 'in_refund'):
+                if inv.date_invoice and inv.date and inv.date_invoice > inv.date:
+                    raise UserError(
+                        _("Supplier invoice date %s cannot be later than "
+                          "the date %s of registration!" % (
+                            inv.date_invoice, inv.date)
+                          )
+                    )
+            if inv not in previously_validated:
+                invoices = self.search(
+                    self._prepare_later_invoices_domain(inv),
+                    order='date desc', limit=1)
+                if invoices:
+                    date_invoice_format = datetime.datetime(
+                        year=invoices[0].date.year,
+                        month=invoices[0].date.month,
+                        day=invoices[0].date.day,
+                    )
+                    date_invoice_tz = format_date(
+                        self.env, fields.Date.context_today(
+                            self, date_invoice_format))
+                    raise UserError(_(
+                        "Chronology Error. Post the invoice with an equal "
+                        "or greater date than {date_invoice}.").format(
+                        date_invoice=date_invoice_tz))
+        return res

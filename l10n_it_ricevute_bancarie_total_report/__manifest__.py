41936abcfed8fd8c486244bf91ca8ee7449b54c7
sergiocorato <sergiocorato@gmail.com>
2020-03-13 16:43:34 +0100

    [PORT] riba report by totals

---
+++ l10n_it_ricevute_bancarie_total_report/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2017-2020 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'Ricevute bancarie report with total by date',
+    'version': '12.0.1.0.0',
+    'category': 'Accounting',
+    'author': 'Sergio Corato',
+    'website': 'https://efatto.it',
+    'description': 'Ricevute bancarie report with total by date',
+    'license': 'LGPL-3',
+    'depends': [
+        'l10n_it_ricevute_bancarie',
+    ],
+    'data': [
+        'report/distinta_report.xml',
+    ],
+}
---
+++ l10n_it_ricevute_bancarie_total_report/report/distinta_qweb.py
@@ -0,0 +1,27 @@
+from odoo import api, models, fields
+from datetime import datetime
+
+
+class DistintaReportQweb(models.AbstractModel):
+    _inherit = 'report.l10n_it_ricevute_bancarie.distinta_qweb'
+
+    @api.multi
+    def _get_report_values(self, docids, data=None):
+        res = super()._get_report_values(docids=docids, data=data)
+        res.update(get_riba_group=self._get_riba_group(
+            self.env['riba.distinta'].browse(docids)))
+        return res
+
+    @staticmethod
+    def _get_riba_group(objects):
+        res = {}
+        for distinta in objects:
+            for line in distinta.line_ids:
+                line_due_date = line.due_date.strftime('%d/%m/%Y')
+                if line_due_date not in res:
+                    res.update({line_due_date: [line]})
+                else:
+                    res[line_due_date] = res[line_due_date] + [line]
+        dates = sorted([datetime.strptime(ts, "%d/%m/%Y") for ts in res])
+        dates_sorted = [datetime.strftime(ts, "%d/%m/%Y") for ts in dates]
+        return res, dates_sorted

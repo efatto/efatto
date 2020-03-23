c751bb4f99f44a68f0e665df0970c7fbb6c35452
sergiocorato <sergiocorato@gmail.com>
2020-03-23 08:57:59 +0100

    [IMP] remove limit for manager

--- project_rules/__manifest__.py
+++ project_rules/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Project rules limit to followers and members or managed',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'other',
     'author': 'Sergio Corato',
     'description': 'Change project rules: add favourite and set manager as an user, '

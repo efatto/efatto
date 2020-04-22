64578b6a32e2a2c08bbf0b933466cf3503569f6f
sergiocorato <sergiocorato@gmail.com>
2020-04-22 12:47:10 +0200

    [IMP] remove py part

---
+++ l10n_it_ddt_force_number/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    'name': "Force DDT Number",
+    'version': '12.0.1.0.0',
+    'category': 'Stock',
+    'summary': "Allows to force number on specific DDT",
+    'description': """
+This module allows to force the ddt numbering.
+It make visible the ddt_number field. If user fills that field, the typed
+value will be used as ddt number.
+Otherwise, the next sequence number will be retrieved and saved.
+So, the new field has to be used when user doesn't want to use the default
+ddt numbering for a specific ddt.
+    """,
+    'author': "Sergio Corato",
+    'website': 'http://www.efatto.it',
+    'license': 'LGPL-3',
+    'depends': [
+        'l10n_it_ddt',
+        'l10n_it_ddt_menu_in_out',
+    ],
+    'data': [
+        'security/security.xml',
+        'views/stock.xml'
+    ],
+    'installable': True,
+}
--- l10n_it_ddt_force_number/models/stock.py
+++ l10n_it_ddt_force_number/models/stock.py
@@ -1,16 +1,12 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from openerp import models, fields
+from odoo import models, fields
 
 
 class StockPickingPackagePreparation(models.Model):
     _inherit = "stock.picking.package.preparation"
 
-    ddt_number = fields.Char(states={'draft': [('readonly', False)]})
-    date = fields.Date(
-        states={'draft': [('readonly', False)],
-                'done': [('readonly', False)],
-                'in_pack': [('readonly', False)],
-                'cancel': [('readonly', True)]})
+    # ddt_number = fields.Char(states={'draft': [('readonly', False)]}) solo su xml
+    # date = fields.Date(
+    #     states={'draft': [('readonly', False)],
+    #             'done': [('readonly', False)],
+    #             'in_pack': [('readonly', False)],
+    #             'cancel': [('readonly', True)]})

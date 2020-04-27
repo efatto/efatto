88a7624e4530993e817eec7450f7c354d1212a2e
sergiocorato <sergiocorato@gmail.com>
2020-04-27 14:39:28 +0200

    [IMP] remove button

--- stock_picking_force_assign/__manifest__.py
+++ stock_picking_force_assign/__manifest__.py
@@ -2,21 +2,18 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'Stock picking force assign',
+    'name': 'Stock picking completed assign',
     'version': '12.0.1.0.0',
     'category': 'other',
     'author': 'Sergio Corato',
     'website': 'https://efatto.it',
     'license': 'LGPL-3',
     'description': '''
-This module add a button to force assign of stock moves with manual inserted 
-completed quantity, available only to user with force stock picking group.''',
+This module set assign of stock moves with manual inserted completed quantity.''',
     'depends': [
         'stock',
     ],
     'data': [
-        'security/security.xml',
-        'views/stock.xml',
     ],
     'installable': True,
 }
--- stock_picking_force_assign/models/stock.py
+++ stock_picking_force_assign/models/stock.py
@@ -5,14 +5,6 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     @api.multi
-    def force_assign(self):
-        # to force recompute of picking state even if no one stock move assigned
-        moves = self.mapped('move_ids_without_package')
-        if all(x.product_uom_qty == x.quantity_done for x in moves
-               if x.state == 'confirmed'):
-            moves.write({'state': 'assigned'})
-
-    @api.multi
     def action_assign(self):
         for move in self.mapped('move_ids_without_package'):
             if move.product_uom_qty == move.quantity_done and \

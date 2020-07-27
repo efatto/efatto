85250bdb46a298dcdc902ebc56099588d7928f17
sergiocorato <sergiocorato@gmail.com>
2020-07-27 21:54:14 +0200

    [FIX] desc

--- sale_order_single_tax_required/__openerp__.py
+++ sale_order_single_tax_required/__openerp__.py
@@ -5,6 +5,7 @@
     'name': "Single tax required in sale order lines",
     'version': "8.0.1.0.0",
     "author": "Sergio Corato",
+    'description': "Forbid none or multiple taxes in sale order line at confirmation",
     'website': "https://efatto.it",
     'category': "Localisation / Accounting",
     'license': "LGPL-3",

4bd7d6223f5cc864ed224452ce8ffa903d88305e
sergiocorato <sergiocorato@gmail.com>
2020-08-06 19:08:16 +0200

    [IMP] migration to sale_triple_discount

--- discount_complex/__manifest__.py
+++ discount_complex/__manifest__.py
@@ -2,7 +2,7 @@
 # Copyright 2017-2018 Sergio Corato
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Sale complex discount',
+    'name': 'Sale complex discount - module for migrate data to sale_triple_discount',
     'version': '10.0.1.0.0',
     'category': 'other',
     'description': 'Add multiple discount field, like 50+14.5+5',

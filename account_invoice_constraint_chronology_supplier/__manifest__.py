308b9169f3b30913da90da4b3130bc0bdbca90da
sergiocorato <sergiocorato@gmail.com>
2020-11-13 11:27:28 +0100

    [FIX] (c) and desc

--- account_invoice_constraint_chronology_supplier/__manifest__.py
+++ account_invoice_constraint_chronology_supplier/__manifest__.py
@@ -1,14 +1,14 @@
 # Copyright 2015-2019 ACSONE SA/NV (<http://acsone.eu>)
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Invoice Constraint Chronology",
-    "version": "12.0.1.0.0",
-    "category": "Accounting",
+    "version": "12.0.1.0.1",
     "author": "Odoo Community Association (OCA), ACSONE SA/NV, Sergio Corato",
     "maintainer": "Sergio Corato",
     "website": "https://efatto.it",
     "license": "AGPL-3",
+    "category": "Accounting",
     "depends": ["account"],
-    "data": ["view/account_view.xml"]
+    "data": ["view/account_view.xml"],
 }

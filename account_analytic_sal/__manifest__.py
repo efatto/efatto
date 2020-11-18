0664ba36b0b20373a78be63064ad24669c4791ce
sergiocorato <sergiocorato@gmail.com>
2020-11-18 18:33:33 +0100

    index on 12.0: 472e42cf [FIX] (c) and desc

---
+++ account_analytic_sal/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'Account analytic SAL',
+    'version': '10.0.1.0.41',
+    'category': 'Extra Tools',
+    'description':
+        'Account analytic SAL',
+    'author': 'Sergio Corato',
+    'website': 'https://efatto.it',
+    'depends': [
+        'analytic',
+        'contract_show_invoice',
+        'contract_show_sale',
+        'hr_timesheet',
+        'project',
+        'contacts',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/account.xml',
+        'views/project.xml',
+        'views/sale.xml',
+    ],
+    'installable': True,
+}
---
+++ account_analytic_sal/models/account.py
@@ -0,0 +1,285 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, exceptions, _
+import odoo.addons.decimal_precision as dp
+
+
+class AccountAnalyticAccount(models.Model):
+    _inherit = 'account.analytic.account'
+
+    @api.multi
+    def get_progress(self):
+        for account in self:
+            account.progress_hours = 0.0
+            if account.hours_done and account.hours_planned:
+                account.progress_hours = \
+                    account.hours_done / account.hours_planned * 100
+
+    @api.multi
+    @api.depends('project_ids')
+    def _compute_hours(self):
+        newself = self.sudo()
+        analytic_line_model = self.env['account.analytic.line']
+        project_task_model = self.env['project.task']
+        sale_line_model = newself.env['sale.order.line']
+        uom_time_categ_id = self.env.ref('product.uom_categ_wtime')
+        for analytic in self:
+            qty_ordered = 0.0
+            hours_delivered = 0.0
+            hours_invoiced = 0.0
+            project_fetch_data = project_task_model.read_group(
+                [('project_id', 'in', analytic.sudo().with_context(
+                    active_test=False).project_ids.ids)],
+                ['planned_hours'], [],
+            )
+            hours_planned = project_fetch_data[0]['planned_hours'] or 0.0
+            analytic_fetch_data = analytic_line_model.read_group(
+                [('project_id', 'in', analytic.sudo().with_context(
+                    active_test=False).project_ids.ids)],
+                ['unit_amount'], [],
+            )
+            hours_done = analytic_fetch_data[0]['unit_amount'] or 0.0
+            sale_fetch_data = sale_line_model.read_group(
+                [('order_id.project_id', '=', analytic.id),
+                 ('order_id.state', 'not in', ['draft', 'sent', 'cancel'])],
+                ['product_uom_qty', 'qty_delivered', 'qty_invoiced',
+                 'product_uom'], ['product_uom'],
+            )
+            for d in sale_fetch_data:
+                uom_hour = self.env.ref('product.product_uom_hour')
+                uom_base = self.env['product.uom'].browse(d['product_uom'][0])
+                if d.get('product_uom_qty'):
+                    qty_ordered += uom_base._compute_quantity(
+                        d['product_uom_qty'], uom_hour if uom_base.category_id
+                        == uom_time_categ_id else uom_base)
+                if d.get('qty_delivered'):
+                    hours_delivered += uom_base._compute_quantity(
+                        d['qty_delivered'], uom_hour if uom_base.category_id ==
+                        uom_time_categ_id else uom_base)
+                if d.get('qty_invoiced'):
+                    hours_invoiced += uom_base._compute_quantity(
+                        d['qty_invoiced'], uom_hour if uom_base.category_id ==
+                        uom_time_categ_id else uom_base)
+            analytic.hours_done = hours_done
+            analytic.qty_ordered = qty_ordered
+            analytic.hours_planned = hours_planned
+            analytic.hours_residual = hours_planned - hours_done
+            analytic.hours_delivered = hours_delivered
+            analytic.hours_invoiced = hours_invoiced
+            # FIXME le ore sono da fatturare se non gia chiuso l'ordine
+            #  e se l'ordine va fatturato ad ore (le singole righe a sto punto)
+            analytic.hours_tobe_invoiced = hours_delivered - hours_invoiced
+
+    @api.multi
+    def _get_amount_sal_to_invoice(self):
+        for account in self:
+            account.amount_sal_to_invoice = sum([
+                x.residual_toinvoice for x in account.account_analytic_sal_ids]
+            )
+
+    @api.multi
+    def _get_amount_remaining(self):
+        for account in self:
+            account.amount_remaining = \
+                account.total_sale - account.total_invoiced
+
+    @api.multi
+    def _get_last_invoice_date(self):
+        invoice_model = self.env['account.invoice']
+        for analytic in self:
+            fetch_data = invoice_model.search_read(
+                [('invoice_line_ids.account_analytic_id', '=', analytic.id),
+                 ('invoice_line_ids.invoice_id.date', '!=', False),
+                 ('type', 'in', ['out_invoice', 'out_refund'])],
+                ['date_invoice'], limit=1, order='date_invoice desc',
+            )
+            if fetch_data:
+                analytic.last_invoice_date = fetch_data[0]['date_invoice']
+
+    invoice_line_ids = fields.One2many(
+        string='Invoice lines',
+        comodel_name='account.invoice.line',
+        inverse_name='account_analytic_id')
+    last_invoice_date = fields.Date(
+        string='Last invoice Date', compute=_get_last_invoice_date)
+    use_sal = fields.Boolean(
+        string='Use SAL')
+    amount_remaining = fields.Float(
+        compute='_get_amount_remaining',
+        string='Remaining Revenue',
+        help="Computed using the formula: Sale Amount - Invoiced Amount",
+        digits=dp.get_precision('Account'))
+    qty_ordered = fields.Float(
+        string="Ordered Qty",
+        help="Sum qty ordered as shown in sale order lines",
+        compute='_compute_hours')
+    hours_planned = fields.Float(
+        string="Planned Hours",
+        help="Sum hours planned in tasks",
+        compute='_compute_hours')
+    hours_done = fields.Float(
+        string="Done Hours",
+        help="Sum hours done in timesheets",
+        compute='_compute_hours')
+    hours_delivered = fields.Float(
+        string="Delivered Hours",
+        help="Sum hours delivered as shown in sale order lines",
+        compute='_compute_hours')
+    hours_tobe_invoiced = fields.Float(
+        string="Hours to be Invoiced",
+        compute='_compute_hours')
+    hours_invoiced = fields.Float(
+        string="Hours Invoiced",
+        help="Sum hours invoiced as shown in sale order lines",
+        compute='_compute_hours')
+    hours_residual = fields.Float(
+        string="Residual Hours",
+        help="Hours planned - Hours done",
+        compute='_compute_hours')
+    progress_hours = fields.Float(
+        help='Progress % of Hours done / Hours planned',
+        compute='get_progress')
+    account_analytic_sal_ids = fields.One2many(
+        comodel_name='account.analytic.sal',
+        inverse_name='account_analytic_id',
+        string='Analytic SAL progression')
+    amount_sal_to_invoice = fields.Float(
+        compute=_get_amount_sal_to_invoice,
+        help='Amount to invoice from SAL')
+    manager_id = fields.Many2one(
+        'res.users', 'Account Manager', track_visibility='onchange')
+    date_end = fields.Date('End Date')
+    total_invoiced_form = fields.Float(related='total_invoiced')
+    total_sale_form = fields.Float(related='total_sale')
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    tag_id = fields.Many2one('account.analytic.tag', string="Tag", copy=True)
+
+    @api.model
+    def create(self, values):
+        if self._context.get('invoice'):
+            invoice = self._context['invoice']
+            if invoice.type in ['out_invoice', 'out_refund']:
+                tag_id = self._get_tag(_('Sales'))
+            else:
+                tag_id = self._get_tag(_('Purchases'))
+        else:
+            tag_id = self._get_tag(_('Timesheets'))
+        # tag_id = self._get_tags('Materials')
+        # tag_id = self._get_tags('Generic')
+        values.update({
+            'tag_id': tag_id.id
+        })
+        res = super(AccountAnalyticLine, self).create(values)
+        return res
+
+    @api.multi
+    def _get_tag(self, name):
+        tag_id = self.env['account.analytic.tag'].search([
+            ('name', '=', name)
+        ], limit=1)
+        if not tag_id:
+            tag_id = self.env['account.analytic.tag'].create({
+                'name': name
+            })
+        return tag_id
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    account_analytic_sal_id = fields.Many2one(
+        comodel_name='account.analytic.sal',
+        string='Analytic SAL',
+        help='SAL linked to this line'
+    )
+
+
+class AccountAnalyticSal(models.Model):
+    _name = 'account.analytic.sal'
+    _description = 'Account Analytic SAL'
+    _order = 'id ASC'
+
+    @api.multi
+    @api.depends('account_analytic_id.invoice_line_ids.price_subtotal',
+                 'account_analytic_id.invoice_line_ids.invoice_id.state',
+                 'account_analytic_id.total_sale', 'percent_toinvoice')
+    def compute_invoiced_sal(self):
+        for sal in self:
+            for line in sal.account_analytic_id.invoice_line_ids.filtered(
+                lambda x: x.account_analytic_sal_id == sal
+            ):
+                sal.amount_invoiced += line.price_subtotal
+            sal.amount_toinvoice = \
+                sal.account_analytic_id.total_sale * sal.percent_toinvoice / 100
+            sal.residual_toinvoice = sal.amount_toinvoice - sal.amount_invoiced
+            if sal.amount_invoiced >= sal.amount_toinvoice > 0.0:
+                sal.invoiced = True
+
+    name = fields.Char('SAL name')
+    percent_completion = fields.Float(
+        'SAL percent done',
+        digits=dp.get_precision('Account')
+    )
+    percent_toinvoice = fields.Float(
+        'SAL percent to invoice',
+        digits=dp.get_precision('Account'))
+    amount_toinvoice = fields.Float(
+        'SAL amount to invoice',
+        compute=compute_invoiced_sal,
+        store=True,
+        digits=dp.get_precision('Account'))
+    amount_invoiced = fields.Float(
+        'SAL amount invoiced',
+        compute=compute_invoiced_sal,
+        store=True,
+        digits=dp.get_precision('Account'))
+    residual_toinvoice = fields.Float(
+        'SAL residual to invoice',
+        compute=compute_invoiced_sal,
+        store=True,
+        digits=dp.get_precision('Account'))
+    done = fields.Boolean(
+        string='SAL done',
+        help='SAL is marked done when completion percent is superior'
+             ' of account progress bar. It can be marked even manually.'
+    )
+    invoiced = fields.Boolean(
+        string='SAL invoiced',
+        compute=compute_invoiced_sal,
+        help='SAL is marked invoiced when amount invoice lines with sal '
+             'reference is superior '
+             'of SAL amount. It can be marked even manually.'
+    )
+    account_analytic_id = fields.Many2one(
+        comodel_name='account.analytic.account',
+        string='Analytic account',
+        ondelete='cascade',
+        required=True
+    )
+    action_id = fields.Many2one(
+        comodel_name='ir.actions.server',
+        string='Server Action to do',
+        help='This action will be done on SAL condition if not already done.'
+    )
+    action_res_model_id = fields.Char(
+        string='Related Model for server action done by the event',
+        readonly=True
+    )
+    action_res_id = fields.Integer(
+        string='Related ID for server action done by the event',
+        readonly=True
+    )
+
+    @api.multi
+    @api.constrains('percent_toinvoice')
+    def _check_percent_toinvoice(self):
+        percent_toinvoice_total = 0.0
+        for sal in self.account_analytic_id.account_analytic_sal_ids:
+            percent_toinvoice_total += sal.percent_toinvoice
+        if percent_toinvoice_total > 100.0:
+            raise exceptions.ValidationError(
+                "SAL total % to invoice must be <= 100%")

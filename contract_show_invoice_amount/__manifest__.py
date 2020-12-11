18a099ab6f26762c72a09edafe20e7053c87232a
sergiocorato <sergiocorato@gmail.com>
2020-12-11 09:43:42 +0100

    [UPD] rename module

---
+++ contract_show_invoice_amount/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2015 Angel Moya <angel.moya@domatix.com>
+# Copyright 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Contract Show Invoiced Amount',
+    'summary': 'Show invoiced amount in contract invoiced button',
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Sergio Corato',
+    'website': 'https://efatto.it',
+    'depends': ['account', 'contract'],
+    'category': 'Sales Management',
+    'data': [
+        'views/contract.xml',
+    ],
+    'installable': True,
+}
---
+++ contract_show_invoice_amount/models/contract.py
@@ -0,0 +1,24 @@
+# Copyright 2018 Alberto Martín Cortada - Guadaltech <alberto.martin@guadaltech.es>
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class ContractContract(models.Model):
+    _inherit = 'contract.contract'
+
+    @api.multi
+    def _compute_total_invoiced(self):
+        invoice_line_model = self.env['account.invoice.line']
+        for contract in self:
+            fetch_data = invoice_line_model.read_group(
+                [('contract_line_id.contract_id', '=', contract.id),
+                 ('invoice_id.state', 'in', ['open', 'paid']),
+                 ('invoice_id.type', 'in', ['out_invoice', 'out_refund'])],
+                ['price_subtotal_signed'], [],
+            )
+            contract.total_invoiced = fetch_data[0]['price_subtotal_signed']
+
+    total_invoiced = fields.Float(string="Total Invoiced",
+                                  compute='_compute_total_invoiced')

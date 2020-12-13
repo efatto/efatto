bf3399106675bbfd978f1d3aca19ea6861a1dea6
sergiocorato <sergiocorato@gmail.com>
2020-12-13 15:17:24 +0100

    [PORT] reminder project

--- reminder_project_deadline/__manifest__.py
+++ reminder_project_deadline/__manifest__.py
@@ -1,7 +1,8 @@
-# -*- coding: utf-8 -*-
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': "Reminders and Agenda for Project",
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Sergio Corato',
     'license': 'LGPL-3',
     'category': 'Reminders and Agenda',

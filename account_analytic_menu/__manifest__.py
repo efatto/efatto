4a136002e1aca44debd2d3b0139f4e5ef2c8691c
sergiocorato <sergiocorato@gmail.com>
2020-12-14 18:31:47 +0100

    [IMP] analytic menu

---
+++ account_analytic_menu/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Account analytic menu',
+    'version': '12.0.1.0.0',
+    'category': 'Sale Management',
+    'description':
+        'Account analytic menu',
+    'author': 'Sergio Corato',
+    'website': 'https://efatto.it',
+    'depends': [
+        'analytic',
+    ],
+    'data': [
+        'views/account.xml',
+    ],
+    'installable': True,
+}

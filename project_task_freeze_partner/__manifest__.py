50921a19ae3660cc36465029b22986809f454238
sergiocorato <sergiocorato@gmail.com>
2020-12-14 17:54:27 +0100

    [PORT] project task freeze partner

--- project_task_freeze_partner/__manifest__.py
+++ project_task_freeze_partner/__manifest__.py
@@ -1,25 +1,9 @@
-# -*- coding: utf-8 -*-
-#
-#
-#    Copyright (C) 2018 Sergio Corato
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# Copyright 2018-2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 {
     'name': 'Project task freeze partner',
-    'version': '10.0.1.0.1',
+    'version': '12.0.1.0.0',
     'category': 'other',
     'author': 'Sergio Corato',
     'description': 'Changing project no more change partner to project '

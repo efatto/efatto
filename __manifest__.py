c8d3cc61d36886843d693b6d584a275c295b0010
sergiocorato <sergiocorato@gmail.com>
2020-12-21 08:55:49 +0100

    [FIX] max_commitment_date and recalculate button visible

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Sale calendar state',
     'author': 'Sergio Corato',
     'website': 'https://efatto.it',
-    'version': '12.0.1.0.4',
+    'version': '12.0.1.0.5',
     'summary': 'Add states to sale order based on manufacturing, deliveries and '
                'purchase, used in calendar view.',
     'depends': [

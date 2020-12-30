6184b6bc84857adf1dbe71b6a897230f67bf7396
sergiocorato <sergiocorato@gmail.com>
2020-12-30 11:58:28 +0100

    [FIX] update shipped state without depends

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from . import account
 from . import mail
 from . import mrp
 from . import picking

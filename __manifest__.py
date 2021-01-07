6d895a0798004d84f7119ea328a4988fc4b99b2e
sergiocorato <sergiocorato@gmail.com>
2021-01-07 19:04:35 +0100

    [ADD] sale order auto confirm

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2013 Creativiquadrati snc
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Sale order auto confirm',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato - Efatto.it',
+    'website': 'https://efatto.it',
+    'description': 'Sale order auto confirm',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'data/cron.xml',
+    ],
+    'installable': True
+}

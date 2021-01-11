ac834b42d0ade87d3ff86640092ae972ae3953b3
sergiocorato <sergiocorato@gmail.com>
2021-01-11 15:21:40 +0100

    [FIX] calendar on commitment_date

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Sale calendar state',
     'author': 'Sergio Corato',
     'website': 'https://efatto.it',
-    'version': '12.0.1.0.9',
+    'version': '12.0.1.0.10',
     'summary': 'Add states to sale order based on manufacturing, deliveries and '
                'purchase, used in calendar view.',
     'depends': [

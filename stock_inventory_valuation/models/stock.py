519dc950e5974fc01eed81ce393884bc5b618a7e
sergiocorato <sergiocorato@gmail.com>
2021-03-05 11:51:20 +0100

    [PORT] inventory valuation

--- stock_inventory_valuation/models/stock.py
+++ stock_inventory_valuation/models/stock.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from openerp import fields, models, exceptions, api, _
+# Copyright 2018-2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import fields, models, api, _
 
 
 class StockInventoryLine(models.Model):
@@ -45,7 +43,8 @@ class StockInventory(models.Model):
             elif inv.valuation_type == 'average':
                 for line in inv.line_ids:
                     res = self.price_calculation(line)
-                    price_amount = amount = 0
+                    price_amount = 0
+                    amount = 0
                     for match in res:
                         price_amount += match[1] * match[2]
                         amount += match[1]
@@ -58,7 +57,8 @@ class StockInventory(models.Model):
             else:
                 for line in inv.line_ids:
                     res = self.price_calculation(line)
-                    price_amount = amount = 0
+                    price_amount = 0
+                    amount = 0
                     for match in res:
                         price_amount += match[1] * match[2]
                         amount += match[1]
@@ -76,7 +76,7 @@ class StockInventory(models.Model):
             # search for incoming moves
             move_ids = move_obj.search([
                 ('company_id', '=', self.env.user.company_id.id),
-                ('date', '<=', self.date_inventory or self.date),
+                ('date', '<=', self.accounting_date or self.date),
                 ('state', '=', 'done'),
                 ('product_id', '=', line.product_id.id),
                 ('location_id.usage', '!=', 'internal'),
@@ -86,7 +86,7 @@ class StockInventory(models.Model):
             # search for incoming and outgoing moves
             move_ids = move_obj.search([
                 ('company_id', '=', self.env.user.company_id.id),
-                ('date', '<=', self.date_inventory or self.date),
+                ('date', '<=', self.accounting_date or self.date),
                 ('state', '=', 'done'),
                 ('product_id', '=', line.product_id.id),
                 '|',
@@ -103,26 +103,40 @@ class StockInventory(models.Model):
         # get only move with lot of inventory line
         flag = False
         for move in move_ids:
-            for quant in move.quant_ids.filtered(
+            for ml in move.move_line_ids.filtered(
                 lambda x: x.lot_id == line.prod_lot_id
             ):
                 # Convert to UoM of product each time
-                uom_from = move.product_uom.id
-                qty_from = quant.qty
-                product_qty = self.env['product.uom']._compute_qty(
-                    uom_from, qty_from, move.product_id.uom_id.id)
+                uom_from = move.product_uom
+                qty_from = ml.qty_done
+                product_qty = uom_from._compute_quantity(
+                    qty_from, move.product_id.uom_id)
                 # Get price from purchase line
                 price_unit = 0
-                if move.invoice_line_ids and move.invoice_line_ids[0].\
-                        invoice_id.state in ('open', 'paid'):
-                    # In real life, all move lines related to an 1 invoice line
-                    # should be in the same state and have the same date
-                    inv_line = move.invoice_line_ids[0]
-                    price_unit = inv_line.price_unit * (
-                        1 - (inv_line.discount or 0.0) / 100.0)
-                elif move.purchase_line_id:
-                    # get price from purchase line
-                    price_unit = move.purchase_line_id.price_unit_net
+                if move.purchase_line_id:
+                    if move.purchase_line_id.invoice_lines:
+                        # In real life, all move lines related to an 1 invoice line
+                        # should be in the same state and have the same date
+                        inv_line = move.purchase_line_id.invoice_lines[0]
+                        invoice = inv_line.invoice_id
+                        if inv_line.invoice_id.state in ('open', 'paid'):
+                            price_unit = (
+                                invoice.currency_id._convert(
+                                    inv_line.price_subtotal,
+                                    invoice.company_id.currency_id,
+                                    invoice.company_id,
+                                    invoice.date or fields.Date.today())
+                                / inv_line.quantity)
+                    else:
+                        # get price from purchase line
+                        purchase = move.purchase_line_id.order_id
+                        price_unit = (
+                            purchase.currency_id._convert(
+                                move.purchase_line_id.price_subtotal,
+                                purchase.company_id.currency_id,
+                                purchase.company_id,
+                                purchase.date_order or fields.Date.today()) /
+                            move.purchase_line_id.product_qty)
                 else:
                     # Get price from product, move is an inventory or not link
                     # to a purchase (income move created and even invoiced, but
@@ -131,12 +145,17 @@ class StockInventory(models.Model):
 
                 if self.valuation_type == 'fifo':
                     if qty_to_go - product_qty >= 0:
-                        tuples.append((move.product_id.id, product_qty,
-                                       price_unit, qty_from))
+                        tuples.append((
+                            move.product_id.id,
+                            product_qty,
+                            price_unit,
+                            qty_from))
                         qty_to_go -= product_qty
                     else:
                         tuples.append((
-                            move.product_id.id, qty_to_go, price_unit,
+                            move.product_id.id,
+                            qty_to_go,
+                            price_unit,
                             qty_from * qty_to_go / product_qty))
                         flag = True
                         break
@@ -150,19 +169,26 @@ class StockInventory(models.Model):
                             move.location_dest_id.usage == 'internal':
                         older_qty -= product_qty
                         if qty_to_go > older_qty > 0:
-                            tuples.append((move.product_id.id,
-                                           (qty_to_go - older_qty), price_unit,
-                                           qty_from))
+                            tuples.append((
+                                move.product_id.id,
+                                qty_to_go - older_qty,
+                                price_unit,
+                                qty_from))
                             qty_to_go = older_qty
                         elif qty_to_go > older_qty <= 0:
-                            tuples.append((move.product_id.id, qty_to_go,
-                                           price_unit,
-                                           qty_from * qty_to_go / product_qty))
+                            tuples.append((
+                                move.product_id.id,
+                                qty_to_go,
+                                price_unit,
+                                qty_from * qty_to_go / product_qty))
                             flag = True
                             break
                 elif self.valuation_type == 'average':
-                    tuples.append((move.product_id.id, product_qty,
-                                   price_unit, qty_from))
+                    tuples.append((
+                        move.product_id.id,
+                        product_qty,
+                        price_unit,
+                        qty_from))
             if flag:
                 break
         return tuples

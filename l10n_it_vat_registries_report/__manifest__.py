94fbb1203919428a6dda3e97facc6bf9aaa52e23
sergiocorato <sergiocorato@gmail.com>
2021-03-07 22:14:07 +0100

    [IMP] border in totals

--- l10n_it_vat_registries_report/__manifest__.py
+++ l10n_it_vat_registries_report/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2019-2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Italia vat registries customized report",
+    "name": "Italian vat registries customized report",
     "version": "12.0.1.0.2",
     "category": "Account",
     "author": "Sergio Corato",
     "website": "https://efatto.it",
     "license": "AGPL-3",
-    "description": "Italia customized vat registries report",
+    "description": "Italian customized vat registries report",
     "depends": [
         "l10n_it_vat_registries",
     ],

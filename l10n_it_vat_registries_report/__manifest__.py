f2a0b4332f7a93b6f3280d1e83eb52ce762930cc
sergiocorato <sergiocorato@gmail.com>
2021-03-07 10:57:18 +0100

    [PORT] vat registries report colorized

--- l10n_it_vat_registries_report/__manifest__.py
+++ l10n_it_vat_registries_report/__manifest__.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
-# Copyright 2019 Sergio Corato <https://github.com/sergiocorato>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# Copyright 2019-2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Italia vat registries customized report',
-    'version': '10.0.1.0.1',
-    'category': 'Account',
-    'author': 'Sergio Corato',
-    'website': 'https://efatto.it',
-    'license': 'LGPL-3',
-    'description': 'Italia customized vat registries report',
-    'depends': [
-        'l10n_it_vat_registries',
+    "name": "Italia vat registries customized report",
+    "version": "12.0.1.0.1",
+    "category": "Account",
+    "author": "Sergio Corato",
+    "website": "https://efatto.it",
+    "license": "AGPL-3",
+    "description": "Italia customized vat registries report",
+    "depends": [
+        "l10n_it_vat_registries",
     ],
-    'data': [
-        'views/vat_registries_report.xml',
+    "data": [
+        "views/vat_registries_report.xml",
     ],
-    'installable': True
+    "installable": True
 }

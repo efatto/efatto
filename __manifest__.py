3ebcc21ce1dac30b1dd5d33b5227dbbab366d480
sergiocorato <sergiocorato@gmail.com>
2021-04-08 10:36:34 +0200

    [IMP] desc

--- __manifest__.py
+++ __manifest__.py
@@ -13,7 +13,10 @@
     ],
     'description': "Add bookmarked info to product availability. A quotation can be "
                    "considered bookmarked for the customer if in a specific state "
-                   "or marked manually by the user.",
+                   "or marked manually by the user. "
+                   "Default bookmark is when quotation is in sent state, to remove this"
+                   " default delete the key 'sale.order.bookmark.state' in config "
+                   "parameters and mark manually order with bookmark flag.",
     'data': [
         'data/ir_config_parameter.xml',
         'views/product.xml',

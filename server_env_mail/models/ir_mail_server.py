a0ba083f3d2b38af6d173906a1a8c11996737e42
sergiocorato <sergiocorato@gmail.com>
2021-04-18 16:27:50 +0200

    [IMP] rename server env

---
+++ server_env_mail/models/ir_mail_server.py
@@ -0,0 +1,22 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+from odoo.addons.server_env.models.server import running
+
+
+class IrMailServer(models.Model):
+    _inherit = "ir.mail_server"
+
+    @running
+    @api.model
+    def send_email(
+            self, message, mail_server_id=None, smtp_server=None,
+            smtp_port=None, smtp_user=None, smtp_password=None,
+            smtp_encryption=None, smtp_debug=False, smtp_session=None):
+        return super().send_email(
+            message, mail_server_id=mail_server_id,
+            smtp_server=smtp_server, smtp_port=smtp_port,
+            smtp_user=smtp_user, smtp_password=smtp_password,
+            smtp_encryption=smtp_encryption, smtp_debug=smtp_debug,
+            smtp_session=smtp_session)

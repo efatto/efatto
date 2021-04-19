7a7b58f78f2f46f6bb0fce38c2d5bdb98b2d9ccb
sergiocorato <sergiocorato@gmail.com>
2021-04-19 09:19:01 +0200

    [IMP] rename

--- server_env_google_calendar/__manifest__.py
+++ server_env_google_calendar/__manifest__.py
@@ -1,14 +1,14 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Server Running Env Google',
+    'name': 'Server Env Google Calendar',
     'version': '12.0.1.0.1',
     'category': 'other',
     'author': 'Sergio Corato',
     'website': 'https://efatto.it',
     'license': 'AGPL-3',
     'description': 'Use decorator to block Google calendar sync on server when '
-                   'running_env != prod or stage',
+                   'running_env != prod',
     'depends': [
         'google_calendar',
         'server_env',

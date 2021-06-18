6296219962f86aede32be5320834acd91814a7bf
sergiocorato <sergiocorato@gmail.com>
2021-06-18 09:17:26 +0200

    [IMP] central journal translate and history depend

--- l10n_it_central_journal_history/__manifest__.py
+++ l10n_it_central_journal_history/__manifest__.py
@@ -3,14 +3,14 @@
 
 {
     'name': 'Italian central journal history',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'Accounting & Finance',
     'author': 'Sergio Corato',
     'description': 'Italian Central Journal with history of partner',
     'website': 'https://efatto.it',
     'license': 'AGPL-3',
     'depends': [
-        'l10n_it_central_journal',
+        'l10n_it_central_journal_subtotal',
         'partner_history',
     ],
     'data': [
--- l10n_it_central_journal_subtotal/__manifest__.py
+++ l10n_it_central_journal_subtotal/__manifest__.py
@@ -1,17 +1,16 @@
 # Copyright 2016-2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Italian central journal monospace layout',
+    'name': 'Italian central journal subtotal',
     'version': '12.0.1.0.0',
     'category': 'other',
     'author': 'Sergio Corato',
-    'description': 'Italian Central journal monospace layout',
+    'description': 'Add subtotal to page in central journal report',
     'website': 'https://efatto.it',
     'license': 'AGPL-3',
     'depends': [
         'l10n_it_account',
         'l10n_it_central_journal',
-        'l10n_it_central_journal_history',
     ],
     'data': [
         'report/report_account_central_journal.xml',

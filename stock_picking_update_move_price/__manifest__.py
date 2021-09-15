071b8d2388a89a1fafec319a6fa936412b4b0e07
sergiocorato <sergiocorato@gmail.com>
2021-09-15 10:10:24 +0200

    [ADD] stock picking update move price

---
+++ stock_picking_update_move_price/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Stock picking update move price',
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Sergio Corato',
+    'category': 'other',
+    'depends': [
+        'mrp',
+        'stock',
+    ],
+    'data': [
+        'wizard/update_stock_move_price.xml',
+        'views/stock_picking.xml',
+    ],
+    'summary': "This module add an action on stock move to update stock move price "
+               "unit for evaluation purposes.",
+    'installable': True,
+}

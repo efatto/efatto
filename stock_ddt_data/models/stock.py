d11c8d3e2862302261aeae2af2718dd28efd60ed
sergiocorato <sergiocorato@gmail.com>
2021-09-20 08:54:07 +0200

    [IMP] add models for migration

---
+++ stock_ddt_data/models/stock.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, fields
+
+
+class StockDdtType(models.Model):
+    _inherit = 'stock.ddt.type'
+
+    carriage_condition_id = fields.Many2one(
+        'stock.picking.carriage_condition', 'Carriage Condition')
+    goods_description_id = fields.Many2one(
+        'stock.picking.goods_description', 'Description of Goods')
+    transportation_reason_id = fields.Many2one(
+        'stock.picking.transportation_reason',
+        'Reason for Transportation')
+    transportation_method_id = fields.Many2one(
+        'stock.picking.transportation_method',
+        'Method of Transportation')

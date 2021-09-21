5cd34d696743f11c4e971d2bb5750c77a14ff0d9
sergiocorato <sergiocorato@gmail.com>
2021-09-21 18:30:45 +0200

    [IMP] add link to so, partner so, mo, po

--- stock_move_view_reserved/models/stock.py
+++ stock_move_view_reserved/models/stock.py
@@ -12,3 +12,23 @@ class StockMoveLine(models.Model):
         store=True,
         related='move_id.date_expected'
     )
+    sale_id = fields.Many2one(
+        string='Sale Order',
+        related='move_id.sale_line_id.order_id',
+        store=True,
+    )
+    sale_partner_id = fields.Many2one(
+        string='Sale Partner',
+        related='sale_id.partner_id',
+        store=True,
+    )
+    production_id = fields.Many2one(
+        string='Production',
+        related='move_id.production_id',
+        store=True,
+    )
+    purchase_id = fields.Many2one(
+        string='Purchase Order',
+        related='move_id.purchase_line_id.order_id',
+        store=True,
+    )

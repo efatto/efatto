d53ba751d703c9fbe52053ebc61074db45e123d2
sergiocorato <sergiocorato@gmail.com>
2021-09-22 18:15:55 +0200

    [WIP] remove product sellers limit and add todo to delete older sellers

--- purchase_supplierinfo_update/models/purchase.py
+++ purchase_supplierinfo_update/models/purchase.py
@@ -21,59 +21,63 @@ class PurchaseOrder(models.Model):
             # Do not add a contact as a supplier
             partner = self.partner_id if not self.partner_id.parent_id \
                 else self.partner_id.parent_id
-            if len(line.product_id.seller_ids) <= 20:
-                # Convert the price in the right currency.
-                currency = partner.property_purchase_currency_id \
-                           or self.env.user.company_id.currency_id
-                price = self.currency_id._convert(
-                    line.price_unit, currency, line.company_id,
-                    line.date_order or fields.Date.today(), round=False)
-                # Compute the price for the template's UoM, because the supplier's
-                # UoM is related to that UoM.
-                if line.product_id.product_tmpl_id.uom_po_id != line.product_uom:
-                    default_uom = line.product_id.product_tmpl_id.uom_po_id
-                    price = line.product_uom._compute_price(price, default_uom)
+            # REMOVED limit otherwise prices won't be updated
+            # Convert the price into the right currency.
+            currency = partner.property_purchase_currency_id \
+                       or self.env.user.company_id.currency_id
+            price = self.currency_id._convert(
+                line.price_unit, currency, line.company_id,
+                line.date_order or fields.Date.today(), round=False)
+            # Compute the price for the template's UoM, because the supplier's
+            # UoM is related to that UoM.
+            if line.product_id.product_tmpl_id.uom_po_id != line.product_uom:
+                default_uom = line.product_id.product_tmpl_id.uom_po_id
+                price = line.product_uom._compute_price(price, default_uom)
 
-                supplierinfo = {
-                    'name': partner.id,
-                    'sequence': max(line.product_id.seller_ids.mapped('sequence')) + 1
-                    if line.product_id.seller_ids else 1,
-                    'min_qty': 0.0,
-                    'price': price,
-                    'currency_id': currency.id,
-                    'delay': 0,
-                    'discount': line.discount,  ### add discount
-                }
-                if hasattr(line, 'discount2'):
-                    supplierinfo.update({
-                        'discount2': line.discount2,
-                        'discount3': line.discount3,
-                    })
-                # In case the order partner is a contact address, a new supplierinfo
-                # is created on
-                # the parent company. In this case, we keep the product name and code.
-                seller = line.product_id._select_seller(
-                    partner_id=line.partner_id,
-                    quantity=line.product_qty,
-                    date=line.order_id.date_order and line.order_id.date_order.date(),
-                    uom_id=line.product_uom)
-                if seller:
-                    supplierinfo['product_name'] = seller.product_name
-                    supplierinfo['product_code'] = seller.product_code
-                vals = {
-                    'seller_ids': [(0, 0, supplierinfo)],
-                }
-                try:
-                    if partner in line.product_id.seller_ids.mapped('name'):
-                        seller = line.product_id.seller_ids.filtered(
-                            lambda x: x.name == partner)
-                        for key in ['name', 'sequence', 'min_qty', 'delay']:
-                            supplierinfo.pop(key)
-                        seller.write(supplierinfo)
-                    else:
-                        line.product_id.write(vals)
-                except AccessError:  # no write access rights -> just ignore
-                    break
+            supplierinfo = {
+                'name': partner.id,
+                'sequence': max(line.product_id.seller_ids.mapped('sequence')) + 1
+                if line.product_id.seller_ids else 1,
+                'min_qty': 0.0,
+                'price': price,
+                'currency_id': currency.id,
+                'delay': 0,
+                'discount': line.discount,  ### add discount
+            }
+            if hasattr(line, 'discount2'):
+                supplierinfo.update({
+                    'discount2': line.discount2,
+                    'discount3': line.discount3,
+                })
+            # In case the order partner is a contact address, a new supplierinfo
+            # is created on
+            # the parent company. In this case, we keep the product name and code.
+            seller = line.product_id._select_seller(
+                partner_id=line.partner_id,
+                quantity=line.product_qty,
+                date=line.order_id.date_order and line.order_id.date_order.date(),
+                uom_id=line.product_uom)
+            if seller:
+                supplierinfo['product_name'] = seller.product_name
+                supplierinfo['product_code'] = seller.product_code
+            vals = {
+                'seller_ids': [(0, 0, supplierinfo)],
+            }
+            try:
+                if partner in line.product_id.seller_ids.mapped('name'):
+                    seller = line.product_id.seller_ids.filtered(
+                        lambda x: x.name == partner)
+                    for key in ['name', 'sequence', 'min_qty', 'delay']:
+                        supplierinfo.pop(key)
+                    seller.write(supplierinfo)
+                else:
+                    line.product_id.write(vals)
+            except AccessError:  # no write access rights -> just ignore
+                break
+
+            if len(line.product_id.seller_ids) >= 20:
+                # todo remove older seller
+                pass
 
     @api.multi
     def button_approve(self, force=False):

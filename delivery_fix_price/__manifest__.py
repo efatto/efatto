98f8a62de6fa023d4354115b99234916ac7e8b25
sergiocorato <sergiocorato@gmail.com>
2021-09-22 18:28:34 +0200

    [PORT] module to fix delivery amount

---
+++ delivery_fix_price/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2016-2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Delivery fix price',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'description': 'Delivery fix price wich is computed with vat',
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'delivery',
+    ],
+    'data': [
+    ],
+    'installable': True
+}

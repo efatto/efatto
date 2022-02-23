218fefd6b00ced04b1922575b05eb7acf267ca6d
sergiocorato <sergiocorato@gmail.com>
2022-02-23 11:32:31 +0100

    [ADD] dn dropshipping module

---
+++ l10n_it_dn_dropshipping/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': "DN dropshipping",
+    'version': '12.0.1.0.0',
+    'category': 'Stock',
+    'summary': "Allows to use dropshipping with DN",
+    'description': """
+    Allows to use dropshipping with DN
+    """,
+    'author': "Sergio Corato",
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'l10n_it_delivery_note',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}

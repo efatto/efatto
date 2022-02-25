d76446cb8a1f95be40f8ecebb99df15181b028e8
sergiocorato <sergiocorato@gmail.com>
2022-02-25 16:47:05 +0100

    [IMP] split module stock move view extended

---
+++ stock_move_view_sale_purchase/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2017-2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Stock move purchase/sale ref in tree',
+    'version': '12.0.1.0.2',
+    'category': 'other',
+    'description': """
+    Show stock move sale and purchase order in tree view.
+    """,
+    'author': 'Sergio Corato',
+    'website': 'https://www.efatto.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'purchase_stock',
+        'sale_stock',
+    ],
+    'data': [
+        'views/stock.xml',
+    ],
+    'installable': True,
+}

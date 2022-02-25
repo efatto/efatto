621f3e8f856c05e51a91b16df1c7badf17c2a22a
sergiocorato <sergiocorato@gmail.com>
2022-02-25 16:47:05 +0100

    [IMP] split module stock move view extended

---
+++ stock_move_view_sale_purchase/models/stock.py
@@ -0,0 +1,24 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    sale_id = fields.Many2one(
+        string='Sale Order',
+        related='move_id.sale_line_id.order_id',
+        store=True,
+    )
+    sale_partner_id = fields.Many2one(
+        string='Sale Partner',
+        related='sale_id.partner_id',
+        store=True,
+    )
+    purchase_id = fields.Many2one(
+        string='Purchase Order',
+        related='move_id.purchase_line_id.order_id',
+        store=True,
+    )

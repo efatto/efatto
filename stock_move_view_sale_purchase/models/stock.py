3eb64104c5f836660b7a7d534856db5e180625db
sergiocorato <sergiocorato@gmail.com>
2022-02-25 16:53:17 +0100

    [IMP] add index

--- stock_move_view_sale_purchase/models/stock.py
+++ stock_move_view_sale_purchase/models/stock.py
@@ -11,14 +11,17 @@ class StockMoveLine(models.Model):
         string='Sale Order',
         related='move_id.sale_line_id.order_id',
         store=True,
+        index=True,
     )
     sale_partner_id = fields.Many2one(
         string='Sale Partner',
         related='sale_id.partner_id',
         store=True,
+        index=True,
     )
     purchase_id = fields.Many2one(
         string='Purchase Order',
         related='move_id.purchase_line_id.order_id',
         store=True,
+        index=True,
     )

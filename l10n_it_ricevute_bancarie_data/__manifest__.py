7b0a419d23ac9a7e17f74239213da80df4662a6f
sergiocorato <sergiocorato@gmail.com>
2022-02-28 01:07:39 +0100

    [FIX] bump version

--- l10n_it_ricevute_bancarie_data/__manifest__.py
+++ l10n_it_ricevute_bancarie_data/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Termini di pagamento per ricevute bancarie',
-    'version': '12.0.1.0.3',
+    'version': '12.0.1.0.4',
     'category': 'Localisation/Italy',
     'description': 'Ri.ba. sbf data',
     'author': 'Corato Sergio',

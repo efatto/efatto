21dc166c2465dd99af7336435843aeed6a868331
sergiocorato <sergiocorato@gmail.com>
2022-03-14 17:49:23 +0100

    [IMP] add module for autobackup healthcheck

---
+++ auto_backup_healthcheck/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Auto backup healthcheck',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'description': 'Add healthcheck autobackup info to remote service.',
+    'website': 'https://efatto.it',
+    'depends': [
+        'auto_backup',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}

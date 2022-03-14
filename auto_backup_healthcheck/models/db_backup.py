329a3653389223143a94baaabc9c0c6dd7ed0e49
sergiocorato <sergiocorato@gmail.com>
2022-03-14 18:07:36 +0100

    [UPD] check success backup

--- auto_backup_healthcheck/models/db_backup.py
+++ auto_backup_healthcheck/models/db_backup.py
@@ -1,8 +1,8 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import api, models, _
-from odoo.tools import config, ustr
-from contextlib import contextmanager
+from odoo.tools import ustr
+
 import requests
 
 
@@ -10,13 +10,14 @@ class DbBackup(models.Model):
     _inherit = 'db.backup'
 
     @api.multi
-    @contextmanager
-    def backup_log(self):
-        res = super().backup_log()
-        url = config.get("healthcheck_url")
-        if url and res:
-            if _("Database backup succeeded.") in self.message_ids[0].body:
-                msg = self.message_ids[0].body
-                arguments = {'arg0': ustr(msg), "action": "update"}
-                r = requests.post(url, data=arguments)
-                r.raise_for_status()
+    def action_backup(self):
+        super().action_backup()
+        url = self.env['ir.config_parameter'].sudo().get_param(
+            "healthcheck_url", False)
+        if url:
+            for backup in self:
+                if _("Database backup succeeded.") in backup.message_ids[0].body:
+                    msg = backup.message_ids[0].body
+                    arguments = {'arg0': ustr(msg), "action": "update"}
+                    r = requests.post(url, data=arguments)
+                    r.raise_for_status()

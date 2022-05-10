ea9c4a4e5b2ab908b4d5fa6c8d004d517db9d916
sergiocorato <sergiocorato@gmail.com>
2022-05-10 17:37:23 +0200

    [ADD] new module stock expected mrp

---
+++ stock_move_available_date_expected_mrp/models/mrp.py
@@ -0,0 +1,25 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import _, models
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def open_view_stock_reserved(self):
+        product = self.product_id
+        domain = [('product_id', '=', product.id),
+                  ('state', '!=', 'cancel'),
+                  ('date_expected', '>=',
+                   product.product_tmpl_id.date_oldest_open_move)]
+        view = self.env.ref(
+            'stock_move_available_date_expected.view_stock_reserved_tree')
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _('Reserved Stock: %s') % product.name,
+            'domain': domain,
+            'views': [(view.id, 'tree'), (False, 'pivot')],
+            'res_model': 'stock.move',
+            'context': {},
+        }

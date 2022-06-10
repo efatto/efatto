40b767b821cdf30715dae6fc031185e319ac5777
sergiocorato <sergiocorato@gmail.com>
2022-06-10 18:37:33 +0200

    [ADD] module for product sellers info

---
+++ product_sellers_info/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021-2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product sellers info",
+    "version": "12.0.1.0.0",
+    "category": "other",
+    "description": "Add product multiple qty to product sellers; add computed fields "
+                   "for product multiple qty and purchase delay of first seller.",
+    "author": "Sergio Corato - Efatto.it",
+    "website": "https://efatto.it",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/product.xml",
+    ],
+    "installable": True
+}

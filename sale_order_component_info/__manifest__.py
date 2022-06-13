77a4e89e5a85e263d947985977cf2bce94d36185
sergiocorato <sergiocorato@gmail.com>
2022-06-13 14:37:11 +0200

    Move module from other repo with history

---
+++ sale_order_component_info/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Sale order info for components',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato - Efatto.it',
+    'website': 'https://efatto.it',
+    'description': 'Sale order info for product with components',
+    'license': 'LGPL-3',
+    'depends': [
+        'sale_bookmark',
+        'sale_stock_mrp_produce_delay',
+    ],
+    'data': [
+        'wizard/sale_order_component.xml',
+        'views/sale.xml',
+    ],
+    'installable': True
+}

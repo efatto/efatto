ecc33bc15b7163d8605d94c600ba00167470c82a
sergiocorato <sergiocorato@gmail.com>
2022-06-17 15:38:31 +0200

    [FIX] tmp download url1

--- l10n_it_fatturapa_share/controllers/main.py
+++ l10n_it_fatturapa_share/controllers/main.py
@@ -26,8 +26,8 @@ class FatturaPAAttachmentDownload(Controller):
         atts = request.env[model].search(domain)
         config_obj = request.env['ir.config_parameter'].get_param(
             'web.base.url')
-        attachment_url = "%s/download/%s/web/%s/token/" % (
-            config_obj, config_obj, model
+        attachment_url = "/web/%s/token/" % (
+            model
         )
         for att_obj in atts:
             if att_obj.access_token:

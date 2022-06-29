16870964e0c4534ce89d12ebfc45b4e225e2427a
sergiocorato <sergiocorato@gmail.com>
2022-06-29 11:03:53 +0200

    [ADD] module for button transfer in stock dashboard

---
+++ stock_transfer_button/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Stock transfer button in dashboard',
+    'summary': 'Add button for planned transfer in stock dashboard kanban',
+    'version': '12.0.1.0.0',
+    'author': 'Sergio Corato',
+    'category': 'Warehouse',
+    'license': 'AGPL-3',
+    'website': 'https://efatto.it',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_view.xml',
+    ],
+    'installable': True,
+}

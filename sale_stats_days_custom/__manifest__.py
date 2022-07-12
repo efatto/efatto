e365ff7503b32688853edc991fd7f4345da3c462
sergiocorato <sergiocorato@gmail.com>
2022-07-12 11:38:03 +0200

    [IMP] add module to customize days of sale statistics

---
+++ sale_stats_days_custom/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Custom days for sale button stats",
+    "summary": "Show stats with custom days period",
+    "version": "12.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Sales",
+    "website": "https://efatto.it",
+    "author": "Sergio Corato",
+    "maintainers": ["sergiocorato"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/res_config_settings_view.xml",
+    ],
+}
---
+++ sale_stats_days_custom/readme/CONFIGURE.rst
@@ -0,0 +1,14 @@
+Nella configurazione vendite è stato aggiunto un campo in cui scegliere su quanti giorni precedenti alla data attuale vengono calcolate le statistiche dei prodotti venduti:
+
+.. image:: ../static/description/configurazione.png
+    :alt: configurazione
+
+Smart button raggiungibile nel prodotto:
+
+.. image:: ../static/description/bottone_vendite.png
+    :alt: bottone vendite
+
+e se è installato il modulo `https://github.com/OCA/sale-workflow/tree/12.0/product_form_sale_link`:
+
+.. image:: ../static/description/bottone_righe_vendite.png
+    :alt: bottone righe vendite

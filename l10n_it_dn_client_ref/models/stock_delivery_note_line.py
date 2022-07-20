3686ac7f6cd0fec8451a8d3f56440c3a6d01e4b0
sergiocorato <sergiocorato@gmail.com>
2022-07-20 09:23:27 +0200

    [ADD] module to show client order ref in delivery note

---
+++ l10n_it_dn_client_ref/models/stock_delivery_note_line.py
@@ -0,0 +1,11 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class StockDeliveryNoteLine(models.Model):
+    _inherit = 'stock.delivery.note.line'
+
+    client_order_ref = fields.Char(
+        related='sale_line_id.order_id.client_order_ref'
+    )

a1babe8819a40198835026aa30c6af9cdf25cf37
sergiocorato <sergiocorato@gmail.com>
2022-09-07 17:52:14 +0200

    [ADD] module for inventory report with discrepancy

---
+++ stock_inventory_discrepancy_report_py3o/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock inventory discrepancy report py3o",
+    "version": "12.0.1.0.0",
+    "category": "other",
+    "description": "Stock inventory ods report for discrepancy.",
+    "author": "Sergio Corato",
+    "website": "https://efatto.it",
+    "license": "AGPL-3",
+    "depends": [
+        "stock_inventory_discrepancy",
+        "report_py3o",
+    ],
+    "data": [
+        "report/report.xml",
+    ],
+    "installable": True,
+}

d098f2d5de2f126ec12e7fede823ff3689e13cad
sergiocorato <sergiocorato@gmail.com>
2022-09-07 18:38:46 +0200

    [FIX] name

--- stock_inventory_discrepancy_report_py3o/__manifest__.py
+++ stock_inventory_discrepancy_report_py3o/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Stock inventory discrepancy report py3o",
     "version": "12.0.1.0.0",
     "category": "other",
-    "description": "Stock inventory xls report for discrepancy.",
+    "description": "Stock inventory ods report for discrepancy.",
     "author": "Sergio Corato",
     "website": "https://efatto.it",
     "license": "AGPL-3",

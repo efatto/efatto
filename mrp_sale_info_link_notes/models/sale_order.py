db6f74f504041628e5cb6fe6837466b5c2adcd70
sergiocorato <sergiocorato@gmail.com>
2022-09-26 14:29:48 +0200

    [IMP] add calendar fields

--- mrp_sale_info_link_notes/models/sale_order.py
+++ mrp_sale_info_link_notes/models/sale_order.py
@@ -1,8 +1,26 @@
-from odoo import fields, models
+from odoo import api, fields, models, _
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     production_notes = fields.Text()
+    production_notes_calendar = fields.Text(
+        compute='_compute_production_notes_calendar', store=True)
     is_prototype = fields.Boolean()
+    is_prototype_calendar = fields.Text(
+        compute='_compute_is_prototype_calendar', store=True)
+
+    @api.multi
+    @api.depends('production_notes')
+    def _compute_production_notes_calendar(self):
+        for order in self:
+            order.production_notes_calendar = _('MO Notes: %s') % \
+                order.production_notes if order.production_notes else ''
+
+    @api.multi
+    @api.depends('is_prototype')
+    def _compute_is_prototype_calendar(self):
+        for order in self:
+            order.is_prototype_calendar = _('PROTOTYPE') \
+                if order.is_prototype else ''

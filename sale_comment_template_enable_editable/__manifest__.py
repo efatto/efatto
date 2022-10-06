a21aa2f7f85960c58d41fd96c19c47886641af38
sergiocorato <sergiocorato@gmail.com>
2022-10-06 16:24:49 +0200

    [ADD] module for sale comment enable editable tree

---
+++ sale_comment_template_enable_editable/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Re-enable editable on sale order line',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'description': 'Warning: this module make impossible to write formatted note '
+                   'on sale order line.',
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale_comment_template',
+    ],
+    'data': [
+        'views/sale.xml',
+    ],
+    'installable': True,
+}

bcc8bac3c8304d5c21bce0436c410fa7d1d0d5b3
sergiocorato <sergiocorato@gmail.com>
2023-01-31 10:03:35 +0100

    [IMP] add abi-cab - clean code

--- l10n_it_ricevute_bancarie_total_report/__manifest__.py
+++ l10n_it_ricevute_bancarie_total_report/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2017-2020 Sergio Corato <https://github.com/sergiocorato>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# Copyright 2017-2023 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Ricevute bancarie report with total by date',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'Accounting',
     'author': 'Sergio Corato',
     'website': 'https://efatto.it',
     'description': 'Ricevute bancarie report with total by date',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'depends': [
         'l10n_it_ricevute_bancarie',
     ],

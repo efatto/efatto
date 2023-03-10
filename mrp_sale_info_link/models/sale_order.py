2e016312083203f1d1b030166146b35e2e34cebc
sergiocorato <sergiocorato@gmail.com>
2023-03-10 18:25:31 +0100

    [FIX] api unused

--- mrp_sale_info_link/models/sale_order.py
+++ mrp_sale_info_link/models/sale_order.py
@@ -1,7 +1,7 @@
 # Copyright 2018 Alex Comba - Agile Business Group
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):

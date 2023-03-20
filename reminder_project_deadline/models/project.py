397aa0ae6fed9d440634f34e3809aea9839cbc59
sergiocorato <sergiocorato@gmail.com>
2023-03-20 09:08:40 +0100

    [FIX] moved to no test repo

--- reminder_project_deadline/models/project.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-
-class Project(models.Model):
-    _name = 'project.project'
-    _inherit = ['project.project', 'reminder']
-
-    _reminder_date_field = 'date'
-    _reminder_attendees_fields = ['user_id']  # favorite_user_ids?

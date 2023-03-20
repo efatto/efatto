bae1b086ff4f3cc773cf744d0847c8e942b62c6b
sergiocorato <sergiocorato@gmail.com>
2023-03-20 09:09:08 +0100

    [FIX] license

--- account_analytic_menu/__manifest__.py
+++ account_analytic_menu/__manifest__.py
@@ -8,6 +8,7 @@
         'Account analytic menu',
     'author': 'Sergio Corato',
     'website': 'https://github.com/sergiocorato/efatto',
+    'license': 'AGPL-3',
     'depends': [
         'analytic',
     ],

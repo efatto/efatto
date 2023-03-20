7b90dfa076e7bb5d34a0d10791a462972888c0ba
sergiocorato <sergiocorato@gmail.com>
2023-03-20 09:27:00 +0100

    [FIX] licenses

--- l10n_it_ddt_data/__manifest__.py
+++ l10n_it_ddt_data/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2016-2020 Sergio Corato <https://github.com/sergiocorato>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Italy - Default DDT data',
@@ -10,7 +10,7 @@
     ''',
     'author': 'Sergio Corato',
     'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'depends': [
         'l10n_it_ddt',
     ],

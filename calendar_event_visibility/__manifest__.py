88458136bd0c1bd7e8c932c24f8f26cabc952e80
sergiocorato <sergiocorato@gmail.com>
2023-03-20 09:27:00 +0100

    [FIX] licenses

--- calendar_event_visibility/__manifest__.py
+++ calendar_event_visibility/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Event visibility',
     'version': '12.0.1.0.0',
@@ -8,7 +8,7 @@
     'description':
         'Limit visibility of event to involved users, excluding hr employee.',
     'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'depends': [
         'calendar',
     ],

f765d1267880de2e3ca934f6a2988889ceb9f52a
sergiocorato <sergiocorato@gmail.com>
2023-03-20 09:27:00 +0100

    [FIX] licenses

--- stock_picking_completed_assign/__manifest__.py
+++ stock_picking_completed_assign/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Stock picking completed assign',
@@ -7,7 +7,7 @@
     'category': 'other',
     'author': 'Sergio Corato',
     'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'description': '''
 This module set assign of stock moves with manual inserted completed quantity.''',
     'depends': [

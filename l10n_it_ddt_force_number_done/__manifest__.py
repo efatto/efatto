00ec0db585816cbea4459822b83af8ee41e4d04b
sergiocorato <sergiocorato@gmail.com>
2023-03-20 09:27:00 +0100

    [FIX] licenses

--- l10n_it_ddt_force_number_done/__manifest__.py
+++ l10n_it_ddt_force_number_done/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2018-2020 Sergio Corato <https://github.com/sergiocorato>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': "Change DDT Number on done state",
     'version': '12.0.1.0.0',

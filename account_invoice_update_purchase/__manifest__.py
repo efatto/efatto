3dd6197eb02c18b9c203025993434d7456231eed
sergiocorato <sergiocorato@gmail.com>
2023-04-24 17:59:42 +0200

    [IMP] add boolean to force validity of purchase linked to invoice

--- account_invoice_update_purchase/__manifest__.py
+++ account_invoice_update_purchase/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2021-2023 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'Account Invoice update purchase',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'author': 'Sergio Corato',
     'website': 'https://github.com/sergiocorato/efatto',
     'license': 'AGPL-3',

38f870c742ddfe4fbdf81d9dd565fa91aa2116ac
sergiocorato <sergiocorato@gmail.com>
2023-05-08 19:12:02 +0200

    [FIX] unused import

--- account_invoice_update_purchase_on_weight/models/account_invoice.py
+++ account_invoice_update_purchase_on_weight/models/account_invoice.py
@@ -1,7 +1,7 @@
-# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, fields
+from odoo import models, fields
 
 
 class AccountInvoiceLine(models.Model):

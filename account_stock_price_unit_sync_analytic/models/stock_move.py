f92e0be45ec92eb1b2f631dad6ea902efbaff773
sergiocorato <sergiocorato@gmail.com>
2023-05-10 16:32:31 +0200

    [FIX] moves var name

--- account_stock_price_unit_sync_analytic/models/stock_move.py
+++ account_stock_price_unit_sync_analytic/models/stock_move.py
@@ -10,20 +10,20 @@ class StockMove(models.Model):
     def _action_done(self):
         res = super()._action_done()
         # only moves linked to a sale or a production have to be updated
-        move_lines_to_do = res.filtered(
+        moves_to_do = res.filtered(
             lambda x: x.sale_line_id.order_id.analytic_account_id or
             x.raw_material_production_id.analytic_account_id
         )
         # search account invoice lines and launch updated from them, to include other
         # possible moves to do
-        if move_lines_to_do:
-            analytic_accounts = move_lines_to_do.mapped(
+        if moves_to_do:
+            analytic_accounts = moves_to_do.mapped(
                 "sale_line_id.order_id.analytic_account_id"
-            ) | move_lines_to_do.mapped(
+            ) | moves_to_do.mapped(
                 "raw_material_production_id.analytic_account_id")
             invoice_lines = self.env['account.invoice.line'].search([
                 ('account_analytic_id', 'in', analytic_accounts.ids),
-                ('product_id', 'in', move_lines_to_do.mapped('product_id.id')),
+                ('product_id', 'in', moves_to_do.mapped('product_id.id')),
                 ('invoice_type', '=', 'in_invoice'),
             ])
             invoice_lines.account_stock_price_unit_sync()

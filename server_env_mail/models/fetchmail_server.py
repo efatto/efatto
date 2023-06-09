e9fd0dfa87e6245f38c344761b826ea82c5cd513
sergiocorato <sergiocorato@gmail.com>
2023-06-09 17:39:06 +0200

    [MIG] server_env_mail: Migration to 14.0

--- server_env_mail/models/fetchmail_server.py
+++ server_env_mail/models/fetchmail_server.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 from odoo.addons.server_env.models.server import running
 
@@ -10,11 +10,9 @@ class Fetchmail(models.Model):
     _inherit = "fetchmail.server"
 
     @running
-    @api.multi
     def fetch_mail(self):
         return super().fetch_mail()
 
     @running
-    @api.multi
     def connect(self):
         return super().connect()

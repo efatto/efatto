85446a11827fad8399c87c57fca3456c70c2d203
sergiocorato <sergiocorato@gmail.com>
2023-06-09 17:03:54 +0200

    [IMP] server_env_mail: black, isort, prettier

--- server_env_mail/models/fetchmail_server.py
+++ server_env_mail/models/fetchmail_server.py
@@ -2,11 +2,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
+
 from odoo.addons.server_env.models.server import running
 
 
 class Fetchmail(models.Model):
-    _inherit = 'fetchmail.server'
+    _inherit = "fetchmail.server"
 
     @running
     @api.multi

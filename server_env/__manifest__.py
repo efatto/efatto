6e9abed97c42ad04ca33f8260253995168032c14
sergiocorato <sergiocorato@gmail.com>
2023-06-09 16:51:15 +0200

    [IMP] server_env: black, isort, prettier

--- server_env/__manifest__.py
+++ server_env/__manifest__.py
@@ -1,15 +1,15 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Server Env',
-    'version': '12.0.1.0.1',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'AGPL-3',
-    'description': 'Create decorator to bypass function on server running_env',
-    'depends': [
-        'base',
+    "name": "Server Env",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "description": "Create decorator to bypass function on server running_env",
+    "depends": [
+        "base",
     ],
-    'installable': True
+    "installable": True,
 }

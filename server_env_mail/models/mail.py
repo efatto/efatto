5c4b5656c797f85e03b7e182f77f1b7353d7a48a
sergiocorato <sergiocorato@gmail.com>
2023-06-09 17:39:06 +0200

    [MIG] server_env_mail: Migration to 14.0

--- server_env_mail/models/mail.py
+++ server_env_mail/models/mail.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 from odoo.addons.server_env.models.server import running
 
@@ -10,6 +10,5 @@ class MailMail(models.Model):
     _inherit = "mail.mail"
 
     @running
-    @api.multi
     def send(self, auto_commit=False, raise_exception=False):
         return super().send(auto_commit=auto_commit, raise_exception=raise_exception)

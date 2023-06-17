fc49e3b89e1523490277304ab88de420410e3cb7
sergiocorato <sergiocorato@gmail.com>
2023-06-17 15:53:20 +0200

    [IMP] config_sale: black, isort, prettier

--- config_sale/__manifest__.py
+++ config_sale/__manifest__.py
@@ -1,10 +1,10 @@
 # Copyright 2019 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Module to configure sale',
-    'version': '12.0.1.0.0',
-    'category': 'other',
-    'description': """
+    "name": "Module to configure sale",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "summary": """
 Module to configure sale.
 Add the next groups to base user:
 * sale.group_discount_per_so_line
@@ -14,14 +14,14 @@ Add the next groups to base user:
 Also set tax calculation rouding method as 'globally' and enable group sale
 delivery address.
 """,
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'AGPL-3',
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
     "depends": [
-        'sale',
+        "sale",
     ],
     "data": [
-        'data/group.xml',
+        "data/group.xml",
     ],
-    'installable': True
+    "installable": True,
 }

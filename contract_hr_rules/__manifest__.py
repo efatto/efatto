fc5d7c25a57a4f0218872c6d6565640a07b37105
sergiocorato <sergiocorato@gmail.com>
2023-06-18 17:51:25 +0200

    [IMP] contract_hr_rules: black, isort, prettier

--- contract_hr_rules/__manifest__.py
+++ contract_hr_rules/__manifest__.py
@@ -1,20 +1,20 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Sergio Corato
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Contract, analytic line and hr rules custom',
-    'version': '10.0.1.0.3',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'description': 'Contract and hr rules custom',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'AGPL-3',
-    'depends': [
-        'contract',
-        'hr',
+    "name": "Contract, analytic line and hr rules custom",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "summary": "Contract and hr rules custom",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "contract",
+        "hr",
+        "analytic",
     ],
-    'data': [
-        'security/contract_security.xml',
+    "data": [
+        "security/contract_security.xml",
     ],
-    'installable': True
+    "installable": True,
 }

6867217a20dded027926e9843c3997dbea96effb
sergiocorato <sergiocorato@gmail.com>
2023-06-26 18:34:57 +0200

    [IMP] account_invoice_constraint_chronology_supplier: black, isort, prettier

--- account_invoice_constraint_chronology_supplier/__manifest__.py
+++ account_invoice_constraint_chronology_supplier/__manifest__.py
@@ -1,9 +1,9 @@
 # Copyright 2015-2019 ACSONE SA/NV (<http://acsone.eu>)
-# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2020-2023 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Invoice Constraint Chronology",
-    "version": "12.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "Odoo Community Association (OCA), ACSONE SA/NV, Sergio Corato",
     "maintainer": "Sergio Corato",
     "website": "https://github.com/sergiocorato/efatto",

4cb62fb70223c8c289fc441ec7fd254fbf320f96
sergiocorato <sergiocorato@gmail.com>
2023-08-24 17:54:33 +0200

    [MIG][14.0] account_invoice_constraint_chronology_supplier

--- account_invoice_constraint_chronology_supplier/model/account_move.py
+++ account_invoice_constraint_chronology_supplier/model/account_move.py
@@ -1,12 +1,9 @@
 # Copyright 2015-2019 ACSONE SA/NV (<http://acsone.eu>)
-# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2020-2023 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
-import datetime
 
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
-from odoo.tools.misc import format_date
 
 
 class AccountMove(models.Model):
@@ -17,7 +14,7 @@ class AccountMove(models.Model):
         domain = [
             ("journal_id", "=", invoice.journal_id.id),
             ("date", ">", invoice.date),
-            ("number", "<", invoice.number),
+            ("name", "<", invoice.name),
         ]
         # check until last date of registration year date_range only
         registration_fy = self.env["account.fiscal.year"].search(
@@ -29,25 +26,28 @@ class AccountMove(models.Model):
         )
         if registration_fy:
             domain.append(("date", "<=", registration_fy.date_to))
-        # check on same type if sequence for refund is separate
+        # check on same move_type if sequence for refund is separate
         if invoice.journal_id.refund_sequence:
-            domain.append(("type", "=", invoice.type))
+            domain.append(("move_type", "=", invoice.move_type))
         return domain
 
-    def action_move_create(self):
-        previously_validated = self.filtered(lambda x: x.move_name)
-        res = super(AccountInvoice, self).action_move_create()
-        for inv in self:
-            if not inv.journal_id.check_chronology:
-                continue
-
-            if inv.type in ("in_invoice", "in_refund"):
-                if inv.date_invoice and inv.date and inv.date_invoice > inv.date:
+    def write(self, vals):
+        if vals.get("state") != "posted":
+            return super().write(vals)
+        previously_validated = self.filtered(lambda m: m.name and m.name != "/")
+        newly_posted = self.filtered(lambda m: m.state != "posted")
+        res = super().write(vals)
+        for inv in newly_posted & self.filtered("journal_id.check_chronology"):
+            if inv.move_type in ("in_invoice", "in_refund"):
+                if inv.invoice_date and inv.date and inv.invoice_date > inv.date:
                     raise UserError(
                         _(
                             "Supplier invoice date %s cannot be later than "
-                            "the date %s of registration!"
-                            % (inv.date_invoice, inv.date)
+                            "the date of registration %s!"
+                            % (
+                                inv.invoice_date.strftime("%d/%m/%Y"),
+                                inv.date.strftime("%d/%m/%Y"),
+                            )
                         )
                     )
             if inv not in previously_validated:
@@ -55,41 +55,34 @@ class AccountMove(models.Model):
                     self._prepare_later_invoices_domain(inv), order="date desc", limit=1
                 )
                 if invoices:
-                    date_invoice_format = datetime.datetime(
-                        year=invoices[0].date.year,
-                        month=invoices[0].date.month,
-                        day=invoices[0].date.day,
-                    )
-                    date_invoice_tz = format_date(
-                        self.env, fields.Date.context_today(self, date_invoice_format)
-                    )
                     raise UserError(
                         _(
                             "Chronology Error. Post the invoice with an equal "
-                            "or greater date than {date_invoice}."
-                        ).format(date_invoice=date_invoice_tz)
+                            "or greater date than {invoice_date}."
+                        ).format(invoice_date=invoices[0].date.strftime("%d/%m/%Y"))
                     )
         return res
 
     def _check_duplicate_supplier_reference(self):
         for invoice in self:
             # refuse to validate a vendor bill/credit note if there already exists one
-            # with the same reference for the same partner,
+            # with the same reference for the same partner for the same year (presuming
+            # fiscal year of vendor is 1/1-31/12),
             # because it's probably a double encoding of the same bill/credit note
-            # ### Whith reference of year ###
-            if invoice.type in ("in_invoice", "in_refund") and invoice.reference:
+            # todo create a boolean to force bypass of this check
+            if invoice.move_type in ("in_invoice", "in_refund") and invoice.ref:
                 first_year_date = (
-                    invoice.date_invoice
-                    and invoice.date_invoice.replace(month=1, day=1)
+                    invoice.invoice_date
+                    and invoice.invoice_date.replace(month=1, day=1)
                     or fields.Date.today().replace(month=1, day=1)
                 )
                 last_year_date = first_year_date.replace(month=12, day=31)
                 if self.search(
                     [
-                        ("date_invoice", ">=", first_year_date),
-                        ("date_invoice", "<=", last_year_date),
-                        ("type", "=", invoice.type),
-                        ("reference", "=", invoice.reference),
+                        ("invoice_date", ">=", first_year_date),
+                        ("invoice_date", "<=", last_year_date),
+                        ("move_type", "=", invoice.move_type),
+                        ("ref", "=", invoice.ref),
                         ("company_id", "=", invoice.company_id.id),
                         (
                             "commercial_partner_id",
@@ -101,7 +94,7 @@ class AccountMove(models.Model):
                 ):
                     raise UserError(
                         _(
-                            "Duplicated vendor reference detected. You probably encoded "
-                            "twice the same vendor bill/credit note."
+                            "Duplicated vendor reference detected. You probably encoded"
+                            " twice the same vendor bill/credit note."
                         )
                     )

328008f303681eecc21696e79f17a018573782ba
sergiocorato <sergiocorato@gmail.com>
2023-09-01 16:38:18 +0200

    [IMP] ir_attachment_visibility: black, isort, prettier

--- ir_attachment_visibility/__manifest__.py
+++ ir_attachment_visibility/__manifest__.py
@@ -1,20 +1,19 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Attachment visibility',
-    'version': '12.0.1.0.3',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'description':
-        'Limit visibility of attachment to creator user, excluding erp manager.',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'AGPL-3',
-    'depends': [
-        'base',
+    "name": "Attachment visibility",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "summary": "Limit visibility of attachment to creator user, excluding erp manager.",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'security/ir_attachment_security.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "security/ir_attachment_security.xml",
     ],
-    'installable': True,
+    "installable": True,
 }

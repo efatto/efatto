28903b5b3579fc17f77ba1a934a1317fe36b695d
sergiocorato <sergiocorato@gmail.com>
2023-09-06 10:56:17 +0200

    [IMP] project_stage_unique: black, isort, prettier [MIG] project_stage_unique: Migration to 14.0

--- project_stage_unique/__init__.py
+++ project_stage_unique/__init__.py
@@ -1,11 +1,15 @@
 from . import models
 import logging
+
 logger = logging.getLogger(__name__)
 
 
 def pre_init_hook(cr):
-    logger.info('Update project task type name to be unique')
-    cr.execute("""
+    logger.info("Update project task type name to be unique")
+    # This work for source term, translation terms are not considered.
+    cr.execute(
+        """
         UPDATE project_task_type SET name = CONCAT(name, '_', id) WHERE
-            id not in (SELECT min(id) from project_task_type group by name) 
-    """)
+            id not in (SELECT min(id) from project_task_type group by name)
+    """
+    )

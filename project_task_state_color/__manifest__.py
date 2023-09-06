3339a92b33764161e17c27785f3b0554426d2a47
sergiocorato <sergiocorato@gmail.com>
2023-09-06 22:57:02 +0200

    [IMP] project_task_state_color: black, isort, prettier [MIG] project_task_state_color to 14.0

--- project_task_state_color/__manifest__.py
+++ project_task_state_color/__manifest__.py
@@ -1,22 +1,21 @@
 # Copyright 2018-2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Project task colors',
-    'version': '12.0.1.0.0',
-    'category': 'Extra Tools',
-    'description':
-        'Change task color on state.',
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'AGPL-3',
-    'depends': [
-        'calendar',
-        'project_stage_closed',
-        'web_widget_color',
+    "name": "Project task colors",
+    "version": "14.0.1.0.0",
+    "category": "Extra Tools",
+    "summary": "Change task color on state.",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "calendar",
+        "project_timeline",
+        "web",
     ],
-    'data': [
-        'views/project.xml',
-        'views/calendar.xml',
+    "data": [
+        "views/project.xml",
+        "views/calendar.xml",
     ],
-    'installable': True,
+    "installable": True,
 }

e75804364b8cff1e639eb39d0fcdb9938e582874
sergiocorato <sergiocorato@gmail.com>
2023-09-06 10:56:17 +0200

    [IMP] project_stage_unique: black, isort, prettier [MIG] project_stage_unique: Migration to 14.0

--- project_stage_unique/__manifest__.py
+++ project_stage_unique/__manifest__.py
@@ -1,19 +1,17 @@
 # Copyright 2016-2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Project task type name unique',
-    'version': '12.0.1.0.0',
-    'category': 'Extra Tools',
-    'description':
-        'With this module the task type name does not accept duplicates.',
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'AGPL-3',
-    'depends': [
-        'project_task_default_stage',
+    "name": "Project task type name unique",
+    "version": "14.0.1.0.0",
+    "category": "Extra Tools",
+    "summary": "With this module the task type name does not accept duplicates.",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
     ],
-    'data': [
-    ],
-    'installable': True,
-    'pre_init_hook': 'pre_init_hook',
+    "data": [],
+    "installable": True,
+    "pre_init_hook": "pre_init_hook",
 }
--- project_stage_unique/models/project.py
+++ project_stage_unique/models/project.py
@@ -2,10 +2,12 @@ from odoo import models
 
 
 class ProjectTaskType(models.Model):
-    _inherit = 'project.task.type'
+    _inherit = "project.task.type"
 
-    _sql_constraints = [(
-        'name_project_task_type_uniq',
-        'unique(name)',
-        'A task type with the same name already exists for this company !'
-        )]
+    _sql_constraints = [
+        (
+            "name_project_task_type_uniq",
+            "unique(name)",
+            "A task type with the same name already exists for this company!",
+        )
+    ]

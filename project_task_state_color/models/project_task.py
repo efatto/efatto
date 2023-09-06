c507056e09288ae39a9e0db88ffed422e6b26793
sergiocorato <sergiocorato@gmail.com>
2023-09-06 22:57:02 +0200

    [IMP] project_task_state_color: black, isort, prettier [MIG] project_task_state_color to 14.0

--- project_task_state_color/models/project_task.py
+++ project_task_state_color/models/project_task.py
@@ -1,20 +1,20 @@
 # Copyright 2018-2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import fields, models
 
 
 class ProjectProject(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
     hex_value = fields.Char()
 
 
 class ProjectTask(models.Model):
-    _inherit = 'project.task'
+    _inherit = "project.task"
 
-    hex_value = fields.Char(related='project_id.hex_value', readonly=True)
-    hex_value_reduced = fields.Char(compute='_get_hex_task_value')
+    hex_value = fields.Char(related="project_id.hex_value", readonly=True)
+    hex_value_reduced = fields.Char(compute="_compute_hex_task_value")
 
     @staticmethod
     def colorscale(hexstr, scalefactor):
@@ -39,13 +39,12 @@ class ProjectTask(models.Model):
                 return maximum
             return val
 
-        hexstr = hexstr.strip('#')
+        hexstr = hexstr.strip("#")
 
         if scalefactor < 0 or len(hexstr) != 6:
             return hexstr
 
-        r, g, b = int(hexstr[:2], 16),\
-            int(hexstr[2:4], 16), int(hexstr[4:], 16)
+        r, g, b = int(hexstr[:2], 16), int(hexstr[2:4], 16), int(hexstr[4:], 16)
 
         r = clamp(r * scalefactor)
         g = clamp(g * scalefactor)
@@ -53,9 +52,9 @@ class ProjectTask(models.Model):
 
         return "#{:02X}{:02X}{:02X}".format(int(r), int(g), int(b))
 
-    @api.multi
-    def _get_hex_task_value(self):
+    def _compute_hex_task_value(self):
         for task in self:
+            hex_value_reduced = False
             if task.hex_value:
-                task.hex_value_reduced = task.colorscale(
-                    task.hex_value, .5)
+                hex_value_reduced = task.colorscale(task.hex_value, 0.5)
+            task.hex_value_reduced = hex_value_reduced

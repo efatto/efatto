dc295723850706ab7c238ea364f80b2e28fb61e2
sergiocorato <sergiocorato@gmail.com>
2023-09-19 14:51:50 +0200

    [MIG] account_invoice_update_purchase: Migration to 14.0

--- account_invoice_update_purchase/models/account_invoice.py
+++ account_invoice_update_purchase/models/account_invoice.py
@@ -1,71 +1,90 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
     purchase_force_valid = fields.Boolean()
 
 
-class AccountInvoiceLine(models.Model):
-    _inherit = "account.invoice.line"
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
 
     display_update_purchase_button = fields.Boolean(
-        compute='_compute_display_update_purchase_button',
-        store=True)
+        compute="_compute_display_update_purchase_button", store=True
+    )
 
-    @api.depends("purchase_line_id", "purchase_line_id.price_unit",
-                 "purchase_line_id.discount", "purchase_line_id.discount2",
-                 "purchase_line_id.discount3", "invoice_id.purchase_force_valid",
-                 "purchase_line_id.product_uom", "uom_id",
-                 "price_unit", "discount", "discount2", "discount3")
+    @api.depends(
+        "purchase_line_id",
+        "purchase_line_id.price_unit",
+        "purchase_line_id.discount",
+        "purchase_line_id.discount2",
+        "purchase_line_id.discount3",
+        "move_id.purchase_force_valid",
+        "purchase_line_id.product_uom",
+        "product_uom_id",
+        "price_unit",
+        "discount",
+        "discount2",
+        "discount3",
+    )
     def _compute_display_update_purchase_button(self):
         for line in self:
             line.display_update_purchase_button = (
-                not line.invoice_id.purchase_force_valid and
-                line.purchase_line_id and (
-                    line.purchase_line_id.price_unit != line.uom_id._compute_price(
+                not line.move_id.purchase_force_valid
+                and line.purchase_line_id
+                and (
+                    line.purchase_line_id.price_unit
+                    != line.product_uom_id._compute_price(
                         line.price_unit, line.purchase_line_id.product_uom
-                    ) or
-                    line.purchase_line_id.discount != line.discount or
-                    line.purchase_line_id.discount2 != line.discount2 or
-                    line.purchase_line_id.discount3 != line.discount3
+                    )
+                    or line.purchase_line_id.discount != line.discount
+                    or line.purchase_line_id.discount2 != line.discount2
+                    or line.purchase_line_id.discount3 != line.discount3
                 )
             )
 
     def update_purchase(self):
-        self.purchase_line_id.write({
-            'price_unit': self.uom_id._compute_price(
-                self.price_unit, self.purchase_line_id.product_uom
-            ),
-            'discount': self.discount,
-            'discount2': self.discount2,
-            'discount3': self.discount3,
-        })
-        supplierinfos = self.env['product.supplierinfo'].search([
-            ('name', '=', self.purchase_line_id.order_id.partner_id.id),
-            '|',
-            ('product_id', '=', self.product_id.id),
-            ('product_tmpl_id', '=', self.product_id.product_tmpl_id.id),
-        ])
+        self.purchase_line_id.write(
+            {
+                "price_unit": self.product_uom_id._compute_price(
+                    self.price_unit, self.purchase_line_id.product_uom
+                ),
+                "discount": self.discount,
+                "discount2": self.discount2,
+                "discount3": self.discount3,
+            }
+        )
+        supplierinfos = self.env["product.supplierinfo"].search(
+            [
+                ("name", "=", self.purchase_line_id.order_id.partner_id.id),
+                "|",
+                ("product_id", "=", self.product_id.id),
+                ("product_tmpl_id", "=", self.product_id.product_tmpl_id.id),
+            ]
+        )
         for supplierinfo in supplierinfos:
             if (
-                supplierinfo.date_end and supplierinfo.date_end >= (
-                    self.invoice_id.date_invoice or fields.Date.today()
-                ) or not supplierinfo.date_end
+                supplierinfo.date_end
+                and supplierinfo.date_end
+                >= (self.move_id.invoice_date or fields.Date.today())
+                or not supplierinfo.date_end
             ) and (
-                supplierinfo.date_start and supplierinfo.date_start <= (
-                    self.invoice_id.date_invoice or fields.Date.today()
-                ) or not supplierinfo.date_start
+                supplierinfo.date_start
+                and supplierinfo.date_start
+                <= (self.move_id.invoice_date or fields.Date.today())
+                or not supplierinfo.date_start
             ):
-                supplierinfo.write({
-                    'price': self.uom_id._compute_price(
-                        self.price_unit, supplierinfo.product_uom
-                    ),
-                    'discount': self.discount,
-                    'discount2': self.discount2,
-                    'discount3': self.discount3,
-                })
+                supplierinfo.write(
+                    {
+                        "price": self.product_uom_id._compute_price(
+                            self.price_unit, supplierinfo.product_uom
+                        ),
+                        "discount": self.discount,
+                        "discount2": self.discount2,
+                        "discount3": self.discount3,
+                    }
+                )
--- account_invoice_update_purchase/tests/test_account_invoice_update_purchase.py
+++ account_invoice_update_purchase/tests/test_account_invoice_update_purchase.py
@@ -1,117 +1,125 @@
+from odoo import fields
 from odoo.tests.common import SavepointCase
 from odoo.tools import mute_logger
-from odoo import fields
 from odoo.tools.date_utils import relativedelta
 
 
 class AccountInvoiceUpdatePurchase(SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
-        cls.user_model = cls.env['res.users'].with_context(no_reset_password=True)
-        cls.partner = cls.env.ref('base.res_partner_2')
-        buy = cls.env.ref('purchase_stock.route_warehouse0_buy')
-        cls.vendor = cls.env.ref('base.res_partner_3')
-        cls.supplierinfo_expired = cls.env['product.supplierinfo'].create({
-            'name': cls.vendor.id,
-            'price': 77,
-            'date_end': fields.Date.today() + relativedelta(days=-10),
-        })
-        cls.supplierinfo = cls.env['product.supplierinfo'].create({
-            'name': cls.vendor.id,
-            'price': 88,
-            'date_start': fields.Date.today() + relativedelta(days=-9),
-        })
-        cls.product = cls.env['product.product'].create({
-            'name': 'Product Test',
-            'type': 'product',
-            'uom_id': cls.env.ref('uom.product_uom_unit').id,
-            'lst_price': 77,
-            'seller_ids': [(6, 0, [cls.supplierinfo_expired.id, cls.supplierinfo.id])],
-            'route_ids': [(6, 0, [buy.id])],
-        })
-        cls.test_user = cls.env['res.users'].create({
-            'name': 'John',
-            'login': 'test',
-        })
-        cls.uom_hundred = cls.env['uom.uom'].create({
-            'name': 'Hundred',
-            'category_id': cls.env.ref('uom.product_uom_categ_unit').id,
-            'factor_inv': 100,
-            'uom_type': 'bigger',
-            'rounding': 0.01,
-        })
+        cls.user_model = cls.env["res.users"].with_context(no_reset_password=True)
+        cls.partner = cls.env.ref("base.res_partner_2")
+        buy = cls.env.ref("purchase_stock.route_warehouse0_buy")
+        cls.vendor = cls.env.ref("base.res_partner_3")
+        cls.supplierinfo_expired = cls.env["product.supplierinfo"].create(
+            {
+                "name": cls.vendor.id,
+                "price": 77,
+                "date_end": fields.Date.today() + relativedelta(days=-10),
+            }
+        )
+        cls.supplierinfo = cls.env["product.supplierinfo"].create(
+            {
+                "name": cls.vendor.id,
+                "price": 88,
+                "date_start": fields.Date.today() + relativedelta(days=-9),
+            }
+        )
+        cls.product = cls.env["product.product"].create(
+            {
+                "name": "Product Test",
+                "type": "product",
+                "uom_id": cls.env.ref("uom.product_uom_unit").id,
+                "lst_price": 77,
+                "seller_ids": [
+                    (6, 0, [cls.supplierinfo_expired.id, cls.supplierinfo.id])
+                ],
+                "route_ids": [(6, 0, [buy.id])],
+            }
+        )
+        cls.test_user = cls.env["res.users"].create(
+            {
+                "name": "John",
+                "login": "test",
+            }
+        )
+        cls.uom_hundred = cls.env["uom.uom"].create(
+            {
+                "name": "Hundred",
+                "category_id": cls.env.ref("uom.product_uom_categ_unit").id,
+                "factor_inv": 100,
+                "uom_type": "bigger",
+                "rounding": 0.01,
+            }
+        )
 
     def _create_purchase_order_line(self, order, product, qty, date_planned=False):
         vals = {
-            'order_id': order.id,
-            'product_id': product.id,
-            'product_qty': qty,
-            'product_uom': product.uom_po_id.id,
-            'price_unit': product.list_price,
-            'name': product.name,
+            "order_id": order.id,
+            "product_id": product.id,
+            "product_qty": qty,
+            "product_uom": product.uom_po_id.id,
+            "price_unit": product.list_price,
+            "name": product.name,
         }
         if date_planned:
-            vals.update({
-                'date_planned': date_planned
-            })
-        line = self.env['purchase.order.line'].create(vals)
+            vals.update({"date_planned": date_planned})
+        line = self.env["purchase.order.line"].create(vals)
         line._onchange_quantity()
         line._convert_to_write(line._cache)
         return line
 
     @staticmethod
     def _action_pack_operation_auto_fill(picking):
-        for op in picking.mapped('move_line_ids'):
-            if op.product_id.type == 'product':
+        for op in picking.mapped("move_line_ids"):
+            if op.product_id.type == "product":
                 op.qty_done = op.move_id.product_uom_qty
 
-    @mute_logger(
-        'odoo.models', 'odoo.models.unlink', 'odoo.addons.base.ir.ir_model'
-    )
+    @mute_logger("odoo.models", "odoo.models.unlink", "odoo.addons.base.ir.ir_model")
     def test_01_invoice_update_purchase_with_standard_cost_method(self):
-        self.assertEqual(self.product.categ_id.property_cost_method, 'standard')
-        purchase_order1 = self.env['purchase.order'].create({
-            'partner_id': self.vendor.id,
-        })
+        self.assertEqual(self.product.categ_id.property_cost_method, "standard")
+        purchase_order1 = self.env["purchase.order"].create(
+            {
+                "partner_id": self.vendor.id,
+            }
+        )
         purchase_planned_date1 = fields.Datetime.now() + relativedelta(days=5)
         purchase_line = self._create_purchase_order_line(
-            purchase_order1, self.product, 18, purchase_planned_date1)
+            purchase_order1, self.product, 18, purchase_planned_date1
+        )
         purchase_order1.button_confirm()
         current_price = purchase_line.price_unit
 
         self.assertEqual(purchase_line.product_uom_qty, 18)
-        self.assertEqual(purchase_order1.state, 'purchase')
+        self.assertEqual(purchase_order1.state, "purchase")
 
         picking = purchase_order1.picking_ids[0]
         self._action_pack_operation_auto_fill(picking)
         picking.button_validate()
-        self.assertEqual(picking.state, 'done')
+        self.assertEqual(picking.state, "done")
 
-        wizard = self.env["purchase.batch_invoicing"].with_context(
-            active_ids=purchase_order1.ids).create(dict())
-        res = wizard.action_batch_invoice()
-        invoice = self.env['account.invoice'].search(res.get('domain'))
+        res = purchase_order1.action_create_invoice()
+        invoice = self.env["account.move"].browse(res.get("res_id"))
         self.assertTrue(invoice)
         invoice_line = invoice.invoice_line_ids
         self.assertEqual(invoice_line.product_id, self.product)
 
         self.assertEqual(purchase_line.price_unit, current_price)
         self.assertEqual(self.supplierinfo.price, current_price)
-        self.assertEqual(picking.move_lines[0].price_unit, self.product.standard_price)
-        self.assertEqual(self.product.standard_price, self.product.standard_price)
+        self.assertEqual(picking.move_lines[0].price_unit, current_price)
 
         new_price = invoice_line.price_unit + 66
-        invoice_line.price_unit = new_price
+        invoice_line.with_context(check_move_validity=False).price_unit = new_price
         invoice_line.update_purchase()
         self.assertEqual(purchase_line.price_unit, new_price)
         self.assertEqual(self.supplierinfo.price, new_price)
         self.assertEqual(picking.move_lines[0].price_unit, new_price)
-        self.assertEqual(self.product.standard_price, self.product.standard_price)
 
         # change uom of invoice and test re-computation
-        invoice_line.uom_id = self.uom_hundred
+        invoice_line.with_context(
+            check_move_validity=False
+        ).product_uom_id = self.uom_hundred
         invoice_line.update_purchase()
-        self.assertAlmostEqual(purchase_line.price_unit, new_price/100)
+        self.assertAlmostEqual(purchase_line.price_unit, new_price / 100)

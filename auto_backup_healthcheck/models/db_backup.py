916e17c30555bdf4a1702aea081c946119ca9a5a
sergiocorato <sergiocorato@gmail.com>
2023-09-19 18:09:28 +0200

    [IMP] auto_backup_healthcheck: black, isort, prettier

--- auto_backup_healthcheck/models/db_backup.py
+++ auto_backup_healthcheck/models/db_backup.py
@@ -1,23 +1,26 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, models, _
-from odoo.tools import ustr
-
 import requests
+
+from odoo import _, api, models
+from odoo.tools import ustr
 
 
 class DbBackup(models.Model):
-    _inherit = 'db.backup'
+    _inherit = "db.backup"
 
     @api.multi
     def action_backup(self):
         super().action_backup()
-        url = self.env['ir.config_parameter'].sudo().get_param(
-            "auto_backup_healthcheck.url", False)
+        url = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("auto_backup_healthcheck.url", False)
+        )
         if url:
             for backup in self:
                 if _("Database backup succeeded.") in backup.message_ids[0].body:
                     msg = backup.message_ids[0].body
-                    arguments = {'arg0': ustr(msg), "action": "update"}
+                    arguments = {"arg0": ustr(msg), "action": "update"}
                     r = requests.post(url, data=arguments)
                     r.raise_for_status()

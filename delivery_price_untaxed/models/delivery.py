5fb0f9546356c70a7aee7063c2b67468d238471a
sergiocorato <sergiocorato@gmail.com>
2023-09-20 10:47:12 +0200

    [IMP] delivery_fix_price: black, isort, prettier and [MIG] to 14.0

--- delivery_price_untaxed/models/delivery.py
+++ delivery_price_untaxed/models/delivery.py
@@ -11,20 +11,23 @@ class ProviderGrid(models.Model):
         self.ensure_one()
         total = weight = volume = quantity = 0
         for line in order.order_line:
-            if line.state == 'cancel':
+            if line.state == "cancel":
                 continue
             if not line.is_delivery:
                 total += line.price_subtotal
             if not line.product_id or line.is_delivery:
                 continue
             qty = line.product_uom._compute_quantity(
-                line.product_uom_qty,
-                line.product_id.uom_id)
+                line.product_uom_qty, line.product_id.uom_id
+            )
             weight += (line.product_id.weight or 0.0) * qty
             volume += (line.product_id.volume or 0.0) * qty
             quantity += qty
 
         total = order.currency_id._convert(
-            total, order.company_id.currency_id,
-            order.company_id, order.date_order or fields.Date.today())
+            total,
+            order.company_id.currency_id,
+            order.company_id,
+            order.date_order or fields.Date.today(),
+        )
         return self._get_price_from_picking(total, weight, volume, quantity)

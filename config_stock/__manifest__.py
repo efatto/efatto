0edfa69f457dce50b9960c58280d932f3bcfc146
sergiocorato <sergiocorato@gmail.com>
2023-09-20 10:24:53 +0200

    [IMP] config_stock: black, isort, prettier and [MIG] to 14.0

--- config_stock/__manifest__.py
+++ config_stock/__manifest__.py
@@ -1,24 +1,24 @@
 # Copyright 2019 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Module to configure stock',
-    'version': '12.0.1.0.1',
-    'category': 'other',
-    'description': """
+    "name": "Module to configure stock",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "summary": """
 Module to configure stock.
 Add the next groups to base user:
 * stock.group_stock_multi_locations
 * stock.group_stock_adv_location
 * uom.group_uom
 """,
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'AGPL-3',
-    'depends': [
-        'stock',
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
     ],
-    'data': [
-        'data/group.xml',
+    "data": [
+        "data/group.xml",
     ],
-    'installable': True
+    "installable": True,
 }

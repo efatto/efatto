55d7d31f65efd5429e166c3ceb9bd2aef48b1d93
sergiocorato <sergiocorato@gmail.com>
2023-09-20 10:04:29 +0200

    [IMP] config_mrp: black, isort, prettier and [MIG] to 14.0

--- config_mrp/__manifest__.py
+++ config_mrp/__manifest__.py
@@ -1,22 +1,18 @@
 # Copyright 2019 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Module to configure mrp',
-    'version': '12.0.1.0.0',
-    'category': 'other',
-    'description': """
-Module to configure stock.
-Add the next groups to base user:
-- mrp.group_mrp_routings
-""",
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'AGPL-3',
-    'depends': [
-        'mrp',
+    "name": "Module to configure mrp",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "summary": "Add mrp.group_mrp_routings groups to base user",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
     ],
-    'data': [
-        'data/group.xml',
+    "data": [
+        "data/group.xml",
     ],
-    'installable': True
+    "installable": True,
 }

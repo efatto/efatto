94cbd99cbe2850363c58ce97ead5a57649736ed0
sergiocorato <sergiocorato@gmail.com>
2023-09-20 10:17:05 +0200

    [FIX] desc

--- config_stock/__manifest__.py
+++ config_stock/__manifest__.py
@@ -10,7 +10,6 @@ Add the next groups to base user:
 * stock.group_stock_multi_locations
 * stock.group_stock_adv_location
 * uom.group_uom
-It also set invoice policy default to delivery.
 """,
     'author': 'Sergio Corato',
     'website': 'https://github.com/sergiocorato/efatto',

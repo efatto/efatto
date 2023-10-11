24e2bf7591007c2451c428597fdb2c4632d5bdca
sergiocorato <sergiocorato@gmail.com>
2023-10-11 12:15:28 +0200

    [IMP] mrp_sale_info_link: black, isort, prettier and Migration to 14.0

--- mrp_sale_info_link/__manifest__.py
+++ mrp_sale_info_link/__manifest__.py
@@ -3,20 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Sale MRP info Link',
-    'summary': 'Show info on manufacturing orders generated from sales order',
-    'version': '12.0.1.0.2',
-    'category': 'Sales Management',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'author': 'Sergio Corato',
-    'license': 'AGPL-3',
-    'application': False,
-    'installable': True,
-    'depends': [
-        'mrp_sale_info',
+    "name": "Sale MRP info Link",
+    "summary": "Show info on manufacturing orders generated from sales order",
+    "version": "14.0.1.0.0",
+    "category": "Sales Management",
+    "website": "https://github.com/sergiocorato/efatto",
+    "author": "Sergio Corato",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "mrp_sale_info",
     ],
-    'data': [
-        'views/mrp_production.xml',
+    "data": [
+        "views/mrp_production.xml",
     ],
-    'post_init_hook': 'post_init_hook',
 }

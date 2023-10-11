036ebd5b6efe4e5233fee3e23861ffbfaa2ccbeb
sergiocorato <sergiocorato@gmail.com>
2023-10-11 12:41:35 +0200

    [IMP] mrp_sale_info_link_notes: black, isort, prettier and Migration to 14.0

--- mrp_sale_info_link_notes/models/sale_order.py
+++ mrp_sale_info_link_notes/models/sale_order.py
@@ -1,26 +1,28 @@
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     production_notes = fields.Text()
     production_notes_calendar = fields.Text(
-        compute='_compute_production_notes_calendar', store=True)
+        compute="_compute_production_notes_calendar", store=True
+    )
     is_prototype = fields.Boolean()
     is_prototype_calendar = fields.Text(
-        compute='_compute_is_prototype_calendar', store=True)
+        compute="_compute_is_prototype_calendar", store=True
+    )
 
-    @api.multi
-    @api.depends('production_notes')
+    @api.depends("production_notes")
     def _compute_production_notes_calendar(self):
         for order in self:
-            order.production_notes_calendar = _('MO Notes: %s') % \
-                order.production_notes if order.production_notes else ''
+            order.production_notes_calendar = (
+                _("MO Notes: %s") % order.production_notes
+                if order.production_notes
+                else ""
+            )
 
-    @api.multi
-    @api.depends('is_prototype')
+    @api.depends("is_prototype")
     def _compute_is_prototype_calendar(self):
         for order in self:
-            order.is_prototype_calendar = _('PROTOTYPE') \
-                if order.is_prototype else ''
+            order.is_prototype_calendar = _("PROTOTYPE") if order.is_prototype else ""

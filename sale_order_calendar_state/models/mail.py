f55094f57dcc609d1f8b10ee2d0e420236e432d0
sergiocorato <sergiocorato@gmail.com>
2023-10-11 12:12:24 +0200

    [IMP] sale_order_calendar_state: black, isort, prettier and Migration to 14.0

--- sale_order_calendar_state/models/mail.py
+++ sale_order_calendar_state/models/mail.py
@@ -2,22 +2,24 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 # Copyright (C) 2013 Stefano Siccardi creativiquadrati snc
 
-from odoo import models, api
+from odoo import models
 
 
 class MailComposeMessage(models.TransientModel):
-    _inherit = 'mail.compose.message'
+    _inherit = "mail.compose.message"
 
-    @api.multi
     def send_mail(self, auto_commit=False):
         """
         If the email has been sent from the "Alert partner of the change"
         button, update the sale order max_commitment_date.
         """
 
-        if (self._context.get('default_model', False) == 'sale.order' and
-            self._context.get('default_res_id', False) and self._context.get(
-                'change_agreed_date', False)):
-            self.env['sale.order']._set_max_commitment_date(
-                self._context['default_res_id'])
+        if (
+            self._context.get("default_model", False) == "sale.order"
+            and self._context.get("default_res_id", False)
+            and self._context.get("change_agreed_date", False)
+        ):
+            self.env["sale.order"]._inverse_max_commitment_date(
+                # self._context["default_res_id"]
+            )
         return super(MailComposeMessage, self).send_mail()
--- sale_order_calendar_state/models/picking.py
+++ sale_order_calendar_state/models/picking.py
@@ -1,44 +1,36 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models, api
+from odoo import api, fields, models
 
 
 class Picking(models.Model):
     _inherit = "stock.picking"
 
     date_ready_to_deliver = fields.Datetime(
-        compute='_compute_date_ready_to_deliver',
-        store=True
+        compute="_compute_date_ready_to_deliver", store=True
     )
-    is_assigned = fields.Boolean(
-        string="Printed for logistic",
-        copy=False
-    )
+    is_assigned = fields.Boolean(string="Printed for logistic", copy=False)
 
-    @api.depends('state')
+    @api.depends("state")
     def _compute_date_ready_to_deliver(self):
         for pick in self:
-            if pick.state in ['draft', 'waiting', 'confirmed', 'cancel']:
+            if pick.state in ["draft", "waiting", "confirmed", "cancel"]:
                 pick.date_ready_to_deliver = False
-            elif pick.state == 'assigned':
+            elif pick.state == "assigned":
                 pick.date_ready_to_deliver = fields.Datetime.now()
 
-    @api.multi
     def mark_printed_for_logistic(self):
-        self.write({'is_assigned': True})
+        self.write({"is_assigned": True})
 
-    @api.multi
     def unmark_printed_for_logistic(self):
-        self.write({'is_assigned': False})
+        self.write({"is_assigned": False})
 
-    @api.multi
     def do_unreserve(self):
         res = super().do_unreserve()
-        self.write({'is_assigned': False})
+        self.write({"is_assigned": False})
         return res
 
-    @api.multi
     def action_cancel(self):
-        self.write({'is_assigned': False})
+        self.write({"is_assigned": False})
         return super().action_cancel()

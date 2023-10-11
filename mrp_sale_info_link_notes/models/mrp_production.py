7ac40e6a755cab9cce0a91dcbba77d6a54183102
sergiocorato <sergiocorato@gmail.com>
2023-10-11 12:41:35 +0200

    [IMP] mrp_sale_info_link_notes: black, isort, prettier and Migration to 14.0

--- mrp_sale_info_link_notes/models/mrp_production.py
+++ mrp_sale_info_link_notes/models/mrp_production.py
@@ -2,9 +2,7 @@ from odoo import fields, models
 
 
 class MrpProduction(models.Model):
-    _inherit = 'mrp.production'
+    _inherit = "mrp.production"
 
-    production_notes = fields.Text(
-        related='sale_id.production_notes', store=True)
-    is_prototype = fields.Boolean(
-        related='sale_id.is_prototype', store=True)
+    production_notes = fields.Text(related="sale_id.production_notes", store=True)
+    is_prototype = fields.Boolean(related="sale_id.is_prototype", store=True)

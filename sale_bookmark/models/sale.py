c84c57935934390f2d743e2c751226a9a9dac137
sergiocorato <sergiocorato@gmail.com>
2023-10-11 09:52:13 +0200

    [IMP] sale_bookmark: black, isort, prettier and Migration to 14.0

--- sale_bookmark/models/sale.py
+++ sale_bookmark/models/sale.py
@@ -8,20 +8,24 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     bookmarked = fields.Boolean(
-        string='Computed bookmarked',
-        compute='_compute_bookmarked', store=True)
+        string="Computed bookmarked", compute="_compute_bookmarked", store=True
+    )
     bookmarked_manual = fields.Boolean(
         readonly=True,
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]})
+        states={"draft": [("readonly", False)], "sent": [("readonly", False)]},
+    )
 
-    @api.multi
-    @api.depends('state', 'bookmarked_manual')
+    @api.depends("state", "bookmarked_manual")
     def _compute_bookmarked(self):
-        bookmark_state = self.env['ir.config_parameter'].sudo().get_param(
-            'sale.order.bookmark.state', default=False)
+        bookmark_state = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("sale.order.bookmark.state", default=False)
+        )
         for order in self:
             if bookmark_state:
                 order.bookmarked = (
-                    bool(order.state == bookmark_state) or order.bookmarked_manual)
+                    bool(order.state == bookmark_state) or order.bookmarked_manual
+                )
             else:
                 order.bookmarked = order.bookmarked_manual

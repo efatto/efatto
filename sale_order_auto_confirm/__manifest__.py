b5785f62d7a47d1d6bc7799b2f2f64091eb612f2
sergiocorato <sergiocorato@gmail.com>
2023-10-11 09:58:29 +0200

    [IMP] sale_order_auto_confirm: black, isort, prettier and Migration to 14.0

--- sale_order_auto_confirm/__manifest__.py
+++ sale_order_auto_confirm/__manifest__.py
@@ -2,18 +2,18 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Sale order auto confirm',
-    'version': '12.0.1.0.2',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'description': 'Sale order auto confirm',
-    'license': 'AGPL-3',
-    'depends': [
-        'sale_order_calendar_state',
+    "name": "Sale order auto confirm",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "summary": "Sale order auto confirm",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_order_calendar_state",
     ],
-    'data': [
-        'data/cron.xml',
+    "data": [
+        "data/cron.xml",
     ],
-    'installable': True
+    "installable": True,
 }

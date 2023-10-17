17005f304dbb0e2ea9deed04301a114ad344f7e2
sergiocorato <sergiocorato@gmail.com>
2023-10-17 18:44:28 +0200

    [IMP] sale_order_component_info: black, isort, prettier and Migration to 14.0

--- sale_order_component_info/__manifest__.py
+++ sale_order_component_info/__manifest__.py
@@ -2,20 +2,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Sale order info for components',
-    'version': '12.0.1.0.0',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'description': 'Sale order info for product with components',
-    'license': 'AGPL-3',
-    'depends': [
-        'sale_bookmark',
-        'sale_stock_mrp_produce_delay',
+    "name": "Sale order info for components",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "summary": "Sale order info for product with components",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_bookmark",
+        "sale_stock_mrp_produce_delay",
     ],
-    'data': [
-        'wizard/sale_order_component.xml',
-        'views/sale.xml',
+    "data": [
+        "wizard/sale_order_component.xml",
+        "views/sale.xml",
     ],
-    'installable': True
+    "installable": True,
 }
--- sale_order_component_info/models/sale.py
+++ sale_order_component_info/models/sale.py
@@ -5,34 +5,38 @@ from odoo import fields, models
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     display_qty_component_widget = fields.Boolean(
-        compute='_compute_display_qty_component_widget')
+        compute="_compute_display_qty_component_widget"
+    )
 
     def _compute_display_qty_component_widget(self):
         for line in self:
-            line.display_qty_component_widget = (line.state in ['draft', 'sent']
-                                                 and line.product_id.bom_ids
-                                                 and line.qty_to_deliver > 0)
+            line.display_qty_component_widget = (
+                line.state in ["draft", "sent"]
+                and line.product_id.bom_ids
+                and line.qty_to_deliver > 0
+            )
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     def _bom_explode(self, product_id, products):
-        bom = self.env['mrp.bom']._bom_find(
-            product=product_id, company_id=self.company_id.id)
+        bom = self.env["mrp.bom"]._bom_find(
+            product=product_id, company_id=self.company_id.id
+        )
         if bom:
             bom_line_data = bom.explode(product_id, 1)[1]
-            products.extend({
-                'product_id': r[0].product_id.id,
-                'qty': r[1]['qty'],
-                'parent_id': product_id.id}
+            products.extend(
+                {
+                    "product_id": r[0].product_id.id,
+                    "qty": r[1]["qty"],
+                    "parent_id": product_id.id,
+                }
                 for r in bom_line_data
             )
-            for product in [
-                r[0].product_id for r in bom_line_data
-            ]:
+            for product in [r[0].product_id for r in bom_line_data]:
                 products = self._bom_explode(product, products)
         return products

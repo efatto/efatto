45d4e735c53a193bc9874ea580d51597eb510cae
sergiocorato <sergiocorato@gmail.com>
2023-10-17 19:05:46 +0200

    [IMP] stock_move_available_date_expected: black, isort, prettier and Migration to 14.0

--- stock_move_available_date_expected/__manifest__.py
+++ stock_move_available_date_expected/__manifest__.py
@@ -1,28 +1,26 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Stock move available date expected',
-    'version': '12.0.1.0.4',
-    'category': 'Stock',
-    'description': """
-    Add facility to view and change sale reserved on stock moves.
-    """,
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'AGPL-3',
-    'depends': [
-        'mrp_sale_info_link',
-        'purchase_line_procurement_group',
-        'purchase_stock',
-        'purchase_state_tag',
-        'sale_stock',
-        'stock_move_details',
-        'stock_quant_manual_assign',
+    "name": "Stock move available date expected",
+    "version": "14.0.1.0.0",
+    "category": "Stock",
+    "summary": "Add facility to view and change sale reserved on stock moves.",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp_sale_info_link",
+        "purchase_line_procurement_group",
+        "purchase_stock",
+        "purchase_state_tag",
+        "sale_stock",
+        "stock_move_details",
+        "stock_quant_manual_assign",
     ],
-    'data': [
-        'views/stock.xml',
-        'views/sale.xml',
-        'views/product.xml',
+    "data": [
+        "views/stock.xml",
+        "views/sale.xml",
+        "views/product.xml",
     ],
-    'installable': True,
+    "installable": True,
 }

33f3dac4d5e073507ff3a523002c968322db774d
sergiocorato <sergiocorato@gmail.com>
2023-10-17 19:09:46 +0200

    [IMP] purchase_state_tag: black, isort, prettier and Migration to 14.0

--- purchase_state_tag/__manifest__.py
+++ purchase_state_tag/__manifest__.py
@@ -1,20 +1,20 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Purchase tag on state',
-    'version': '12.0.1.0.0',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'description': 'Add purchase tag linked to order state.',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'AGPL-3',
-    'depends': [
-        'purchase',
-        'web_widget_color',
+    "name": "Purchase tag on state",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "summary": "Add purchase tag linked to order state.",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "purchase",
+        "web",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/purchase.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/purchase.xml",
     ],
-    'installable': True,
+    "installable": True,
 }

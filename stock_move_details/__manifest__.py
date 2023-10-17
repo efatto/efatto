32a2b31760e9b5283d6d17427facd5f61a59c28b
sergiocorato <sergiocorato@gmail.com>
2023-10-17 19:14:29 +0200

    [IMP] stock_move_details: black, isort, prettier and Migration to 14.0

--- stock_move_details/__manifest__.py
+++ stock_move_details/__manifest__.py
@@ -2,20 +2,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Stock move details in tree',
-    'version': '12.0.1.0.0',
-    'category': 'other',
-    'description': """
+    "name": "Stock move details in tree",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "summary": """
     Stock product available quantity in move tree
     """,
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'AGPL-3',
-    'depends': [
-        'stock',
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
     ],
-    'data': [
-        'views/stock.xml',
+    "data": [
+        "views/stock.xml",
     ],
-    'installable': True,
+    "installable": True,
 }

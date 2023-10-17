df391cdf8765de0e6a360f6d7d39bb4c2d49251e
sergiocorato <sergiocorato@gmail.com>
2023-10-17 19:14:29 +0200

    [IMP] stock_move_details: black, isort, prettier and Migration to 14.0

--- stock_move_details/models/stock.py
+++ stock_move_details/models/stock.py
@@ -1,56 +1,54 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models, api
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    @api.multi
-    @api.depends('product_uom_qty', 'location_dest_id', 'location_id')
+    @api.depends("product_uom_qty", "location_dest_id", "location_id")
     def _set_sign_product_qty(self):
         for move in self:
             move.qty_signed = move.product_uom_qty * (
-                -1 if move.location_dest_id.usage in [
-                    'customer', 'inventory', 'production', 'procurement',
-                    'supplier'
-                ] and move.location_id.usage == 'internal' else
-                0 if move.location_dest_id.usage in [
-                    'customer', 'inventory', 'production', 'procurement',
-                    'supplier'
-                ] and move.location_id.usage != 'internal' else
-                0 if move.location_dest_id.usage == move.location_id.usage
-                else 1)
+                -1
+                if move.location_dest_id.usage
+                in ["customer", "inventory", "production", "supplier"]
+                and move.location_id.usage == "internal"
+                else 0
+                if move.location_dest_id.usage
+                in ["customer", "inventory", "production", "supplier"]
+                and move.location_id.usage != "internal"
+                else 0
+                if move.location_dest_id.usage == move.location_id.usage
+                else 1
+            )
 
     qty_signed = fields.Float(
-        compute=_set_sign_product_qty,
-        store=True,
-        group_operator="sum"
+        compute=_set_sign_product_qty, store=True, group_operator="sum"
     )
 
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    @api.multi
-    @api.depends('qty_done', 'location_dest_id', 'location_id')
+    @api.depends("qty_done", "location_dest_id", "location_id")
     def _set_sign_product_qty(self):
         for line in self:
             line.qty_signed = line.qty_done * (
-                -1 if line.location_dest_id.usage in [
-                    'customer', 'inventory', 'production', 'procurement',
-                    'supplier'
-                ] and line.location_id.usage == 'internal' else
-                0 if line.location_dest_id.usage in [
-                    'customer', 'inventory', 'production', 'procurement',
-                    'supplier'
-                ] and line.location_id.usage != 'internal' else
-                0 if line.location_dest_id.usage == line.location_id.usage
-                else 1)
+                -1
+                if line.location_dest_id.usage
+                in ["customer", "inventory", "production", "supplier"]
+                and line.location_id.usage == "internal"
+                else 0
+                if line.location_dest_id.usage
+                in ["customer", "inventory", "production", "supplier"]
+                and line.location_id.usage != "internal"
+                else 0
+                if line.location_dest_id.usage == line.location_id.usage
+                else 1
+            )
 
     qty_signed = fields.Float(
-        compute=_set_sign_product_qty,
-        store=True,
-        group_operator="sum"
+        compute=_set_sign_product_qty, store=True, group_operator="sum"
     )

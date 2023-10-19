2c2c5bbdb1311e64dee713909fb994c4bdc4e908
sergiocorato <sergiocorato@gmail.com>
2023-10-19 13:07:55 +0200

    [IMP] stock_move_available_date_expected_mrp: black, isort, prettier and Migration to 14.0

--- stock_move_available_date_expected_mrp/__manifest__.py
+++ stock_move_available_date_expected_mrp/__manifest__.py
@@ -1,21 +1,21 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Stock move available date expected with MRP',
-    'version': '12.0.1.0.0',
-    'category': 'Stock',
-    'description': """
+    "name": "Stock move available date expected with MRP",
+    "version": "14.0.1.0.0",
+    "category": "Stock",
+    "summary": """
     Add facility to view and change sale reserved on stock moves for MRP components.
     """,
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'AGPL-3',
-    'depends': [
-        'mrp',
-        'stock_move_available_date_expected',
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+        "stock_move_available_date_expected",
     ],
-    'data': [
-        'views/mrp.xml',
+    "data": [
+        "views/mrp.xml",
     ],
-    'installable': True,
+    "installable": True,
 }

644171a11f3faf8c4e3462377579d8e172d6234d
sergiocorato <sergiocorato@gmail.com>
2023-10-20 19:17:21 +0200

    [IMP] stock_reserve_date_check: black, isort, prettier and Migration to 14.0

--- stock_reserve_date_check/__manifest__.py
+++ stock_reserve_date_check/__manifest__.py
@@ -1,19 +1,19 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Stock reserve date check',
-    'version': '12.0.1.0.0',
-    'category': 'Stock Management',
-    'author': 'Sergio Corato',
-    'license': 'AGPL-3',
-    'summary': 'Add logic to block confirmation of sale order on date not possible '
-               'on product stock or predicted arrival and manufacturing time.',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'depends': [
-        'sale_stock_mrp_produce_delay',
+    "name": "Stock reserve date check",
+    "version": "14.0.1.0.0",
+    "category": "Stock Management",
+    "author": "Sergio Corato",
+    "license": "AGPL-3",
+    "summary": "Add logic to block confirmation of sale order on date not possible "
+    "on product stock or predicted arrival and manufacturing time.",
+    "website": "https://github.com/sergiocorato/efatto",
+    "depends": [
+        "sale_stock_mrp_produce_delay",
     ],
-    'data': [
-        'views/sale.xml',
+    "data": [
+        "views/sale.xml",
     ],
-    'installable': True,
+    "installable": True,
 }

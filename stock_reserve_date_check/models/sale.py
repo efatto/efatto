c566188f2e3afa5fadfd53f81943d8a2bf1a7a8d
sergiocorato <sergiocorato@gmail.com>
2023-10-20 19:17:21 +0200

    [IMP] stock_reserve_date_check: black, isort, prettier and Migration to 14.0

--- stock_reserve_date_check/models/sale.py
+++ stock_reserve_date_check/models/sale.py
@@ -1,38 +1,43 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     enable_reserve_date_check = fields.Boolean(
-        help="Forbid reservation on not possible date",
-        default=True, copy=False)
+        help="Forbid reservation on not possible date", default=True, copy=False
+    )
 
-    @api.multi
     def action_confirm(self):
         # Add variable in context to enable check
         for order in self:
             if order.enable_reserve_date_check:
                 errors = []
-                for line in order.order_line.filtered('product_id'):
-                    commitment_date = line.commitment_date and\
-                        line.commitment_date.date() \
-                        or line.order_id.commitment_date and \
-                        line.order_id.commitment_date.date() or \
-                        line.order_id.date_order and line.order_id.date_order.date()
+                for line in order.order_line.filtered("product_id"):
+                    commitment_date = (
+                        line.commitment_date
+                        and line.commitment_date.date()
+                        or line.order_id.commitment_date
+                        and line.order_id.commitment_date.date()
+                        or line.order_id.date_order
+                        and line.order_id.date_order.date()
+                    )
                     avail_date, avail_date_info = line.get_available_date(
                         line.product_id,
                         line.product_uom_qty,
-                        fields.Date.context_today(line))
+                        fields.Date.context_today(line),
+                    )
                     if avail_date > commitment_date:
-                        dates_info = avail_date_info.split('\n')
-                        commitment_date_str = commitment_date.strftime('%d/%m/%Y')
+                        dates_info = avail_date_info.split("\n")
+                        commitment_date_str = commitment_date.strftime("%d/%m/%Y")
                         dates_info_clean = [
-                            x for x in dates_info if commitment_date_str not in x
-                            and x != '']
+                            x
+                            for x in dates_info
+                            if commitment_date_str not in x and x != ""
+                        ]
                         dates_info_clean.reverse()
                         produce_delay = 0
                         if line.product_id.produce_delay:
@@ -41,22 +46,32 @@ class SaleOrder(models.Model):
                             bom_id = line.product_id.bom_ids[0]
                             if bom_id.routing_id:
                                 produce_delay = int(
-                                    sum(bom_id.mapped(
-                                        'routing_id.operation_ids.time_cycle_manual'
-                                    ) or [0]) / 1440)
-                        errors.append(_(
-                            "Reservation of product [[%s] %s] is not possible for date"
-                            " %s!\nAvailable date: %s %s\n"
-                            "Exception availability info:\n%s") % (
+                                    sum(
+                                        bom_id.mapped(
+                                            "routing_id.operation_ids.time_cycle_manual"
+                                        )
+                                        or [0]
+                                    )
+                                    / 1440
+                                )
+                        errors.append(
+                            _(
+                                "Reservation of product [[%s] %s] is not possible for date"
+                                " %s!\nAvailable date: %s %s\n"
+                                "Exception availability info:\n%s"
+                            )
+                            % (
                                 line.product_id.default_code,
                                 line.product_id.name,
-                                commitment_date.strftime('%d/%m/%Y'),
-                                avail_date.strftime('%d/%m/%Y'),
-                                _("(Produce delay: %.0f days)") %
-                                produce_delay if produce_delay else '',
-                                '\n'.join([x for x in dates_info_clean])
-                            ))
+                                commitment_date.strftime("%d/%m/%Y"),
+                                avail_date.strftime("%d/%m/%Y"),
+                                _("(Produce delay: %.0f days)") % produce_delay
+                                if produce_delay
+                                else "",
+                                "\n".join([x for x in dates_info_clean]),
+                            )
+                        )
                 if errors:
-                    raise UserError(' '.join(errors))
+                    raise UserError(" ".join(errors))
         super().action_confirm()
         return True

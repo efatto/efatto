1cfafc1abebf351a1f3025067bac0926e0db2571
sergiocorato <sergiocorato@gmail.com>
2023-10-20 19:25:45 +0200

    [IMP] sale_stock_lot_group: black, isort, prettier and Migration to 14.0

--- sale_stock_lot_group/models/stock.py
+++ sale_stock_lot_group/models/stock.py
@@ -5,17 +5,19 @@ from odoo import api, models
 
 
 class ProductionLot(models.Model):
-    _inherit = 'stock.production.lot'
+    _inherit = "stock.production.lot"
 
-    @api.depends('name')
+    @api.depends("name")
     def _compute_sale_order_ids(self):
         for lot in self:
-            stock_moves = self.env['stock.move.line'].search([
-                ('lot_id', '=', lot.id),
-                ('state', '=', 'done')
-            ]).mapped('move_id')
-            stock_moves = stock_moves.search([('id', 'in', stock_moves.ids)]).filtered(
-                lambda move: move.picking_id.location_dest_id.usage == 'customer'
-                and move.state == 'done')
-            lot.sale_order_ids = stock_moves.sudo().mapped('sale_line_id.order_id')
+            stock_moves = (
+                self.env["stock.move.line"]
+                .search([("lot_id", "=", lot.id), ("state", "=", "done")])
+                .mapped("move_id")
+            )
+            stock_moves = stock_moves.search([("id", "in", stock_moves.ids)]).filtered(
+                lambda move: move.picking_id.location_dest_id.usage == "customer"
+                and move.state == "done"
+            )
+            lot.sale_order_ids = stock_moves.sudo().mapped("sale_line_id.order_id")
             lot.sale_order_count = len(lot.sale_order_ids)

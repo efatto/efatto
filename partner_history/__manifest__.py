2af00d325445768af713c197ae826ff9a87a390c
sergiocorato <sergiocorato@gmail.com>
2023-10-24 11:17:04 +0200

    [IMP] partner_history: black, isort, prettier and Migration to 14.0

--- partner_history/__manifest__.py
+++ partner_history/__manifest__.py
@@ -2,19 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Partner history',
-    'version': '12.0.1.0.1',
-    'category': 'Accounting & Finance',
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'AGPL-3',
-    'description': 'Partner name history',
-    'depends': [
-        'account',
+    "name": "Partner history",
+    "version": "14.0.1.0.0",
+    "category": "Accounting & Finance",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "summary": "Partner name history",
+    "depends": [
+        "account",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/partner.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/partner.xml",
     ],
-    'installable': True,
+    "installable": True,
 }

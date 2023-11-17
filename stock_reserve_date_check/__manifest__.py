9e832f24cbae3d92f2d44375e4ce8d917e45fd14
sergiocorato <sergiocorato@gmail.com>
2023-11-17 09:55:42 +0100

    [FIX] removed depends on sale_stock_mrp_produce_delay

--- stock_reserve_date_check/__manifest__.py
+++ stock_reserve_date_check/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Stock reserve date check",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Stock Management",
     "author": "Sergio Corato",
     "license": "AGPL-3",
@@ -10,7 +10,11 @@
     "on product stock or predicted arrival and manufacturing time.",
     "website": "https://github.com/sergiocorato/efatto",
     "depends": [
-        "sale_stock_mrp_produce_delay",
+        "mrp_production_demo",
+        "product_sellers_info",
+        "sale_order_line_date",
+        "sale_stock",
+        "stock_move_available_date_expected",
     ],
     "data": [
         "views/sale.xml",

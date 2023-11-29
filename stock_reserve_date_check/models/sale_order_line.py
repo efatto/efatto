f5cb2e652e4b6fb917627955ec2d17cc95badd1e
sergiocorato <sergiocorato@gmail.com>
2023-11-29 18:14:14 +0100

    [FIX] description of method

--- stock_reserve_date_check/models/sale_order_line.py
+++ stock_reserve_date_check/models/sale_order_line.py
@@ -44,15 +44,13 @@ class SaleOrderLine(models.Model):
             ]
             + domain_move_out_loc
         )
-        # check moves with date > date_deadline
+        # add moves with date > date_deadline to date_error_moves list
         moves_to_check = (incoming_stock_moves | reserved_stock_moves).filtered(
             lambda move: move.date and move.date_deadline
         )
         date_error_moves = [
             x.date_deadline.date() for x in moves_to_check if x.date > x.date_deadline
         ]
-        if date_error_moves:
-            pass
         for reserve_date in set(
             [x.date() for x in reserved_stock_moves.mapped("date_deadline")]
             + [y.date() for y in incoming_stock_moves.mapped("date_deadline")]

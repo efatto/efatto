bed1d34495a5270cce6ca46cc97e37b4fecae6a8
sergiocorato <sergiocorato@gmail.com>
2023-11-30 16:02:35 +0100

    [ADD] module product is kit

---
+++ product_is_kit/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product is kit",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "summary": "Add a field to check is product is a kit.",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/product.xml",
+    ],
+    "installable": True,
+}

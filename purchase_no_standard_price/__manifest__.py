0601d445b53fca52b975db13edbae8fafdd8d60c
sergiocorato <sergiocorato@gmail.com>
2023-11-30 12:29:07 +0100

    [ADD] module to restore purchase price logic from v. 12.0

---
+++ purchase_no_standard_price/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase no standard price",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "summary": "Restore logic of v. 12.0 as standard price is never a good choice for "
+               "purchases.",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
---
+++ purchase_no_standard_price/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module restore logic from v. 12.0 to not use standard price for purchases.

73dd5adb3747efa36933ae310154f3ece53244f1
sergiocorato <sergiocorato@gmail.com>
2023-12-05 11:22:11 +0100

    [ADD] module to set deadline date writeable in stock move

---
+++ stock_move_deadline_writeable/models/stock.py
@@ -0,0 +1,10 @@
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    date_deadline = fields.Datetime(
+        readonly=False)

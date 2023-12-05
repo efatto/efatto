3de2aa89570dd026290ee0da8bd3c5b8ffe065d9
sergiocorato <sergiocorato@gmail.com>
2023-12-05 11:22:11 +0100

    [ADD] module to set deadline date writeable in stock move

---
+++ stock_move_deadline_writeable/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock move deadline writeable",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "summary": """
+    Enable write on deadline on stock move
+    """,
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}

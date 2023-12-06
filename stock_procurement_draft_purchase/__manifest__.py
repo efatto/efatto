d4048422cbf672febdc22ce6eb8ef33668e37869
sergiocorato <sergiocorato@gmail.com>
2023-12-06 11:49:19 +0100

    [IMP] stock_procurement_draft_purchase: black, isort, prettier and Migration to 14.0

--- stock_procurement_draft_purchase/__manifest__.py
+++ stock_procurement_draft_purchase/__manifest__.py
@@ -1,22 +1,22 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Procurement include PO in DRAFT and SENT states',
-    'version': '12.0.1.0.1',
-    'category': 'Stock Management',
-    'author': 'Sergio Corato',
-    'summary': 'Include already created purchase order in draft and sent states when '
-               'computing stock minimum qty in orderpoint rules.',
-    'website': 'https://github.com/sergiocorato/efatto',
-    'license': 'AGPL-3',
-    'depends': [
-        'purchase_stock',
-        'sale_management',
-        'stock_orderpoint_manual_procurement',
-        'stock_warehouse_orderpoint_stock_info',
+    "name": "Procurement include PO in DRAFT and SENT states",
+    "version": "14.0.1.0.0",
+    "category": "Stock Management",
+    "author": "Sergio Corato",
+    "summary": "Include already created purchase order in draft and sent states when "
+    "computing stock minimum qty in orderpoint rules.",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "purchase_stock",
+        "sale_management",
+        "stock_orderpoint_manual_procurement",
+        "stock_warehouse_orderpoint_stock_info",
     ],
-    'data': [
-        'views/stock.xml',
+    "data": [
+        "views/stock.xml",
     ],
-    'installable': True,
+    "installable": True,
 }

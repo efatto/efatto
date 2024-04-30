9d1e4a7520b3eef6f28b092e3d7be26ba73dea23
sergiocorato <sergiocorato@gmail.com>
2024-04-30 16:19:57 +0200

    [FIX] test

--- sale_order_calendar_state/tests/test_sale_order_calendar_state.py
+++ sale_order_calendar_state/tests/test_sale_order_calendar_state.py
@@ -140,7 +140,7 @@ class TestSaleOrderCalendarState(TestProductionData):
         mo_form = Form(man_order)
         mo_form.qty_producing = 2
         man_order = mo_form.save()
-        self.assertEqual(order.calendar_state, "production_started")
+        self.assertEqual(order.calendar_state, "to_produce")
         action = man_order.button_mark_done()
         self.assertEqual(order.calendar_state, "production_started")
         backorder = Form(

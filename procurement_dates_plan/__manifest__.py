8fd1c89cace6d29c404bfa9d9e75cb4a25c4a1c5
sergiocorato <sergiocorato@gmail.com>
2024-06-25 18:41:27 +0200

    [FIX] module info

--- procurement_dates_plan/__manifest__.py
+++ procurement_dates_plan/__manifest__.py
@@ -1,16 +1,15 @@
 # Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "MRP Dates ignore Manufacturing Lead",
-    "version": "14.0.1.0.1",
+    "name": "Procurement ignore lead days",
+    "version": "14.0.1.0.0",
     "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/efatto",
-    "summary": "Ignore manufacturing_lead on setting dates in production on "
-    "product moves dates.",
+    "summary": "Ignore lead days on setting dates in product moves dates.",
     "license": "AGPL-3",
-    "category": "Manufacture",
+    "category": "Stock",
     "depends": [
-        "mrp",
+        "stock",
         "stock_reserve_date_check",
     ],
     "data": [],

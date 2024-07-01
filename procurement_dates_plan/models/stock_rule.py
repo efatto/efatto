1ea9f1a767f567dd802ab5307b6188da9b2288fd
sergiocorato <sergiocorato@gmail.com>
2024-07-01 11:32:49 +0200

    [IMP] update date_order for purchase order without po lead delay

--- procurement_dates_plan/models/stock_rule.py
+++ procurement_dates_plan/models/stock_rule.py
@@ -5,27 +5,14 @@ from odoo.tools.date_utils import relativedelta
 class StockRule(models.Model):
     _inherit = "stock.rule"
 
-    def _get_date_planned(self, product_id, company_id, values):
-        super()._get_date_planned(product_id, company_id, values)
-        date_requested = fields.Date.context_today(self)
-        if values.get("date_planned", False) and not values.get("orderpoint_id", False):
-            # not sure when this happens, sold MTO products perhaps
-            date_requested = fields.Date.from_string(values["date_planned"])
-        avail_date, avail_date_info = self.env["sale.order.line"].get_available_date(
-            product_id,
-            values["orderpoint_id"].qty_to_order
-            if values.get("orderpoint_id", False)
-            else 1,
-            date_requested,
+    def _prepare_purchase_order(self, company_id, origins, values):
+        res = super()._prepare_purchase_order(
+            company_id=company_id, origins=origins, values=values)
+        purchase_date = min(
+            [
+                fields.Datetime.from_string(value['date_planned'])
+                - relativedelta(days=int(value['supplier'].delay)) for value in values
+            ]
         )
-        if avail_date:
-            date_planned = avail_date - relativedelta(days=product_id.produce_delay)
-        else:
-            date_planned = fields.Datetime.now() + relativedelta(
-                days=product_id.produce_delay
-            )
-        # ignore company_id.manufacturing_lead for date planned
-        format_date_planned = fields.Datetime.from_string(values["date_planned"])
-        if date_planned == format_date_planned:
-            date_planned = date_planned - relativedelta(hours=1)
-        return date_planned
+        res.update(date_order=purchase_date)
+        return res

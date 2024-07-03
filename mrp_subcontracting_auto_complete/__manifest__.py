8ea27602894495ad301a28dac96804ffbec33ac5
sergiocorato <sergiocorato@gmail.com>
2024-07-03 12:15:57 +0200

    [ADD] new module mrp_subcontracting_auto_complete

---
+++ mrp_subcontracting_auto_complete/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Subcontracting auto complete on IN validation",
+    "version": "14.0.1.0.0",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "summary": "Validate a IN picking with submanufactured products will "
+               "complete the production, even if the qty production is the "
+               "same of qty requested.",
+    "license": "AGPL-3",
+    "category": "Manufacture",
+    "depends": [
+        "mrp_subcontracting",
+    ],
+    "data": [],
+    "installable": True,
+}

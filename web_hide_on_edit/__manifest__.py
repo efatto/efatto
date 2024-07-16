73c6a281a37755f8e6e22cda2cae6e8443214794
sergiocorato <sergiocorato@gmail.com>
2024-07-16 11:04:49 +0200

    [ADD] setup

---
+++ web_hide_on_edit/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018-2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Web hide statusbar on edit",
+    "version": "14.0.1.0.0",
+    "category": "Extra Tools",
+    "summary": "Hide statusbar when the user is editing the document.",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        "web",
+    ],
+    "data": [
+        "views/form.xml",
+        "views/account_invoice_view.xml",
+    ],
+    "installable": True,
+}

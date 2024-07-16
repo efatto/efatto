3989363e1f90d85d1779a3e06a821f3fcd13c8e6
sergiocorato <sergiocorato@gmail.com>
2024-07-16 18:55:31 +0200

    [FIX] web_hide_on_edit manifest

--- web_hide_on_edit/__manifest__.py
+++ web_hide_on_edit/__manifest__.py
@@ -1,7 +1,8 @@
-# Copyright 2018-2020 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2024 Marco Colombo <https://github.com/TheMule71>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Web hide statusbar on edit",
+    "name": "Web hide buttons on edit",
     "version": "14.0.1.0.0",
     "category": "Extra Tools",
     "summary": "Hide statusbar when the user is editing the document.",

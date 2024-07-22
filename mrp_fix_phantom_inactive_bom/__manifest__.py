68f589200d065025e07ef35162f977d6bd2e9ee7
sergiocorato <sergiocorato@gmail.com>
2024-07-22 11:35:49 +0200

    [ADD] module to fix phantom bom production

---
+++ mrp_fix_phantom_inactive_bom/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Alex Comba - Agile Business Group
+# Copyright 2016-2018 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "MRP fix phantom inactive bom",
+    "summary": "Override explode() methon in core MRP",
+    "version": "14.0.1.0.0",
+    "category": "MRP",
+    "website": "https://github.com/sergiocorato/efatto",
+    "author": "Sergio Corato",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "mrp",
+    ],
+    "data": [],
+}

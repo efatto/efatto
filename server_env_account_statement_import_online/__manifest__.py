dc3dad1fc0ab1ff53f9cf34c32093df38e5a0f71
sergiocorato <sergiocorato@gmail.com>
2024-09-06 11:39:02 +0200

    [ADD] module to link server env to bank statement import online

---
+++ server_env_account_statement_import_online/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Server Env Account Statement Import Online",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "account_statement_import_online",
+        "server_env",
+    ],
+    "installable": True,
+}

b0c01f39db77e94f821bf6904f53fcf5cda4c8d4
sergiocorato <sergiocorato@gmail.com>
2024-12-13 21:31:14 +0100

    [IMP] stock_inventory_valuation_report_py3o: black, isort, prettier

--- stock_inventory_valuation_report_py3o/__manifest__.py
+++ stock_inventory_valuation_report_py3o/__manifest__.py
@@ -2,9 +2,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Stock inventory valuation report py3o",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "other",
-    "description": """
+    "summary": """
     Stock inventory ods report for valuation.
     """,
     "author": "Sergio Corato",

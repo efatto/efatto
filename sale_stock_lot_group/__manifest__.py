3027b404ef0b3fe02d4b917f1ce7826c03a1c305
sergiocorato <sergiocorato@gmail.com>
2024-12-18 11:55:23 +0100

    [IMP] change repo slug to efatto

--- sale_stock_lot_group/__manifest__.py
+++ sale_stock_lot_group/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "Extra Tools",
     "summary": "Add group to sale button in stock lot",
     "author": "Sergio Corato",
-    "website": "https://github.com/sergiocorato/efatto",
+    "website": "https://github.com/efatto/efatto",
     "license": "AGPL-3",
     "depends": [
         "sale_stock",

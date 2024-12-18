ae9489afe6cb36776ddd9e8a5d1f878ea5e02c2f
sergiocorato <sergiocorato@gmail.com>
2024-12-18 11:55:23 +0100

    [IMP] change repo slug to efatto

--- stock_move_details/__manifest__.py
+++ stock_move_details/__manifest__.py
@@ -9,7 +9,7 @@
     Stock product available quantity in move tree
     """,
     "author": "Sergio Corato",
-    "website": "https://github.com/sergiocorato/efatto",
+    "website": "https://github.com/efatto/efatto",
     "license": "AGPL-3",
     "depends": [
         "stock",

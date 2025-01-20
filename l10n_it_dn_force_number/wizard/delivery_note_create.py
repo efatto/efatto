9e6d74529c6c052cadf0d267cafa13c6516338d7
sergiocorato <sergiocorato@gmail.com>
2025-01-20 12:15:55 +0100

    [IMP] refactor and add carrier tracking ref to DDT creation from wizard

--- l10n_it_dn_force_number/wizard/delivery_note_create.py
+++ l10n_it_dn_force_number/wizard/delivery_note_create.py
@@ -12,32 +12,16 @@ class StockDeliveryNoteCreateWizard(models.TransientModel):
         self.check_compliance(self.selected_picking_ids)
 
         sale_order_ids = self.mapped("selected_picking_ids.sale_id")
-        sale_order_id = sale_order_ids and sale_order_ids[0] or False
-
-        values = {
-            "partner_sender_id": self.partner_sender_id.id,
-            "partner_id": self.partner_id.id,
-            "partner_shipping_id": self.partner_shipping_id.id,
-            "type_id": self.type_id.id,
-            "date": self.date,
-            "delivery_method_id": self.partner_id.property_delivery_carrier_id.id,
-            "transport_condition_id": sale_order_id
-            and sale_order_id.default_transport_condition_id.id
-            or self.partner_id.default_transport_condition_id.id
-            or self.type_id.default_transport_condition_id.id,
-            "goods_appearance_id": sale_order_id
-            and sale_order_id.default_goods_appearance_id.id
-            or self.partner_id.default_goods_appearance_id.id
-            or self.type_id.default_goods_appearance_id.id,
-            "transport_reason_id": sale_order_id
-            and sale_order_id.default_transport_reason_id.id
-            or self.partner_id.default_transport_reason_id.id
-            or self.type_id.default_transport_reason_id.id,
-            "transport_method_id": sale_order_id
-            and sale_order_id.default_transport_method_id.id
-            or self.partner_id.default_transport_method_id.id
-            or self.type_id.default_transport_method_id.id,
-        }
+        sale_order_id = (
+            sale_order_ids and sale_order_ids[0] or self.env["sale.order"].browse()
+        )
+        values = self._prepare_delivery_note_vals(sale_order_id)
+        values.update(
+            {
+                "partner_id": self.partner_id.id,
+                "delivery_method_id": self.partner_id.property_delivery_carrier_id.id,
+            }
+        )
         if self.name:
             values.update({"name": self.name})
         delivery_note = self.env["stock.delivery.note"].create(values)
@@ -46,3 +30,12 @@ class StockDeliveryNoteCreateWizard(models.TransientModel):
 
         if self.user_has_groups("l10n_it_delivery_note.use_advanced_delivery_notes"):
             return delivery_note.goto()
+
+    def _prepare_delivery_note_vals(self, sale_order_id):
+        res = super()._prepare_delivery_note_vals(sale_order_id=sale_order_id)
+        carrier_tracking_ref = ", ".join(
+            {pick.carrier_tracking_ref for pick in self.selected_picking_ids}
+        )
+        if carrier_tracking_ref:
+            res["carrier_tracking_ref"] = carrier_tracking_ref
+        return res

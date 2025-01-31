3e0d1d371b79d2537c43c725bfdadb962472f2c4
sergiocorato <sergiocorato@gmail.com>
2025-01-31 19:23:47 +0100

    [FIX] default code column

--- product_pricelist_direct_print_all_lang/models/product_pricelist_direct_print.py
+++ product_pricelist_direct_print_all_lang/models/product_pricelist_direct_print.py
@@ -10,7 +10,8 @@ class ProductPricelistXlsx(models.AbstractModel):
             for lang in self.env["res.lang"].search([]):
                 next_col += 1
                 sheet.write(5, next_col, _(lang.name), header_format)
-            sheet.write(5, next_col, _("Default Code"), header_format)
+            # Override first header column with default code
+            sheet.write(5, 0, _("Default Code"), header_format)
         return next_col
 
     def _add_extra_info(self, sheet, book, product, row, next_col):
@@ -19,5 +20,6 @@ class ProductPricelistXlsx(models.AbstractModel):
             for lang in self.env["res.lang"].search([]):
                 next_col += 1
                 sheet.write(row, next_col, product.with_context(lang=lang.code).name)
-            sheet.write(row, next_col, product.default_code)
+            # Override first column with default code
+            sheet.write(row, 0, product.default_code)
         return next_col

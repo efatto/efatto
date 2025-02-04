2e7c84bb3d74830fe74fee2d1976811841f11702
sergiocorato <sergiocorato@gmail.com>
2025-02-04 10:33:51 +0100

    [IMP] add ability to print child categories

--- product_pricelist_direct_print_all_lang/wizards/product_pricelist_print.py
+++ product_pricelist_direct_print_all_lang/wizards/product_pricelist_print.py
@@ -1,7 +1,21 @@
 from odoo import fields, models
+from odoo.osv import expression
 
 
 class ProductPricelistPrint(models.TransientModel):
     _inherit = "product.pricelist.print"
 
     show_all_langs = fields.Boolean(string="Show All Langs")
+    show_child_categ = fields.Boolean(string="Show Child Categories")
+
+    def get_products_domain(self):
+        domain = super().get_products_domain()
+        if self.categ_ids and self.show_child_categ:
+            domain = expression.OR([
+                domain,
+                [
+                    ("sale_ok", "=", True),
+                    ("categ_id", "child_of", self.categ_ids.ids),
+                ]
+            ])
+        return domain

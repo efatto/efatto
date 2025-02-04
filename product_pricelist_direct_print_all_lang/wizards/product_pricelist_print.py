0181706b52b8d2d04d431f3728dbce50a7c41a88
sergiocorato <sergiocorato@gmail.com>
2025-02-04 12:40:52 +0100

    [IMP] create a dict of dict to order by category

--- product_pricelist_direct_print_all_lang/wizards/product_pricelist_print.py
+++ product_pricelist_direct_print_all_lang/wizards/product_pricelist_print.py
@@ -26,26 +26,31 @@ class ProductPricelistPrint(models.TransientModel):
 
     def get_group_key(self, product):
         max_level = self.max_categ_level or 99
-        return "%s ][ %s" % (
-            product.categ_id.sequence,
-            " / ".join(product.categ_id.complete_name.split(" / ")[:max_level]),
-        )
+        return {
+            product.categ_id.sequence: " / ".join(
+                product.categ_id.complete_name.split(" / ")[:max_level]
+            )
+        }
 
     def get_groups_to_print(self):
         self.ensure_one()
         products = self.get_products_to_print()
         if not products:
             return []
-        group_dict = defaultdict(lambda: products.browse())
+        seq_group_dict = {}
         for product in products:
-            key = self.get_group_key(product)
-            group_dict[key] |= product
+            key_dict = self.get_group_key(product)
+            for seq in key_dict:
+                if seq not in seq_group_dict:
+                    seq_group_dict.update({seq: defaultdict(lambda: products.browse())})
+                seq_group_dict[seq][key_dict[seq]] |= product
         group_list = []
-        for key in sorted(group_dict.keys()):
-            group_list.append(
-                {
-                    "group_name": key.split(" ][ ")[1],
-                    "products": self.get_sorted_products(group_dict[key]),
-                }
-            )
+        for seq in sorted(seq_group_dict.keys()):
+            for key in sorted(seq_group_dict[seq].keys()):
+                group_list.append(
+                    {
+                        "group_name": key,
+                        "products": self.get_sorted_products(seq_group_dict[seq][key]),
+                    }
+                )
         return group_list

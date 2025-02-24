80a48c95d5a9445124fe736744f17bee7f9fbf7a
sergiocorato <sergiocorato@gmail.com>
2025-02-24 15:32:32 +0100

    [FIX] other start pyenv

--- python_venv/python_venv.py
+++ python_venv/python_venv.py
@@ -32,14 +32,14 @@ def _create_python_venv(venv_path, py_version):
     if not os.path.isdir(os.path.join(os.path.expanduser("~"), ".pyenv")):
         subprocess.Popen(["curl -fsSL https://pyenv.run | bash"], shell=True).wait()
     # Load pyenv automatically by appending the following text
-    export_text = 'export PYENV_ROOT="$HOME/.pyenv\n"\
-    [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH\n"\
-    eval "$(pyenv init - bash)\n'
     for file in [".bash_profile", ".profile", ".bashrc"]:
         file_path = os.path.join(os.path.expanduser("~"), file)
         if not os.path.isfile(file_path):
             with open(file_path, "w") as f:
-                f.write(export_text)
+                f.write('export PYENV_ROOT="$HOME/.pyenv"')
+                f.write(
+                    '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH')
+                f.write('eval "$(pyenv init - bash)')
     # Restart your shell for the changes to take effect.
     # Load pyenv-virtualenv automatically by adding
     # the following to ~/.bashrc:

bcc69b2bfac739f465cd97d682ff478fb02f2d0c
sergiocorato <sergiocorato@gmail.com>
2025-04-17 17:04:40 +0200

    [IMP] add migration to account_analytic_mrp_extra_cost to recalculate extra cost on account analytic lines

---
+++ account_analytic_mrp_extra_cost/migrations/12.0.1.0.1/post-migration.py
@@ -0,0 +1,17 @@
+import logging
+from openupgradelib import openupgrade
+
+_logger = logging.getLogger(__name__)
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    lines = env['account.analytic.line'].search([])
+    i_max = len(lines)
+    i = 0
+    for line in lines:
+        i += 1
+        line._compute_extra_cost()
+        _logger.info('Recomputed analytic cost for analytic line #%s/%s' % (
+            i, i_max
+        ))

3222de1ac312766696c4878a848c9e290f68dce7
sergiocorato <sergiocorato@gmail.com>
2025-04-30 19:24:46 +0200

    [IMP] python venv

--- efatto/__manifest__.py
+++ efatto/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Python venv",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "license": "AGPL-3",

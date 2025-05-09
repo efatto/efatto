3ffe50ba98932822b65e53db21e648096042717c
sergiocorato <sergiocorato@gmail.com>
2025-05-09 09:17:07 +0200

    [IMP] make production notes writable

--- mrp_sale_info_link_notes/models/mrp_production.py
+++ mrp_sale_info_link_notes/models/mrp_production.py
@@ -4,5 +4,9 @@ from odoo import fields, models
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
-    production_notes = fields.Text(related="sale_id.production_notes", store=True)
+    production_notes = fields.Text(
+        related="sale_id.production_notes",
+        readonly=False,
+        store=True,
+    )
     is_prototype = fields.Boolean(related="sale_id.is_prototype", store=True)

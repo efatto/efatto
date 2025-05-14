b6b8ef978a66251961f4f9e361c340750a024cc4
sergiocorato <sergiocorato@gmail.com>
2025-05-14 15:55:38 +0200

    [IMP] add button to update price from stock.move.line

--- stock_picking_update_move_price/__manifest__.py
+++ stock_picking_update_move_price/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'Stock picking update move price',
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.0.2',
     'license': 'AGPL-3',
     'author': 'Sergio Corato',
     'website': 'https://github.com/efatto/efatto',

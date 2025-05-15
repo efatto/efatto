fb4b6e3715ee34d660d9d1f953b114b79770f09e
sergiocorato <sergiocorato@gmail.com>
2025-05-15 10:47:22 +0200

    [IMP] refactor method to compute qty_signed

--- stock_move_details/models/stock.py
+++ stock_move_details/models/stock.py
@@ -1,6 +1,3 @@
-# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 from odoo import api, fields, models
 
 
@@ -8,24 +5,37 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     @api.depends("product_uom_qty", "location_dest_id", "location_id")
-    def _set_sign_product_qty(self):
+    def _compute_sign_product_qty(self):
         for move in self:
-            move.qty_signed = move.product_uom_qty * (
-                -1
-                if move.location_dest_id.usage
+            # 1 in all cases exept the ones specified below
+            sign = 1
+            # -1 if from inventory to a return location
+            if (
+                move.location_id.usage == "inventory"
+                and move.location_dest_id.return_location
+            ):
+                sign = -1
+            # -1 if from internal to customer|inventory|production|supplier
+            if (
+                move.location_id.usage == "internal"
+                and move.location_dest_id.usage
                 in ["customer", "inventory", "production", "supplier"]
-                and move.location_id.usage == "internal"
-                else 0
-                if move.location_dest_id.usage
+            ):
+                sign = -1
+            # 0 if not from internal to customer|inventory|production|supplier
+            elif (
+                move.location_id.usage != "internal"
+                and move.location_dest_id.usage
                 in ["customer", "inventory", "production", "supplier"]
-                and move.location_id.usage != "internal"
-                else 0
-                if move.location_dest_id.usage == move.location_id.usage
-                else 1
-            )
+            ):
+                sign = 0
+            # 0 if the origin location is the same as the dest location
+            elif move.location_dest_id.usage == move.location_id.usage:
+                sign = 0
+            move.qty_signed = move.product_uom_qty * sign
 
     qty_signed = fields.Float(
-        compute=_set_sign_product_qty, store=True, group_operator="sum"
+        compute=_compute_sign_product_qty, store=True, group_operator="sum"
     )
 
 
@@ -33,22 +43,35 @@ class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
     @api.depends("qty_done", "location_dest_id", "location_id")
-    def _set_sign_product_qty(self):
+    def _compute_sign_product_qty(self):
         for line in self:
-            line.qty_signed = line.qty_done * (
-                -1
-                if line.location_dest_id.usage
+            # 1 in all cases exept the ones specified below
+            sign = 1
+            # -1 if from inventory to a return location
+            if (
+                line.location_id.usage == "inventory"
+                and line.location_dest_id.return_location
+            ):
+                sign = -1
+            # -1 if from internal to customer|inventory|production|supplier
+            if (
+                line.location_id.usage == "internal"
+                and line.location_dest_id.usage
                 in ["customer", "inventory", "production", "supplier"]
-                and line.location_id.usage == "internal"
-                else 0
-                if line.location_dest_id.usage
+            ):
+                sign = -1
+            # 0 if not from internal to customer|inventory|production|supplier
+            elif (
+                line.location_id.usage != "internal"
+                and line.location_dest_id.usage
                 in ["customer", "inventory", "production", "supplier"]
-                and line.location_id.usage != "internal"
-                else 0
-                if line.location_dest_id.usage == line.location_id.usage
-                else 1
-            )
+            ):
+                sign = 0
+            # 0 if the origin location is the same as the dest location
+            elif line.location_dest_id.usage == line.location_id.usage:
+                sign = 0
+            line.qty_signed = line.qty_done * sign
 
     qty_signed = fields.Float(
-        compute=_set_sign_product_qty, store=True, group_operator="sum"
+        compute=_compute_sign_product_qty, store=True, group_operator="sum"
     )

392b243c3c183583638cb2267a577268f9d79fb6
sergiocorato <sergiocorato@gmail.com>
2025-07-16 22:35:53 +0200

    [IMP] stock_transfer_button: black, isort, prettier

--- stock_transfer_button/__manifest__.py
+++ stock_transfer_button/__manifest__.py
@@ -1,18 +1,18 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Stock transfer button in dashboard',
-    'summary': 'Add button for planned transfer in stock dashboard kanban',
-    'version': '12.0.1.0.0',
-    'author': 'Sergio Corato',
-    'category': 'Warehouse',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/efatto/efatto',
-    'depends': [
-        'stock',
+    "name": "Stock transfer button in dashboard",
+    "summary": "Add button for planned transfer in stock dashboard kanban",
+    "version": "14.0.1.0.0",
+    "author": "Sergio Corato",
+    "category": "Warehouse",
+    "license": "AGPL-3",
+    "website": "https://github.com/efatto/efatto",
+    "depends": [
+        "stock",
     ],
-    'data': [
-        'views/stock_view.xml',
+    "data": [
+        "views/stock_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }

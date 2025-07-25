53a23e0f0d1fbdc04251092ffe225d83932ccc69
sergiocorato <sergiocorato@gmail.com>
2025-07-25 17:18:25 +0200

    [IMP] add field to filter stock.move without analytic lines assigned to mrp

--- account_analytic_mrp_extra_cost/models/mis_report.py
+++ account_analytic_mrp_extra_cost/models/mis_report.py
@@ -15,7 +15,7 @@ class MisReportInstance(models.Model):
                 analytic_account_id = domain_item[2]
         if analytic_account_id:
             for i, domain_item in enumerate(domain):
-                if domain_item[0] == "has_mrp_raw_moves":
+                if domain_item[0] in ["has_mrp_raw_moves", "has_mrp_analytic_lines"]:
                     domain[i] = (
                         (domain_item[0], domain_item[1], analytic_account_id)
                     )
--- account_analytic_mrp_extra_cost/models/stock_move.py
+++ account_analytic_mrp_extra_cost/models/stock_move.py
@@ -4,17 +4,62 @@ from odoo import api, fields, models
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    mrp_analytic_line_ids = fields.Many2many(
-        comodel_name="account.analytic.line",
-        compute="_compute_mrp_analytic_line_ids",
-        string="Mrp account analytic lines",
-        # not storable
+    has_mrp_analytic_lines = fields.Boolean(
+        compute="_compute_has_mrp_analytic_lines",
+        search="_search_has_mrp_analytic_lines",
     )
 
-    def _compute_mrp_analytic_line_ids(self):
-        for move in self:
-            move.mrp_analytic_line_ids = self.env['account.analytic.line'].search([
+    def _get_analytic_info(self):
+        self.ensure_one()
+        analytic_lines = self.env['account.analytic.line'].browse()
+        if (
+            self.state != "cancel"
+            and self.product_id
+            and self.raw_material_production_id
+        ):
+            analytic_lines = self.env['account.analytic.line'].search([
                 ('account_id', '=',
-                 move.raw_material_production_id.analytic_account_id.id),
-                ('product_id', '=', move.product_id.id),
+                 self.raw_material_production_id.analytic_account_id.id),
+                ('product_id', '=', self.product_id.id),
+                ('amount', '<', 0),
             ])
+        return analytic_lines
+
+    def _compute_has_mrp_analytic_lines(self):
+        for line in self:
+            analytic_lines = line._get_analytic_info()
+            line.has_mrp_analytic_lines = bool(analytic_lines)
+
+    @api.model
+    def _search_has_mrp_analytic_lines(self, operator, value):
+        if self.env.context.get("mis_report_filters"):
+            lines = self.env['stock.move'].browse()
+            if self.env.context.get("mis_report_filters"):
+                mis_report_filters = self.env.context.get("mis_report_filters")
+                if mis_report_filters.get("analytic_account_id"):
+                    dict_domain = mis_report_filters.get("analytic_account_id")
+                    lines = self.env['stock.move'].search([
+                        ("product_id", "!=", False),
+                        ("raw_material_production_id.analytic_account_id",
+                         dict_domain["operator"], dict_domain["value"]),
+                    ])
+            elif value:
+                lines = self.env['stock.move'].search([
+                    ("product_id", "!=", False),
+                    ("raw_material_production_id.analytic_account_id", "=", value),
+                ])
+            if operator == "!=":
+                # this domain is [('has_mrp_analytic_lines', '!=', False)]
+                # so we return the lines which has analytic lines with a value or not
+                filtered_lines = lines.filtered(
+                    lambda l: l._get_analytic_info()
+                )
+                return [("id", "in", filtered_lines.ids)]
+            elif operator == "=":
+                # this domain is [('has_mrp_analytic_lines', '=', False)]
+                # so we return the lines which hasn't analytic lines with a value or not
+                filtered_lines = lines.filtered(
+                    lambda l: not l._get_analytic_info()
+                )
+                return [("id", "in", filtered_lines.ids)]
+        return [("id", operator, value)]

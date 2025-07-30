8f6f03b7d7544a2c0d6617ec99425fa751f66ac8
sergiocorato <sergiocorato@gmail.com>
2025-07-30 11:18:08 +0200

    [IMP] stock_picking_update_move_price: black, isort, prettier

--- stock_picking_update_move_price/__manifest__.py
+++ stock_picking_update_move_price/__manifest__.py
@@ -1,21 +1,21 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Stock picking update move price',
-    'version': '12.0.1.0.2',
-    'license': 'AGPL-3',
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/efatto/efatto',
-    'category': 'other',
-    'depends': [
-        'mrp',
-        'stock',
+    "name": "Stock picking update move price",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/efatto",
+    "category": "other",
+    "depends": [
+        "mrp",
+        "stock",
     ],
-    'data': [
-        'wizard/update_stock_move_price.xml',
-        'views/stock_picking.xml',
+    "data": [
+        "wizard/update_stock_move_price.xml",
+        "views/stock_picking.xml",
     ],
-    'summary': "This module add an action on stock move to update stock move price "
-               "unit for evaluation purposes.",
-    'installable': True,
+    "summary": "This module add an action on stock move to update stock move price "
+    "unit for evaluation purposes.",
+    "installable": True,
 }
--- stock_picking_update_move_price/wizard/update_stock_move_price.py
+++ stock_picking_update_move_price/wizard/update_stock_move_price.py
@@ -1,24 +1,26 @@
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class UpdateStockMovePriceWizard(models.TransientModel):
-    _name = 'update.stock.move.price'
-    _description = 'Update stock move price'
+    _name = "update.stock.move.price"
+    _description = "Update stock move price"
 
     new_price = fields.Float(required=True, default=0)
 
-    @api.multi
     def update_stock_move_price(self):
         if self.env.context.get("active_model") == "stock.move.line":
-            move_line_ids = self.env.context.get('active_ids', False)
-            moves = self.env['stock.move.line'].browse(move_line_ids).mapped('move_id')
+            move_line_ids = self.env.context.get("active_ids", False)
+            moves = self.env["stock.move.line"].browse(move_line_ids).mapped("move_id")
         else:
-            move_ids = self.env.context.get('active_ids', False)
-            moves = self.env['stock.move'].browse(move_ids)
+            move_ids = self.env.context.get("active_ids", False)
+            moves = self.env["stock.move"].browse(move_ids)
         for move in moves:
             # ensure only outgoing move price is negative, leave other decisions to user
-            sign = -1 if move.picking_type_id.code == 'outgoing' else 1
-            move.write({
-                'price_unit': sign * abs(self.new_price) if sign == -1
-                else self.new_price
-            })
+            sign = -1 if move.picking_type_id.code == "outgoing" else 1
+            move.write(
+                {
+                    "price_unit": sign * abs(self.new_price)
+                    if sign == -1
+                    else self.new_price
+                }
+            )

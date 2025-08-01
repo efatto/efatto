1aa988ad3cc309920edddb96c1c7a26f7ba17809
sergiocorato <sergiocorato@gmail.com>
2025-08-01 14:59:10 +0200

    [FIX] update from all product invoice lines

--- account_stock_price_unit_sync_analytic/models/account_invoice.py
+++ account_stock_price_unit_sync_analytic/models/account_invoice.py
@@ -16,7 +16,6 @@ class AccountInvoice(models.Model):
                     ('product_id', 'in', product_invoice_line_ids.mapped(
                         'product_id.id')),
                     ('invoice_type', '=', 'in_invoice'),
-                    ('exclude_extra_cost', '=', False),
                 ])
                 product_invoice_lines |= product_invoice_line_ids
                 if product_invoice_lines:

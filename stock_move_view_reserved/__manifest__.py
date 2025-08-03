808509ea0c90a76d1f10e78664a350384b3d9ecc
sergiocorato <sergiocorato@gmail.com>
2025-08-03 22:01:23 +0200

    [IMP] stock_move_view_reserved: black, isort, prettier

--- stock_move_view_reserved/__manifest__.py
+++ stock_move_view_reserved/__manifest__.py
@@ -2,21 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Stock move reserved qty in tree',
-    'version': '12.0.1.0.2',
-    'category': 'other',
-    'description': """
-    Show stock move reserved quantity and date in tree view.
-    """,
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/efatto/efatto',
-    'license': 'AGPL-3',
-    'depends': [
-        'mrp',
-        'stock_move_view_sale_purchase',
+    "name": "Stock move reserved qty in tree",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "summary": "Show stock move reserved quantity and date in tree view",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+        "stock_move_view_sale_purchase",
     ],
-    'data': [
-        'views/stock.xml',
+    "data": [
+        "views/stock.xml",
     ],
-    'installable': True,
+    "installable": True,
 }

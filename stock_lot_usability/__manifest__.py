7305d4f31e5d903c1793f1ffec5229429e359096
sergiocorato <sergiocorato@gmail.com>
2025-08-03 16:47:05 +0200

    [IMP] stock_lot_usability: black, isort, prettier

--- stock_lot_usability/__manifest__.py
+++ stock_lot_usability/__manifest__.py
@@ -1,20 +1,20 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Stock lot usability',
-    'version': '12.0.1.0.0',
-    'category': 'other',
-    'description': """
+    "name": "Stock lot usability",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "summary": """
     Stock lot qty in tree and removed groupby
     """,
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/efatto/efatto',
-    'license': 'AGPL-3',
-    'depends': [
-        'stock',
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
     ],
-    'data': [
-        'views/stock.xml',
+    "data": [
+        "views/stock.xml",
     ],
-    'installable': True,
+    "installable": True,
 }

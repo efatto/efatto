3f4f096d51c3f14890263c3fd0aade10a15e8b3d
sergiocorato <sergiocorato@gmail.com>
2025-08-12 22:23:33 +0200

    [IMP] module installation in venv

--- efatto/python_venv.py
+++ efatto/python_venv.py
@@ -18,11 +18,13 @@ def _get_env_for_subprocess(folder, py_version):
     if pip_extra_index_url:
         env_for_subprocess["PIP_EXTRA_INDEX_URL"] = pip_extra_index_url
     pyenv_path = os.path.join(os.path.expanduser("~"), ".pyenv")
+    uv_path = os.path.join(os.path.expanduser("~"), ".local", "bin", "uv")
     env_for_subprocess["PATH"] = ":".join(
         [
             os.path.join(pyenv_path, "bin"),
             folder,
             os.path.join(folder, "bin"),
+            uv_path,
             "/bin",
             "/usr/bin",
         ]
@@ -42,23 +44,17 @@ def _create_python_venv(venv_path, py_version):
     if not os.path.isdir(venv_path):
         subprocess.Popen([f"mkdir -p {venv_path}"], shell=True).wait()
         # do not recreate virtualenv as it regenerates file with bug in split()
-    if not os.path.isdir(os.path.join(os.path.expanduser("~"), ".pyenv")):
-        subprocess.Popen(["curl -fsSL https://pyenv.run | bash"], shell=True).wait()
+    # if not os.path.isdir(os.path.join(os.path.expanduser("~"), ".pyenv")):
+    #     subprocess.Popen(["curl -fsSL https://pyenv.run | bash"], shell=True).wait()
     # Copy some pip configuration files that could exist in local to the python venv
     subprocess.Popen(
-        [f"pyenv install -s {py_version}"],
+        [f"uv python install {py_version}"],
         cwd=venv_path,
         env=subprocess_env,
         shell=True,
     ).wait()
-    pyenv_path = os.path.join(
-        os.path.expanduser("~"),
-        ".pyenv",
-        "versions",
-        py_version,
-    )
     subprocess.Popen(
-        [f"{pyenv_path}/bin/python -m venv {venv_path}"],
+        [f"uv venv {venv_path}"],
         cwd=venv_path,
         env=subprocess_env,
         shell=True,

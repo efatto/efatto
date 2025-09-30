c61d0ab71f09ba2b7038e009080018e10a92654d
sergiocorato <sergiocorato@gmail.com>
2025-09-30 11:37:30 +0200

    [MIG] l10n_it_dn_force_number_done to 14.0

---
+++ l10n_it_dn_force_number_done/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2018 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Change DDT Number on done state",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "summary": "Allows ddt change number on done state.",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_it_delivery_note",
+    ],
+    "data": [],
+    "installable": True,
+}

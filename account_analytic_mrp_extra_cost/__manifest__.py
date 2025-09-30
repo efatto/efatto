414531ca2fca8d63c87cbf826b1344cd936bc3d3
sergiocorato <sergiocorato@gmail.com>
2025-09-30 10:54:07 +0200

    [MIG] account_analytic_mrp_extra_cost

--- account_analytic_mrp_extra_cost/__manifest__.py
+++ account_analytic_mrp_extra_cost/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Invoice analytic line mrp actual cost",
     "summary": "Compute actual mrp cost from analytic line from invoices",
-    "version": "12.0.1.1.0",
+    "version": "14.0.1.0.0",
     "category": "Accounting",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/efatto",

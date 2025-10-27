4689a5c7d52424da4e923ef2091928571da894b8
sergiocorato <sergiocorato@gmail.com>
2025-10-27 19:26:55 +0100

    [ADD] module to block auto backup on server env

---
+++ server_env_auto_backup/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Server Env Auto Backup",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "auto_backup",
+        "server_env",
+    ],
+    "installable": True,
+}

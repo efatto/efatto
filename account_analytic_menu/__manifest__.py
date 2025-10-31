c67a04ae5b0b60afd9eb94abee520984588ef1e1
sergiocorato <sergiocorato@gmail.com>
2025-10-31 22:14:07 +0100

    [upd]

--- account_analytic_menu/__manifest__.py
+++ account_analytic_menu/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Account analytic menu in Customers",
+    "name": "Account analytic menu in Customer",
     "version": "14.0.1.0.0",
     "category": "Sale Management",
     "summary": "Add account analytic menu in Invoicing -> Customers.",

e23ebb8e22899872b97139be91a83ad5f11fda42
sergiocorato <sergiocorato@gmail.com>
2025-12-31 11:49:06 +0100

    [MIG] link_tracker_partner to 16.0

--- link_tracker_partner/__manifest__.py
+++ link_tracker_partner/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Link tracker show partners",
     "summary": "Add ability to open partners created with a link tracker",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/sergiocorato/efatto",
     "author": "Sergio Corato",

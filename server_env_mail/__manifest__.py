1ec56980e581c18960a7c49aa5781f64eb51a09c
sergiocorato <sergiocorato@gmail.com>
2025-12-31 15:09:59 +0100

    [MIG] server_env_mail to 16.0

--- server_env_mail/__manifest__.py
+++ server_env_mail/__manifest__.py
@@ -2,13 +2,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Server Env Mail",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/efatto",
     "license": "AGPL-3",
     "depends": [
-        "fetchmail",
         "mail",
         "server_env",
     ],

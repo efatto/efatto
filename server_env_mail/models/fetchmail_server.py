933a9f4998665fefabf8e4e2bb61bebc68cbad52
sergiocorato <sergiocorato@gmail.com>
2025-12-31 15:09:59 +0100

    [MIG] server_env_mail to 16.0

--- server_env_mail/models/fetchmail_server.py
+++ server_env_mail/models/fetchmail_server.py
@@ -1,6 +1,3 @@
-# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
 from odoo import models
 
 from odoo.addons.server_env.models.server import running

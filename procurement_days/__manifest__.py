f5f607e5a606b7ac2051fe1c0aff5a8cfc6121e5
sergiocorato <sergiocorato@gmail.com>
2026-01-05 17:31:15 +0000

    [MIG] procurement_days to 16.0

--- procurement_days/__manifest__.py
+++ procurement_days/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Procurement days",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/efatto",
     "summary": "Add a procurement delay days without changing lead date.",

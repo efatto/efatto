a59c608d33fe4ec633c33c28276449f60536c002
sergiocorato <sergiocorato@gmail.com>
2026-01-05 17:27:58 +0000

    [MIG] delivery_fix_price to 16.0

--- delivery_fix_price/__manifest__.py
+++ delivery_fix_price/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Delivery fix price",
-    "version": "14.0.1.0.1",
+    "version": "16.0.1.0.1",
     "category": "other",
     "author": "Sergio Corato",
     "summary": "Delivery fix price wich is computed with vat [this part can be "

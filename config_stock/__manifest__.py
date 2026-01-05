04d94f031ad33ff078a6be95b8632a8431731f4e
sergiocorato <sergiocorato@gmail.com>
2026-01-05 17:26:49 +0000

    [MIG] config_stock to 16.0

--- config_stock/__manifest__.py
+++ config_stock/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Module to configure stock",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "other",
     "summary": """
 Module to configure stock.

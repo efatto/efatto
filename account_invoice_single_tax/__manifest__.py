e07bf3678731f0d9d113a15714f5a37a1fea957f
sergiocorato <sergiocorato@gmail.com>
2026-01-05 15:48:50 +0100

    [IMP] account_invoice_single_tax: pre-commit auto fixes

--- account_invoice_single_tax/__manifest__.py
+++ account_invoice_single_tax/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2019-24 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2019 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Single tax restrict in invoice",
-    "version": "14.0.1.0.1",
+    "version": "16.0.1.0.1",
     "author": "Sergio Corato",
-    "website": "https://github.com/efatto/efatto",
+    "website": "https://github.com/sergiocorato/efatto",
     "category": "Localisation / Accounting",
     "license": "AGPL-3",
     "depends": ["account"],

9abd09f7e50edd0cdc3829b114885fee70536ec7
sergiocorato <sergiocorato@gmail.com>
2026-01-05 17:38:14 +0000

    [MIG] stock_move_deadline_writeable to 16.0

--- stock_move_deadline_writeable/__manifest__.py
+++ stock_move_deadline_writeable/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock move deadline writeable",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "other",
     "summary": """
     Enable write on deadline on stock move

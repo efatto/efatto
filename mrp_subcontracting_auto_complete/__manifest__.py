311ba303015a718281f999c8540c62ad1f5b93e0
sergiocorato <sergiocorato@gmail.com>
2026-01-05 17:30:33 +0000

    [MIG] mrp_subcontracting_auto_complete to 16.0

--- mrp_subcontracting_auto_complete/__manifest__.py
+++ mrp_subcontracting_auto_complete/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "MRP Subcontracting auto complete on IN validation",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/efatto",
     "summary": "Validate a IN picking with submanufactured products will "

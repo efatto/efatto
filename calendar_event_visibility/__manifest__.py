d51e2468025d5d32a8a4d6fb8098e17e577abd26
sergiocorato <sergiocorato@gmail.com>
2026-01-05 17:26:07 +0000

    [MIG] calendar_event_visibility to 16.0

--- calendar_event_visibility/__manifest__.py
+++ calendar_event_visibility/__manifest__.py
@@ -2,10 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Event visibility",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
-    "summary": "Limit visibility of event to involved users, excluding hr employee.",
     "website": "https://github.com/efatto/efatto",
     "license": "AGPL-3",
     "depends": [

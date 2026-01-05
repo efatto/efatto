6848a2406187a8571d02207748f5e818543110d1
sergiocorato <sergiocorato@gmail.com>
2026-01-05 16:07:10 +0100

    [FIX] modules website

--- link_tracker_partner/__manifest__.py
+++ link_tracker_partner/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "Add ability to open partners created with a link tracker",
     "version": "16.0.1.0.0",
     "category": "Sales",
-    "website": "https://github.com/sergiocorato/efatto",
+    "website": "https://github.com/efatto/efatto",
     "author": "Sergio Corato",
     "maintainers": ["sergiocorato"],
     "license": "AGPL-3",

f4426d65a954903c9636fb6827f47e9efe9deaca
sergiocorato <sergiocorato@gmail.com>
2026-01-05 17:29:37 +0000

    [MIG] mail_activity_open_origin to 16.0

--- mail_activity_open_origin/__manifest__.py
+++ mail_activity_open_origin/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Mail activity open origin",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Social",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/efatto",

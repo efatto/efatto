a04606cd0425cc7005a4e68a1f7c699a9e93cfa2
sergiocorato <sergiocorato@gmail.com>
2026-01-05 17:35:22 +0000

    [MIG] sale_order_calendar_state_report to 16.0

--- sale_order_calendar_state_report/__manifest__.py
+++ sale_order_calendar_state_report/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Order Calendar State Report",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "summary": "Statistics from sale order calendar state",
     "license": "AGPL-3",
     "category": "Manufacturing",

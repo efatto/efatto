051fffb9a9813958d0d6b82d14c7b26d8988b24c
sergiocorato <sergiocorato@gmail.com>
2026-01-05 17:34:24 +0000

    [MIG] sale_bookmark to 16.0

--- sale_bookmark/__manifest__.py
+++ sale_bookmark/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Bookmark",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/efatto",
     "category": "Tools",

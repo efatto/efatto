05dd60fb5dc9278795de2899c20cf3fa2e35a464
sergiocorato <sergiocorato@gmail.com>
2026-01-05 17:32:12 +0000

    [MIG] product_supplierinfo_overtime_delay to 16.0

--- product_supplierinfo_overtime_delay/__manifest__.py
+++ product_supplierinfo_overtime_delay/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Product sellers overtime delay",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "other",
     "summary": "Add product seller overtime delay computed on purchases.",
     "author": "Sergio Corato",

696b6bac06c5a015e39167c3070bd1775dbff362
sergiocorato <sergiocorato@gmail.com>
2026-01-05 17:35:08 +0000

    [MIG] sale_order_calendar_state to 16.0

--- sale_order_calendar_state/__manifest__.py
+++ sale_order_calendar_state/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/efatto",
     "license": "AGPL-3",
-    "version": "14.0.1.0.1",
+    "version": "16.0.1.0.1",
     "summary": "Add states to sale order based on manufacturing, deliveries and "
     "purchase, used in calendar view.",
     "depends": [

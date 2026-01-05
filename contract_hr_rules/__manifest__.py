28a9b6cdc94e9b99e9a10603bcdf2b5926ab4fc9
sergiocorato <sergiocorato@gmail.com>
2026-01-05 17:27:15 +0000

    [MIG] contract_hr_rules to 16.0

--- contract_hr_rules/__manifest__.py
+++ contract_hr_rules/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Contract, analytic line and hr rules custom",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "summary": "Contract and hr rules custom",

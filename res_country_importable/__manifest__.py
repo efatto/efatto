6b67c07db1c10ce976d75226731f55ef1cd3d8c5
sergiocorato <sergiocorato@gmail.com>
2026-01-07 09:45:15 +0100

    [ADD] new module res_country_importable

---
+++ res_country_importable/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Res Country Importable",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_country.xml",
+    ],
+    "installable": True,
+}

2b3d19a79b2cde071e6ed115dd3f4994e7cbd64a
sergiocorato <sergiocorato@gmail.com>
2026-01-08 09:46:11 +0000

    [MIG] stock_reserve_date_check to 16.0

--- stock_reserve_date_check/__manifest__.py
+++ stock_reserve_date_check/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Stock reserve date check",
-    "version": "14.0.1.0.1",
+    "version": "16.0.1.0.1",
     "category": "Stock Management",
     "author": "Sergio Corato",
     "license": "AGPL-3",

18949c1435598b8886a13039922fed5a0cc1203b
sergiocorato <sergiocorato@gmail.com>
2026-01-09 16:41:55 +0000

    [MIG] auto_backup_healthcheck to 16.0

--- auto_backup_healthcheck/__manifest__.py
+++ auto_backup_healthcheck/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Auto backup healthcheck",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "summary": "Add healthcheck autobackup info to remote service.",

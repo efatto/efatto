a91b0228819ffc46556772c5d490c4c62b77979e
sergiocorato <sergiocorato@gmail.com>
2026-01-15 18:21:08 +0000

    [MIG] server_env_auto_backup to 16.0

--- server_env_auto_backup/__manifest__.py
+++ server_env_auto_backup/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Server Env Auto Backup",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/efatto",

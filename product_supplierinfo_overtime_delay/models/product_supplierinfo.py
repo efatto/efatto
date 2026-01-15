522eaf907501c5e7a491f2f8a4ea0521a5a59d12
sergiocorato <sergiocorato@gmail.com>
2026-01-15 10:30:39 +0100

    [FIX] removed normal_purchase_delay product_supplierinfo_overtime_delay

--- product_supplierinfo_overtime_delay/models/product_supplierinfo.py
+++ product_supplierinfo_overtime_delay/models/product_supplierinfo.py
@@ -13,14 +13,6 @@ class SupplierInfo(models.Model):
         "of days over time of a minimum 3 days for this vendor to deliver this "
         "product.",
     )
-    normal_purchase_delay = fields.Integer(
-        compute="_compute_overtime_purchase_delay",
-        store=True,
-        help="Over the past x days (configured in parameter "
-        "'purchase_stock.on_time_delivery_days', by default 365 days): the number "
-        "of days for this vendor to deliver this product, computed on the first "
-        "delivery date.",
-    )
     overtime_move_ids = fields.Many2many(
         comodel_name="stock.move",
         compute="_compute_overtime_purchase_delay",
@@ -31,7 +23,6 @@ class SupplierInfo(models.Model):
     def _compute_overtime_purchase_delay(self):
         for seller in self:
             overtime_purchase_delay = 0
-            normal_purchase_delay = 0
             overtime_move_ids = False
             if seller.name.purchase_line_ids:
                 date_order_days_delta = int(
@@ -66,30 +57,7 @@ class SupplierInfo(models.Model):
                         for m in overtime_moves
                     ) / len(overtime_moves)
                     overtime_move_ids = overtime_moves
-                delivery_move_ids = order_lines.move_ids.filtered(
-                    lambda m: m.state == "done"
-                    and (
-                        m.product_id == seller.product_id
-                        or m.product_id.product_tmpl_id == seller.product_tmpl_id
-                    )
-                ).sorted(lambda m: m.date)
-                first_delivery_move_ids = self.env["stock.move"]
-                for order_line in order_lines:
-                    first_delivery_move_ids |= delivery_move_ids.filtered(
-                        lambda m, o=order_line: m.purchase_line_id == o
-                    )[:1]
-                if first_delivery_move_ids:
-                    normal_purchase_delay = (
-                        sum(
-                            (
-                                fdm_id.date.date()
-                                - fdm_id.purchase_line_id.order_id.date_order.date()
-                            ).days
-                            for fdm_id in first_delivery_move_ids
-                        )
-                    ) / len(first_delivery_move_ids)
             seller.overtime_purchase_delay = overtime_purchase_delay
-            seller.normal_purchase_delay = normal_purchase_delay
             seller.overtime_move_ids = overtime_move_ids
 
     def open_view_stock_move_overtime(self):

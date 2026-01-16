9bc9b27dade4edb1a832629482c470494dfa7303
sergiocorato <sergiocorato@gmail.com>
2026-01-16 15:06:25 +0000

    [MIG] l10n_it_dn_force_number to 16.0

--- l10n_it_dn_force_number/__manifest__.py
+++ l10n_it_dn_force_number/__manifest__.py
@@ -2,16 +2,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Force DDT Number",
-    "version": "14.0.1.0.1",
+    "version": "16.0.1.0.1",
     "category": "Stock",
-    "summary": """
-This module allows to force the ddt numbering.\n
-It add the ddt name field to ddt wizard creation. If user fills that field, the typed
-value will be used as ddt number.\n
-Otherwise, the next sequence number will be retrieved and saved.\n
-So, the new field has to be used when user doesn't want to use the default
-ddt numbering for a specific ddt.
-    """,
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/efatto",
     "license": "AGPL-3",
---
+++ l10n_it_dn_force_number/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+This module allows to force the ddt numbering.
+
+It add the ddt name field to ddt wizard creation. If user fills that field, the typed value will be used as ddt number.
+Otherwise, the next sequence number will be retrieved and saved.
+So, the new field has to be used when user doesn't want to use the default ddt numbering for a specific ddt.

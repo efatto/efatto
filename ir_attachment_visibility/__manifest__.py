a519836797f6f670650946ea15d0375b3db33942
sergiocorato <sergiocorato@gmail.com>
2026-01-16 14:46:52 +0000

    [MIG] ir_attachment_visibility to 16.0

--- ir_attachment_visibility/__manifest__.py
+++ ir_attachment_visibility/__manifest__.py
@@ -2,10 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Attachment visibility",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
-    "summary": "Limit visibility of attachment to creator user, excluding erp manager.",
     "website": "https://github.com/efatto/efatto",
     "license": "AGPL-3",
     "depends": [
---
+++ ir_attachment_visibility/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module limit visibility of attachment to the user the created them and the erp manager.

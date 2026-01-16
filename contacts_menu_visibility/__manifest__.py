45ae17678f2cc5201b2ef267f4eeea96a9f106fd
sergiocorato <sergiocorato@gmail.com>
2026-01-16 07:54:33 +0000

    [MIG] contacts_menu_visibility to 16.0

--- contacts_menu_visibility/__manifest__.py
+++ contacts_menu_visibility/__manifest__.py
@@ -2,10 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Hide contacts menu for basic user",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
-    "summary": "Hide contacts menu for basic user.",
     "website": "https://github.com/efatto/efatto",
     "license": "AGPL-3",
     "depends": [

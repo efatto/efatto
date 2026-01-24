d64cc9aacb3ee4fcc4c29e607bf7db4dff8ee9a4
sergiocorato <sergiocorato@gmail.com>
2026-01-24 08:44:05 +0000

    [MIG] crm_res_partner_exist to 16.0

--- crm_res_partner_exist/__manifest__.py
+++ crm_res_partner_exist/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "CRM lead with existing partner",
     "summary": "Add ability to filter crm lead with existing but not linked partner",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/efatto/efatto",
     "author": "Sergio Corato",

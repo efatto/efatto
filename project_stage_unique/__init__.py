57523d0191dd12483de963e776dd5dd0519b13f1
sergiocorato <sergiocorato@gmail.com>
2026-02-09 15:32:32 +0100

    [WIIP] check ability to do

--- project_stage_unique/__init__.py
+++ project_stage_unique/__init__.py
@@ -1,3 +1,5 @@
+from psycopg2 import sql
+
 from . import models
 import logging
 
@@ -6,10 +8,42 @@ logger = logging.getLogger(__name__)
 
 def pre_init_hook(cr):
     logger.info("Update project task type name to be unique")
-    # This work for source term, translation terms are not considered.
-    cr.execute(
-        """
-        UPDATE project_task_type SET name = CONCAT(name, '_', id::text) WHERE
-            id not in (SELECT min(id) from project_task_type group by name)
-    """
+    # This work for source and translation terms
+    # cr.execute("SELECT code FROM res_lang WHERE active=true")
+    # lang_codes = [x[0] for x in cr.fetchall()]
+    # UPDATE project_task_type
+    # SET name = jsonb_set(
+    #     name,
+    #     '{en_US}',
+    #     '"mieao"'::jsonb) where name @> '{"en_US": "miea"}'::jsonb;
+    # for lang_code in lang_codes:
+    # set the uniqueness only on the default language
+    # es. funzionante:
+    # update utm_source set name = jsonb_set(
+    # name, '{en_US}', '"Novità fattura elettronica 2021 (copia1)"') where id = 8;
+    # es. lettura: select name->'en_US' from utm_source; (non funziona con -->)
+    lang_code = "en_US"
+    new_name_field = sql.SQL("{field}-->{key}").format(
+        field=sql.Literal("name"), key=sql.Literal(lang_code)
     )
+    field_position = sql.SQL(f"{{{lang_code}}}")
+    query = sql.SQL(
+        """
+            UPDATE project_task_type
+                SET name = jsonb_set(
+                    name,
+                    '{en_US}',
+                    CONCAT(
+                        ''"', name->'en_US', '_', id, '"''
+                    )
+                )
+                WHERE id not in (
+                    SELECT min(id) from project_task_type group by name-->'en_US'
+                )
+        """
+    ).format(
+        new_name_field=new_name_field,
+        field_name=sql.Identifier("name"),
+        field_position=field_position,
+    )
+    cr.execute(query)
--- project_stage_unique/models/project.py
+++ project_stage_unique/models/project.py
@@ -1,12 +1,19 @@
-from odoo import _, api, models
-from odoo.exceptions import UserError
+from odoo import models
 
 
 class ProjectTaskType(models.Model):
     _inherit = "project.task.type"
 
-    @api.constrains("name")
-    def _constrains_name_unique(self):
-        for rec in self:
-            if self.search_count([("name", "=", rec.name), ("id", "!=", rec.id)]):
-                raise UserError(_("A task type with the same name already exists!"))
+    # @api.constrains("name")
+    # def _constrains_name_unique(self):
+    #     for rec in self:
+    #         if self.search_count([("name", "=", rec.name), ("id", "!=", rec.id)]):
+    #             raise UserError(_("A task type with the same name already exists!"))
+
+    _sql_constraints = [
+        (
+            "name_first_lang_uniq",
+            "UNIQUE (name->>'en_US')",
+            "Task type name must be unique.",
+        )
+    ]

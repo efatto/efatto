b2a82de5131c27686e5f413fbcb8aa49e67a6ced
sergiocorato <sergiocorato@gmail.com>
2026-03-10 15:31:53 +0100

    [FIX] get slot for workorder from the first available date of components

--- stock_reserve_date_check/models/sale_order_line.py
+++ stock_reserve_date_check/models/sale_order_line.py
@@ -166,10 +166,12 @@ class SaleOrderLine(models.Model):
                             op_start_dt,
                             op_end_dt,
                         ) = operation.workcenter_id._get_first_available_slot(
-                            max(
-                                fields.Datetime.to_datetime(start_date),
-                                (self.commitment_date or commitment_date),
-                            ),
+                            # min(
+                            fields.Datetime.to_datetime(start_date),
+                            # (self.commitment_date or commitment_date)
+                            # todo + relativedelta(-giorni di produzione totali? e come
+                            #  aggiungere i tempi di lavorazione/ricezione dei figli?),
+                            # ),
                             operation.time_cycle_manual,
                         )
                         op_start_date = op_start_dt.date()

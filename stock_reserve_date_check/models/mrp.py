03d83a1bd4673e647667086ab19370c2fa758262
sergiocorato <sergiocorato@gmail.com>
2026-03-16 19:09:49 +0100

    [IMP] add constrains on date planned start of production and commitment date

---
+++ stock_reserve_date_check/models/mrp.py
@@ -0,0 +1,24 @@
+from odoo import _, api, models
+from odoo.exceptions import UserError
+
+
+class MrpProduction(models.Model):
+    _inherit = "mrp.production"
+
+    @api.constrains("date_planned_start")
+    def _check_commitment_date(self):
+        # check that date_planned_start is greater than commitment_date
+        if (
+            self.date_planned_start
+            and self.commitment_date
+            and self.commitment_date < self.date_planned_start
+        ):
+            raise UserError(
+                _(
+                    "Production start date {date_planned_start} cannot be after "
+                    "commitment date {commitment_date}"
+                ).format(
+                    date_planned_start=self.date_planned_start,
+                    commitment_date=self.commitment_date,
+                )
+            )

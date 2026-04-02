603df069bc37c63896a8c44bd659dd24fe77b0d2
sergiocorato <sergiocorato@gmail.com>
2026-04-02 18:38:58 +0200

    [MIG] account_invoice_constraint_chronology_supplier: Migration to 18.0

--- account_invoice_constraint_chronology_supplier/model/account_move.py
+++ account_invoice_constraint_chronology_supplier/model/account_move.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2019 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2015 ACSONE SA/NV (<http://acsone.eu>)
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 

a921534e56358cf9f4db86598375df559d6d4f88
sergiocorato <sergiocorato@gmail.com>
2026-04-02 18:47:19 +0200

    [MIG] l10n_it_dn_force_number: Migration to 18.0

--- l10n_it_dn_force_number/__manifest__.py
+++ l10n_it_dn_force_number/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Force DDT Number",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.0",
     "category": "Stock",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/efatto",

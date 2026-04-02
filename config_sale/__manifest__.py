c6e60b74ac92ff1858df1f52d54a15e7382ca52c
sergiocorato <sergiocorato@gmail.com>
2026-04-02 18:44:05 +0200

    [MIG] config_sale: Migration to 18.0

--- config_sale/__manifest__.py
+++ config_sale/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Module to configure sale",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/efatto",

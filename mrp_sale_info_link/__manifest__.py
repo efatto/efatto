35a1e3c4a88626c09f5435fef0d8f9e4de4ce612
sergiocorato <sergiocorato@gmail.com>
2026-04-02 18:49:09 +0200

    [MIG] mrp_sale_info_link: Migration to 18.0

--- mrp_sale_info_link/__manifest__.py
+++ mrp_sale_info_link/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale MRP info Link",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Sales Management",
     "website": "https://github.com/efatto/efatto",
     "author": "Sergio Corato",

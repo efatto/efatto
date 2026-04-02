1e163edf7b9da285627a837a784b17b851f7276b
sergiocorato <sergiocorato@gmail.com>
2026-04-02 18:39:51 +0200

    [MIG] account_invoice_single_tax: Migration to 18.0

--- account_invoice_single_tax/__manifest__.py
+++ account_invoice_single_tax/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Single tax restrict in invoice",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/efatto",
     "category": "Localisation / Accounting",

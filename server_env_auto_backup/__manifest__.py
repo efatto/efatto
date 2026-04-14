9a8208411b00d25fe58f061d92568e06a6d614f6
sergiocorato <sergiocorato@gmail.com>
2026-04-14 12:07:20 +0200

    [IMP] bump version

--- server_env_auto_backup/__manifest__.py
+++ server_env_auto_backup/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Server Env Auto Backup",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "other",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/efatto",

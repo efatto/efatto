d2a8d18991e6b50e47cccafe5faea5c14a8d4485
sergiocorato <sergiocorato@gmail.com>
2026-04-15 18:03:37 +0200

    [MIG] project_stage_unique: Migration to 18.0

--- project_stage_unique/models/project.py
+++ project_stage_unique/models/project.py
@@ -1,12 +1,9 @@
-from odoo import _, api, models
-from odoo.exceptions import UserError
+from odoo import models
 
 
 class ProjectTaskType(models.Model):
     _inherit = "project.task.type"
 
-    @api.constrains("name")
-    def _constrains_name_unique(self):
-        for rec in self:
-            if self.search_count([("name", "=", rec.name), ("id", "!=", rec.id)]):
-                raise UserError(_("A task type with the same name already exists!"))
+    _sql_constraints = [
+        ("name_uniq", "unique (name)", "Name must be unique"),
+    ]

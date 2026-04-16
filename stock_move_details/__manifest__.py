5633f08c9f686ba1780c02fe52986d340d72afb7
sergiocorato <sergiocorato@gmail.com>
2026-04-16 18:16:43 +0200

    [MIG] stock_move_details: Migration to 18.0

--- stock_move_details/__manifest__.py
+++ stock_move_details/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2017-2020 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2017 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock move details in tree",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/efatto",

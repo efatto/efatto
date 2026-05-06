1872a56e025a0b7dd844c64162ab93a3379135ff
sergiocorato <sergiocorato@gmail.com>
2026-05-06 12:00:38 +0200

    [FIX] server_env_mail wrong method calls

--- server_env_mail/models/fetchmail_server.py
+++ server_env_mail/models/fetchmail_server.py
@@ -7,9 +7,9 @@ class Fetchmail(models.Model):
     _inherit = "fetchmail.server"
 
     @running
-    def fetch_mail(self):
-        return super().fetch_mail()
+    def fetch_mail(self, raise_exception=True):
+        return super().fetch_mail(raise_exception=raise_exception)
 
     @running
-    def connect(self):
-        return super().connect()
+    def connect(self, allow_archived=False):
+        return super().connect(allow_archived=allow_archived)
--- server_env_mail/models/mail.py
+++ server_env_mail/models/mail.py
@@ -7,5 +7,9 @@ class MailMail(models.Model):
     _inherit = "mail.mail"
 
     @running
-    def send(self, auto_commit=False, raise_exception=False):
-        return super().send(auto_commit=auto_commit, raise_exception=raise_exception)
+    def send(self, auto_commit=False, raise_exception=False, post_send_callback=None):
+        return super().send(
+            auto_commit=auto_commit,
+            raise_exception=raise_exception,
+            post_send_callback=post_send_callback,
+        )

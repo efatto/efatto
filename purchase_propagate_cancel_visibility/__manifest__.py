e3790eeb2e3c13e90565189fdf77fe5ab4ef65bf
sergiocorato <sergiocorato@gmail.com>
2026-05-29 09:27:17 +0200

    [MIG] purchase_propagate_cancel_visibility: Migration to 18.0

--- purchase_propagate_cancel_visibility/__manifest__.py
+++ purchase_propagate_cancel_visibility/__manifest__.py
@@ -2,13 +2,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Purchase Propagate Cancel Visibility",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "other",
-    "summary": """
-    When MTO route is removed from a product which has draft RfQ and OUT from Sale
-    Orders are done, the propagation cancel option must be removed manually from
-    purchase order line to proceed.
-    """,
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/efatto",
     "license": "AGPL-3",
---
+++ purchase_propagate_cancel_visibility/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module change groups to `propagate_cancel` in purchase order form to set it visible to purchase manager, instead of only system manager.

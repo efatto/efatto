ef258f0aa5105031ff83ebcef98c63c30036e76c
sergiocorato <sergiocorato@gmail.com>
2026-09-01 11:27:02 +0200

    [FIX] set name dn with super()

--- l10n_it_dn_force_number/wizard/delivery_note_create.py
+++ l10n_it_dn_force_number/wizard/delivery_note_create.py
@@ -9,27 +9,11 @@ class StockDeliveryNoteCreateWizard(models.TransientModel):
     name = fields.Char()
 
     def confirm(self):
-        self.check_compliance(self.selected_picking_ids)
-
-        sale_order_ids = self.mapped("selected_picking_ids.sale_id")
-        sale_order_id = (
-            sale_order_ids and sale_order_ids[0] or self.env["sale.order"].browse()
-        )
-        values = self._prepare_delivery_note_vals(sale_order_id)
-        values.update(
-            {
-                "partner_id": self.partner_id.id,
-                "delivery_method_id": self.partner_id.property_delivery_carrier_id.id,
-            }
-        )
-        if self.name:
-            values.update({"name": self.name})
-        delivery_note = self.env["stock.delivery.note"].create(values)
-
-        self.selected_picking_ids.write({"delivery_note_id": delivery_note.id})
-
-        if self.env.user.has_group("l10n_it_delivery_note.use_advanced_delivery_notes"):
-            return delivery_note.goto()
+        res = super().confirm()
+        if self.name and res.get("res_id"):
+            delivery_note = self.env["stock.delivery.note"].browse(res["res_id"])
+            delivery_note.write({"name": self.name})
+        return res
 
     def _prepare_delivery_note_vals(self, sale_order_id):
         res = super()._prepare_delivery_note_vals(sale_order_id=sale_order_id)
